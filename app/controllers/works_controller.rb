5fe785ff21fd7b4c3f2baee421c04245d73fe61c
Lauren <Laurenelee88@gmail.com>
2017-10-10 15:04:32 -0700

    working index page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,9 +1,11 @@
 class WorksController < ApplicationController
 
   def index
+    @works = Work.all
   end
 
   def show
+    @work = Work.find(params[:id])
   end
 
   def new
@@ -12,15 +14,12 @@ class WorksController < ApplicationController
   def create
   end
 
-  def show
-  end
-
   def edit
   end
 
   def update
   end
-  
+
   def destroy
   end
 
