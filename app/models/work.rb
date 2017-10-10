3d099927e9b01476c11cce6b34e4b4e6796a8197
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-10 16:30:02 -0700

    Validations for users, votes, works.

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,7 @@
 class Work < ApplicationRecord
   belongs_to :user, optional: true
   has_many :votes
+  validates :publication_year, numericality: { only_integer: true }
+  validates :title, presence: true
+  validates :creator, presence: true
 end
