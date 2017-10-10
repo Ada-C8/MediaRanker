bb864534bdde40df3cf8224990a27da44bf24d8a
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-10-10 15:46:56 -0700

    added controller and model for works, users

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,22 @@
+class WorksController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
