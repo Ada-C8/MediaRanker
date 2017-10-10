52d6ef6e78b175d9e93df94ee7dfd60520b4e1f8
sairagula <abdukhalieva@gmail.com>
2017-10-10 16:51:11 -0700

    generated controller, model and created db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+WORK_FILE = Rails.root.join('db', 'media_seeds.csv')
+puts "Loading raw driver data from #{DRIVER_FILE}"
+
+driver_failures = []
+CSV.foreach(DRIVER_FILE, :headers => true) do |row|
+  driver = Driver.new
+  driver.id = row['id']
+  driver.name = row['name']
+  driver.vin = row['vin']
+  puts "Created driver: #{driver.inspect}"
+  successful = driver.save
+  if !successful
+    driver_failures << driver
+  end
+end
+
+puts "Added #{Driver.count} driver records"
+puts "#{driver_failures.length} drivers failed to save"
