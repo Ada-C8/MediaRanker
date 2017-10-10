d40c2b3474170790f3603c801b27abc92e4c462f
Bennett Rahn <rahnibennett@gmail.com>
2017-10-10 16:58:06 -0700

    add the seed data csv reader, and create an index page for works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,3 +1,50 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all#.order('title asc')
+  end
 
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to('/works')
+    else
+      render :new
+    end
+
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+    @work.update_attributes(work_params)
+    if @work.save
+      redirect_to(work_path(@work))
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @work = Work.find(params[:id])
+    @work.destroy
+
+    redirect_to works_path
+  end
+
+private
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+require 'rails'
+
+# puts Rails.root
+
+MEDIA_FILE = Rails.root.join('db', 'media_seeds.csv')
+# what is this doing and why does it need to happen?
+puts "Loading raw driver data from #{MEDIA_FILE}"
+
+work_failures = []
+
+CSV.foreach(MEDIA_FILE, :headers => true) do |row|
+  work = Work.new
+  work.category = row['category']
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row['description']
+
+  puts "Created work: #{work.inspect}"
+  successful = work.save
+
+  if !successful
+    work_failures << work
+  end
+end
+
+puts "Added #{Work.count} work records"
+puts "#{work_failures.length} works failed to save"
+
+# driver_failures = []
+# CSV.foreach(MEDIA_FILE, :headers => true) do |row|
+#   driver = Driver.new
+#   driver.id = row['id']
+#   driver.name = row['name']
+#   driver.vin = row['vin']
+#   puts "Created driver: #{driver.inspect}"
+#   successful = driver.save
+#   if !successful
+#     driver_failures << driver
+#   end
+# end
+#
+# puts "Added #{Driver.count} driver records"
+# puts "#{driver_failures.length} drivers failed to save"
