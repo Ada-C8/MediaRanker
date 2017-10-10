9435971aa6a517781cca5755e106e52b53617cfc
sairagula <abdukhalieva@gmail.com>
2017-10-10 16:24:45 -0700

    Added routes for works, votes, users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  ###### Works ######
+  # # VERB    PATH    # CONTROLLERACTION   PREFIX
+  # get "/works", to: "works#index", as: "works"
+
+  # get "/works/new", to: "works#new", as: "new_work"
+  # post "/works", to: "works#create" YES
+  #
+  # get "/works/:id", to: "works#show", as: "work"
+  #
+  # get "/work/:id/edit", to: "works#edit", as: "edit_work"
+  # patch "/works/:id", to: "works#update"
+  #
+  # delete "/work/:id", to: "works#destroy"
+  resources :works
+
+  resources :votes
+
+  resources :users
 end
