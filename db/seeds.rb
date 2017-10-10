4405e124b5a7097e9391a204f4081d3f4a000366
Shaunna <skwiens@gmail.com>
2017-10-10 15:07:38 -0700

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+WORK_FILE = Rails.root.join('db', 'seed_data', 'media_seeds.csv')
+puts "Loading raw work data from #{MEDIA_FILE}"
+
+work_failures = []
+CSV.foreach(MEDIA_FILE, :headers => true) do |row|
+  work = Work.new
+  work.id = row['id']
+  work.name = row['name']
+  work.vin = row['vin']
+  puts "Created work: #{work.inspect}"
+  successful = work.save
+  if !successful
+    work_failures << work
+  end
+end
+
+puts "Added #{Work.count} driver records"
+puts "#{media_failures.length} drivers failed to save"
+
+puts "Manually resetting PK sequence on each table"
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+puts "done"
