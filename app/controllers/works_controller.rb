5610a8a571bd1a4ac979c44f07e8f5ddc8725c80
Gale Harrington <gale.harrington19@gmail.com>
2017-10-10 16:51:30 -0700

    Can create new work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,14 +1,23 @@
 class WorksController < ApplicationController
   def index
+    @works = Work.all
   end
 
   def show
+    # @work = Work.find_by
   end
 
   def new
+    @work = Work.new
   end
 
   def create
+    @work = Work.new(works_params)
+  if @work.save
+    redirect_to works_path
+  else
+    render :new
+  end
   end
 
   def edit
@@ -18,6 +27,12 @@ class WorksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def works_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 
 end
