8bdb090f7d35e9933cb46f994e95a5d4b2ebb307
murog <guillermina.muro@gmail.com>
2017-10-10 16:29:12 -0700

    updated routes to include resources and defined rooth path

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,50 @@
 Rails.application.routes.draw do
-  get 'main_page/index'
+  resources :users, :works, :votes
+  root to: 'main_page#index'
 
-  get 'works/index'
-
-  get 'works/show'
-
-  get 'works/edit'
-
-  get 'works/update'
-
-  get 'works/new'
-
-  get 'works/create'
-
-  get 'works/destroy'
-
-  get 'votes/index'
-
-  get 'votes/show'
-
-  get 'votes/edit'
-
-  get 'votes/update'
-
-  get 'votes/new'
-
-  get 'votes/create'
-
-  get 'votes/destroy'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/destroy'
+  # get 'main_page/index'
+  #
+  # get 'works/index'
+  #
+  # get 'works/show'
+  #
+  # get 'works/edit'
+  #
+  # get 'works/update'
+  #
+  # get 'works/new'
+  #
+  # get 'works/create'
+  #
+  # get 'works/destroy'
+  #
+  # get 'votes/index'
+  #
+  # get 'votes/show'
+  #
+  # get 'votes/edit'
+  #
+  # get 'votes/update'
+  #
+  # get 'votes/new'
+  #
+  # get 'votes/create'
+  #
+  # get 'votes/destroy'
+  #
+  # get 'users/index'
+  #
+  # get 'users/show'
+  #
+  # get 'users/edit'
+  #
+  # get 'users/update'
+  #
+  # get 'users/new'
+  #
+  # get 'users/create'
+  #
+  # get 'users/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
