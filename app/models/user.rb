43bbc89f6618759d9aeb41847b10f9f5c6d4ffea
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-10 16:43:21 -0700

    add test and relationship for users votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   #validations
   validates :username, presence: { message: "can't be blank" }
-  
+
   #relationships
-  # has_and_belongs_to_many :votes
+  has_many :votes
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,31 +2,24 @@ require "test_helper"
 
 describe User do
   describe "validations" do
-    it "can be created and is valid" do
+    it "must be valid, created with all fields" do
       u = User.new(username: "test")
       u.must_be :valid?
     end
 
-    it "isn't valid without a name" do
+    it "isn't valid with out a name" do
       u = User.new(username: "")
       u.wont_be :valid?
     end
-
-    # it "must be valid" do
-    #   value(user).must_be :valid?
-    # end
-    #
-    # it "isn't valid with out" do
-    # end
   end
 
   describe "relations" do
-    # it "responds to vote" do
-    #   user = User.new(username: "test")
-    #   user.must_respond_to :vote
-    # end
+    it "responds to vote" do
+      u = User.new(username: "test")
+      u.must_respond_to :votes
+    end
   end
 
-  describe "methods" do
-  end
+  # describe "methods" do
+  # end
 end
