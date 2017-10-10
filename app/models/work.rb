cf55fdbfee845593ddcd990fe2b5be55a183ecfd
Andrea Chen <andreachen.ac@gmail.com>
2017-10-10 16:15:41 -0700

    Add initial tests for Work model validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,6 @@
 class Work < ApplicationRecord
-  #validates presence of category, title, creator 
+  #validates presence of category, title, creator
+  validates :category, presence: {message: "The category must be present"}
+  validates :title, presence: {message: "The title must be present"}
+  validates :creator, presence: {message: "The creator must be present"}
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,20 +1,32 @@
 require "test_helper"
 
 describe Work do
-  # let(:work) { Work.new }
 
   describe "validations" do
+    let(:work) { Work.new }
+
     it "can be created with all fields" do
       w = Work.new(category: "album", title: "Infinite Worlds", creator: "Vagabon", publication_year: 2017, description: "On her compact debut, Lætitia Tamko explores ideas of home, community, and sharing space with others who don’t necessarily see eye to eye.")
 
       w.must_be :valid?
     end
 
-    it 'is invalid without a category' do
-      w = Work.new
-      result = w.valid?
+    it "is invalid without a category" do
+      result = work.valid?
       result.must_equal false
-      w.errors.messages.must_include :category
+      work.errors.messages.must_include :category
+    end
+
+    it "is invalid without a title" do
+      result = work.valid?
+      result.must_equal false
+      work.errors.messages.must_include :title
+    end
+
+    it "is invalid without a creator" do
+      result = work.valid?
+      result.must_equal false
+      work.errors.messages.must_include :creator
     end
   end
 
