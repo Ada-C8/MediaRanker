8ca9b747f311575aa9a19a09f874646b56ca46dd
Tanisha <tanishahampden@gmail.com>
2017-10-10 16:57:28 -0700

    add create contoller action

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -5,5 +5,12 @@ class WorksController < ApplicationController
 
   def new
     @work = Work.new
-  end 
+  end
+
+  def create
+    work = Work.new(title: params[:work][:title], description: params[:work][:description], creator: params[:work][:creator], category: params[:work][:category], publication_year: params[:work][:publication_year])
+
+    work.save
+    redirect_to('/works')
+  end
 end
