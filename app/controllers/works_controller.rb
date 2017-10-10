a0ef7b0dbde8002de9c86b4b03eaa07a1f83f30a
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-10 16:25:15 -0700

    create form partial, edit page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -8,4 +8,24 @@ class WorksController < ApplicationController
   def show
     @work = Work.find(params[:id])
   end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+    result = @work.update(work_params)
+
+    if result
+      redirect_to work_path(params[:id])
+    else
+      #error message
+    end
+  end
+
+  private
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,10 @@ class Work < ApplicationRecord
   has_many :votes, dependent: :destroy
 
   validates :title, presence: true, uniqueness: true
+
+  LEGAL_CATEGORIES = ["album", "book", "movie"]
+
+  def self.all_categories
+    LEGAL_CATEGORIES
+  end
 end
