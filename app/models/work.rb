782a12b952fe214c9ce53314894f391aff54cd13
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-10 15:40:18 -0700

    model validation for works

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,5 @@
 class Work < ApplicationRecord
   has_many :votes, dependent: :destroy
+
+  validates :title, presence: true, uniqueness: true
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,32 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.first }
+  let(:w) { works(:bladerunner) }
 
   # it "must be valid" do
   #   value(work).must_be :valid?
   # end
+
+
+  describe "Validations" do
+    it "can be created with all validations" do
+      w = Work.create!(title: "The Phantom Menace")
+
+      w.must_be :valid?
+    end
+
+    it "requires a title" do
+      w = Work.new
+
+      w.wont_be :valid?
+      w.errors.messages.must_include :title
+    end
+
+    it "requires unique title" do
+      w = Work.new(title:"Blade Runner")
+
+      w.wont_be :valid?
+      w.errors.messages.must_include :title      
+    end
+  end
 end
