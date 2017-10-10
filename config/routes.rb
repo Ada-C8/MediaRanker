e0936e438356a32356dfa09bd38ae45f624b0853
murog <guillermina.muro@gmail.com>
2017-10-10 15:58:28 -0700

    created models, model relationships and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,45 @@
 Rails.application.routes.draw do
+  get 'works/index'
+
+  get 'works/show'
+
+  get 'works/edit'
+
+  get 'works/update'
+
+  get 'works/new'
+
+  get 'works/create'
+
+  get 'works/destroy'
+
+  get 'votes/index'
+
+  get 'votes/show'
+
+  get 'votes/edit'
+
+  get 'votes/update'
+
+  get 'votes/new'
+
+  get 'votes/create'
+
+  get 'votes/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
