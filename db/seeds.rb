7829f018ad3d0f1f0104289be2c7a2fda1fba409
Sara C <srachandler@gmail.com>
2017-10-10 16:32:55 -0700

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,69 +1,28 @@
 require 'csv'
 
-DRIVER_FILE = Rails.root.join('db', 'seed_data', 'drivers.csv')
-puts "Loading raw driver data from #{DRIVER_FILE}"
+WORK_FILE = Rails.root.join('db', 'media_seeds.csv')
+puts "Loading raw works data from #{WORK_FILE}"
 
-driver_failures = []
-CSV.foreach(DRIVER_FILE, :headers => true) do |row|
-  driver = Driver.new
-  driver.id = row['id']
-  driver.name = row['name']
-  driver.vin = row['vin']
-  puts "Created driver: #{driver.inspect}"
-  successful = driver.save
+work_failures = []
+i = 1
+CSV.foreach(WORK_FILE, :headers => true) do |row|
+  work = Work.new
+  work.id = i
+  work.category = row['category']
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row['description']
+  puts "Created work: #{work.inspect}"
+  successful = work.save
   if !successful
-    driver_failures << driver
+    work_failures << work
   end
+  i += 1
 end
 
-puts "Added #{Driver.count} driver records"
-puts "#{driver_failures.length} drivers failed to save"
-
-
-
-PASSENGER_FILE = Rails.root.join('db', 'seed_data', 'passengers.csv')
-puts "Loading raw passenger data from #{PASSENGER_FILE}"
-
-passenger_failures = []
-CSV.foreach(PASSENGER_FILE, :headers => true) do |row|
-  passenger = Passenger.new
-  passenger.id = row['id']
-  passenger.name = row['name']
-  passenger.phone_num = row['phone_num']
-  puts "Created passenger: #{passenger.inspect}"
-  successful = passenger.save
-  if !successful
-    passenger_failures << passenger
-  end
-end
-
-puts "Added #{Passenger.count} passenger records"
-puts "#{passenger_failures.length} passengers failed to save"
-
-
-
-TRIP_FILE = Rails.root.join('db', 'seed_data', 'trips.csv')
-puts "Loading raw trip data from #{TRIP_FILE}"
-
-trip_failures = []
-CSV.foreach(TRIP_FILE, :headers => true) do |row|
-  trip = Trip.new
-  trip.id = row['id']
-  trip.driver_id = row['driver_id']
-  trip.passenger_id = row['passenger_id']
-  trip.date = Date.strptime(row['date'], '%Y-%m-%d')
-  trip.rating = row['rating']
-  trip.cost = row['cost']
-  puts "Created trip: #{trip.inspect}"
-  successful = trip.save
-  if !successful
-    trip_failures << trip
-  end
-end
-
-puts "Added #{Trip.count} trip records"
-puts "#{trip_failures.length} trips failed to save"
-
+puts "Added #{Work.count} work records"
+puts "#{work_failures.length} works failed to save"
 
 # Since we set the primary key (the ID) manually on each of the
 # tables, we've got to tell postgres to reload the latest ID
