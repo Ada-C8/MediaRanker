ba998618910d12d04a81d7532999f32c16aa59cc
Maria McGrew <mcgmar1@outlook.com>
2017-10-10 15:41:44 -0700

    Initial rails setup

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+require 'csv'
+
+WORKS_FILE = Rails.root.join('db', 'seed_data', 'media_seeds.csv')
+puts "Loading raw media data from #{WORKS_FILE}"
+
+works_failures = []
+CSV.foreach(WORKS_FILE, :headers => true) do |row|
+  work = Work.new
+  work.id = row['id']
+  work.category = row['category']
+  work.title = row['title']
+  puts "Created work: #{work.inspect}"
+  successful = work.save
+  if !successful
+    works_failures << work
+  end
+end
+
+puts "Added #{Work.count} work records"
+puts "#{works_failures.length} works failed to save"
+
+# Since we set the primary key (the ID) manually on each of the
+# tables, we've got to tell postgres to reload the latest ID
+# values. Otherwise when we create a new record it will try
+# to start at ID 1, which will be a conflict.
+puts "Manually resetting PK sequence on each table"
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+puts "done"
