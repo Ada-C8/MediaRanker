0196ae0ba23a0ab133381b242be973b2e56afe77
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-10-10 16:05:33 -0700

    Initial draft of controller methods for work model

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,24 +1,50 @@
 class WorksController < ApplicationController
 
   def index
+    @works = Work.all
   end
 
   def show
+    @work = Work.find(params[:id])
   end
 
   def new
+    @work = Work.new
   end
 
   def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @work = Work.find(params[:id])
   end
 
   def update
+    @work = Work.find(params[:id])
+    result = @work.update(work_params)
+    if result
+      redirect_to work_path(@work.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    work = Work.find(params[:id])
+    work.destroy
+
+    redirect_to works_path
   end
 
+  private
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
