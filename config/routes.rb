6370286d8430b543f52aa48bb6845b5e5f340182
kee nam <nam.keeyeon@gmail.com>
2017-10-10 14:48:26 -0700

    Added home controller for index and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  resources :users, :works do
+    resources :votes, only: [:index, :new, :create]
+  end
 end
