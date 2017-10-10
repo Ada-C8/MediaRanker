659628c4b7cbd271d9223f24d830bba24129d6a5
Sara C <srachandler@gmail.com>
2017-10-10 15:46:38 -0700

    set up models and controllers for User and Works

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'works/index'
+
+  get 'works/show'
+
+  get 'works/new'
+
+  get 'works/edit'
+
+  get 'works/update'
+
+  get 'works/create'
+
+  get 'works/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
