1845b2358dc5d6045e09a25cb6206c0f34ebda66
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-10 16:01:45 -0700

    add work#show page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,8 +1,11 @@
 class WorksController < ApplicationController
-
   def index
     @albums = Work.where(category: "album")
     @movies = Work.where(category: "movie")
     @books = Work.where(category: "book")
+  end
+
+  def show
+    @work = Work.find(params[:id])
   end
 end
