52577e6f0aa46b16972b65d1cc60da534757353d
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-10 14:39:29 -0700

    generated Works controller

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,22 @@
+class WorksController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'works/create'
+
+  get 'works/destroy'
+
+  get 'works/edit'
+
+  get 'works/new'
+
+  get 'works/show'
+
+  get 'works/update'
+
+  get 'works/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
