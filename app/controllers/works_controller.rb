a67de67febaf14f5746663bfd6787327a2bbbda9
Lauren <Laurenelee88@gmail.com>
2017-10-10 16:40:10 -0700

    setting up controller and models

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -9,18 +9,48 @@ class WorksController < ApplicationController
   end
 
   def new
+    @work = Work.new
   end
 
   def create
+    @work = Work.new(
+    work_params
+    )
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @work = Work.find(params[:id])
   end
 
   def update
+    @work = Work.find(params[:id])
+    result = @work.update(work_params)
+
+    if result
+      redirect_to work_path(@work.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @work = Work.find(params[:id])
+    if @work.destroy
+      redirect_to work_path
+    else
+      #error message
+    end
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 
 end
