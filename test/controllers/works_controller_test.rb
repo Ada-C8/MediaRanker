4ec6e419d0b123d45235e21916fd5afb61c4449d
Amy Lee <amy.yj.lee@gmail.com>
2017-10-11 16:16:55 -0700

    moved tests for creating a new work to the Work Controller tests

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -44,7 +44,7 @@ describe WorksController do
 
   it "should stay on the add work page and display the error message banner if we are unsuccessful in creating or updating a work" do
 
-  end 
+  end
 
   it "should be able to update a work" do
 
@@ -56,6 +56,34 @@ describe WorksController do
 
   it "renders to the same page if unsucessful create or update" do
 
+  end
+
+  describe 'creating a new Work' do
+    it "requires only a title to create a new Work" do
+      work = Work.new
+
+      work.valid?.must_equal false
+
+      work.title = "New Work"
+
+      work.valid?.must_equal true
+    end
+
+    it "raises an error if there is a Work that has the same category AND same title" do
+      existing_book = works(:book_one)
+      new_book_same_title = Work.new(category: existing_book.category, title: existing_book.title)
+
+      new_book_same_title.valid?.must_equal false
+
+      new_book_same_title.errors.must_include :title
+    end
+
+    it "allows you to create a new Work that has the same title of a Work in a different category" do
+      book_with_title = works(:book_one)
+      album_with_same_title = Work.new(category: "album", title: book_with_title.title)
+
+      album_with_same_title.valid?.must_equal true
+    end
   end
 
 end
