d92994ac76a0b9b6eca5ea497ae589af822d77ea
Canaan West <canaan.west@gmail.com>
2017-10-11 16:27:03 -0700

    built table to display all work_index data

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,8 @@
 Rails.application.routes.draw do
-  get 'votes/index'
+  root to: 'works#index'
 
-  get 'votes/show'
-
-  get 'votes/new'
-
-  get 'votes/create'
-
-  get 'votes/destroy'
-
-  get 'votes/edit'
-
-  get 'votes/update'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/destroy'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'works/index'
-
-  get 'works/show'
-
-  get 'works/create'
-
-  get 'works/new'
-
-  get 'works/destroy'
-
-  get 'works/edit'
-
-  get 'works/update'
-
+  resources :works
+  resources :users
+  resources :votes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
