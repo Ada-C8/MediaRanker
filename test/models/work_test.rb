ada0b0de8aafb8a6118bed652f0fd82dbb3d9f82
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-10-10 21:16:08 -0700

    added work tests- validates title

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,7 +3,13 @@ require "test_helper"
 describe Work do
   let(:work) { Work.new }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  it "is invalid without a title" do
+    work.valid?.must_equal false
+    work.errors.messages.must_include :title
+  end
+  it "must have a title to be valid" do
+    work.valid?.must_equal false
+    work.title = "Title"
+    work.valid?.must_equal true
   end
 end
