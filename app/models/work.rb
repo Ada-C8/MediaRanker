dcf7fc81643ea5c72e32618816b9a79636c10e34
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-10 20:30:35 -0700

    Display works grouped by category

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,6 +3,4 @@ class Work < ApplicationRecord
   validates :title, presence: { message: "Please enter a title of your entry" }
   validates :category, presence: { message: "Please select a category" }
 
-  # @@categories = ["Albums", "Books", "Movies"]
-
 end
