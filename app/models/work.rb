55de7e8f11a64cc6f09b9e6f9096d677fef8909e
Tanisha <tanishahampden@gmail.com>
2017-10-10 22:24:49 -0700

    add validations to model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,6 @@
 class Work < ApplicationRecord
+  validates :title, presence: true
+  validates :creator, presence: true
+  validates :category, presence: true
+  validates :description, length: { maximum: 500, message: "The description is too long" }
 end
