ab5920c360122158f9a45461de50b268c268d743
Andrea Chen <andreachen.ac@gmail.com>
2017-10-11 15:58:41 -0700

    Add tests for #index, #new, #create for WorksController

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -10,6 +10,48 @@ describe "WorksController" do
       get works_path
       must_respond_with :success
     end
+
+    it "returns a success status when there are no works" do
+      Work.destroy_all
+      get works_path
+      must_respond_with :success
+    end
+
+  end
+
+  describe "new" do
+    it "returns a success status for creating a new work" do
+      get new_work_path
+      must_respond_with :success
+    end
+  end
+
+  describe "create" do
+    it "adds the work to the database and redirects when the work data is valid" do
+      #arrange
+      work_data = {
+        work: {
+          category: "book",
+          title: "When Nietzsche Wept",
+          creator: "Irvin Yalom",
+          publication_year: 1990,
+          description: "Read it"
+        }
+      }
+
+      Work.new(work_data[:work]).must_be :valid?
+
+      start_work_count = Work.count
+
+      #act
+      post works_path, params: work_data
+
+      #assert
+      must_respond_with :redirect
+      must_redirect_to works_path
+
+      Work.count.must_equal start_work_count + 1
+    end
   end
 
 end
