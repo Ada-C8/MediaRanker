03654e2253c3e4cfc9e5b45852a40aa4f6d63d2b
Roxanne <argonkryptonneon@gmail.com>
2017-10-11 11:27:56 -0700

    all resources for vote except show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :vote
+  resources :vote, except[:show]
   resources :user
   resources :media_instance
   root 'main#index'
