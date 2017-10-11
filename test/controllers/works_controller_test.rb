7d40f69ad6614c0fb6c5cc8d4cb52ea568976733
Jessica Owens <vertige@gmail.com>
2017-10-11 14:01:08 -0700

    Tested WorksController#index and #show and created ApplicationController#render_404

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,39 +1,43 @@
 require "test_helper"
 
 describe WorksController do
+  let(:work) { works(:book1) }
   it "should get index" do
-    get works_index_url
-    value(response).must_be :success?
+    get works_path
+    must_respond_with :success
   end
+  #
+  # it "should get new" do
+  #   get works_new_url
+  #   value(response).must_be :success?
+  # end
+  #
+  it "should get work detail page (#show) or render a 404" do
+    get work_path(work.id)
+    must_respond_with :success
 
-  it "should get new" do
-    get works_new_url
-    value(response).must_be :success?
+    # get work_path(ada)
+    # must_respond_with :error
   end
-
-  it "should get show" do
-    get works_show_url
-    value(response).must_be :success?
-  end
-
-  it "should get create" do
-    get works_create_url
-    value(response).must_be :success?
-  end
-
-  it "should get update" do
-    get works_update_url
-    value(response).must_be :success?
-  end
-
-  it "should get edit" do
-    get works_edit_url
-    value(response).must_be :success?
-  end
-
-  it "should get destroy" do
-    get works_destroy_url
-    value(response).must_be :success?
-  end
+  #
+  # it "should get create" do
+  #   get works_create_url
+  #   value(response).must_be :success?
+  # end
+  #
+  # it "should get update" do
+  #   get works_update_url
+  #   value(response).must_be :success?
+  # end
+  #
+  # it "should get edit" do
+  #   get works_edit_url
+  #   value(response).must_be :success?
+  # end
+  #
+  # it "should get destroy" do
+  #   get works_destroy_url
+  #   value(response).must_be :success?
+  # end
 
 end
