b6e023eac0fe87293511706bbf464f2c6c715725
Tanisha <tanishahampden@gmail.com>
2017-10-10 23:27:03 -0700

    add @works to index method

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
   def index
+    @works = Work.all
     @movies = Work.where(category: "movie").limit(10)
     @books = Work.where(category: "book").limit(10)
     @albums = Work.where(category: "album").limit(10)
