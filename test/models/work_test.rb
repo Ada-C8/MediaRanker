2b72e0bfc1ff05bffc76bc2050a2169ccbd3e3c3
Gale Harrington <gale.harrington19@gmail.com>
2017-10-10 17:17:26 -0700

    Test that new works are valid

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,8 +2,27 @@ require "test_helper"
 
 describe Work do
   let(:work) { Work.new }
+  let(:gatsby) { works(:gatsby) }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  it "must have a category to be valid" do
+    work.title = "title"
+    work.valid?.must_equal false
+    work.category = "category"
+    work.valid?.must_equal true
+  end
+
+  it "must have a title to be valid" do
+    work.category = "category"
+    work.valid?.must_equal false
+    work.title = "title"
+    work.valid?.must_equal true
+  end
+
+  it "must have a unique title to be valid" do
+    work.category = "category"
+    work.title = "The Great Gatsby"
+    work.valid?.must_equal false
+    work.title = "Uniquest Book Ever"
+    work.valid?.must_equal true
   end
 end
