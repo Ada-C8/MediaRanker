44038d50f7b74540a934661010cbbcd61928e0f4
averikitsch <aekitsch@gmail.com>
2017-10-11 10:41:13 -0700

    Added tests to the work controller

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,10 +1,11 @@
 require "test_helper"
 
 describe WorksController do
-  # it "should get index" do
-  #   get works_index_url
-  #   value(response).must_be :success?
-  # end
+  let(:book) {works(:hp)}
+  it "should get index" do
+    get works_path
+    must_respond_with :success
+  end
   #
   # it "should get new" do
   #   get works_new_url
@@ -21,15 +22,18 @@ describe WorksController do
   #   value(response).must_be :success?
   # end
   #
-  # it "should get show" do
-  #   get works_show_url
-  #   value(response).must_be :success?
-  # end
+  it "should get show" do
+    get work_path(book.id)
+    must_respond_with :success
+  end
   #
-  # it "should get update" do
-  #   get works_update_url
-  #   value(response).must_be :success?
-  # end
+  it "should get update" do
+    put work_path(works(:hp).id), params: {work: {title: "Harry and The Stone"} }
+    book = Work.find(works(:hp).id)
+    book.title.must_equal "Harry and The Stone"
+    must_respond_with :redirect
+    must_redirect_to works_path
+  end
   #
   # it "should get destroy" do
   #   get works_destroy_url
