72594946cf2490a84a5ac6c6e91d43b2ab198945
Lauren <Laurenelee88@gmail.com>
2017-10-11 11:56:28 -0700

    new tests in update

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,18 +3,29 @@ class WorksController < ApplicationController
   def index
     # if params[:author_id]
     # @books = Author.find(params[:author_id]).books
-  # else
-  # @books = Book.all
-  # end
+    # else
+    # @books = Book.all
+    # end
+
+    # if ##### do I want an if statement??
+    # else
+    # head :not_found
+    # end
 
     @works = Work.all
     @movies = Work.where(category: "movie")
     @books = Work.where(category: "book")
     @albums = Work.where(category: "album")
+    unless @works
+      head :not_found
+    end
   end
 
   def show
-    @work = Work.find(params[:id])
+    find_work
+    unless @work
+      head :not_found
+    end
   end
 
   def new
@@ -26,27 +37,36 @@ class WorksController < ApplicationController
     work_params
     )
     if @work.save
+      flash[:status] = :success
+      flash[:message] = "Successfully created work #{work.id}"
       redirect_to works_path
     else
+      flash[:status] = :failure
+      flash[:message] = "Failed to created work #{work.id}"
+      flash[:details] = @work.error.messages
       render :new, status: :bad_request
     end
   end
 
   def edit
-    @work = Work.find(params[:id])
-  end
-
-  def update
-    @work = Work.find(params[:id])
-    result = @work.update(work_params)
-
-    if result
-      redirect_to work_path(@work.id)
-    else
-      render :edit
+    find_work
+    unless @work
+      head :not_found
     end
   end
 
+  def update
+    if find_work
+      @work.update_attributes(work_params)
+      if @work.save
+        redirect_to work_path(@work)
+        return
+      else
+        render :edit, status: :bad_request
+        return
+      end
+    end
+  end
   def destroy
     @work = Work.find(params[:id])
     if @work.destroy
@@ -60,6 +80,15 @@ class WorksController < ApplicationController
 
   def work_params
     return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+
+  def find_work
+    @work = Work.find_by(id: params[:id])
+    unless @work
+      head :not_found
+    else
+      return @work
+    end
   end
 
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -17,13 +17,29 @@ describe WorksController do
     end
   end
 
-  describe "new" do
+  describe "show" do
+    it "returns success when given a valid work id" do
+      work_id = Work.first.id
+      get work_path(work_id)
+      must_respond_with :success
+    end
 
+    it "returns not_found when given a bad work id" do
+      bad_work_id = Work.last.id + 1
+      get work_path(bad_work_id)
+      must_respond_with :not_found
+    end
+  end
+
+  describe "new" do
+    it "creates a new work" do
+      # fill in!
+    end
   end
 
   describe "create" do
     #the work we created is valid # positive test
-    it "does something when the work data is valid" do
+    it "saves and redirects to works_path when the work data is valid" do
       work_data = {
         work: {
           category: "book",
@@ -40,7 +56,7 @@ describe WorksController do
     end
 
     # negative test
-    it "does something when the work data is invalid" do
+    it "renders a bad_request when the work data is invalid" do
       # arrange
       bad_work = {
         work: {
@@ -52,7 +68,6 @@ describe WorksController do
       start_work_count = Work.count
       # act
       post works_path, params: bad_work
-
       # assert
       must_respond_with :bad_request
       Work.count.must_equal start_work_count
@@ -60,14 +75,82 @@ describe WorksController do
   end
 
   describe "edit" do
+    it "returns success when given a valid work id" do
+      work_id = Work.first.id
+      get work_path(work_id)
+      must_respond_with :success
+    end
 
+    it "returns not_found when given a bad work id" do
+      bad_work_id = Work.last.id + 1
+      get work_path(bad_work_id)
+      must_respond_with :not_found
+    end
   end
 
   describe "update" do
+    it "returns success when change is valid" do
+      work = Work.first
+      work_data = {
+        work: {
+          category: "book",
+          title: "Changed title"
+        }
+      }
+      work.update_attributes(work_data[:work])
+      work.must_be :valid?
+
+      patch work_path(work), params: work_data
+      must_respond_with :redirect
+      must_redirect_to work_path(work)
+
+      work.reload
+      work.title.must_equal work_data[:work][:title]
+    end
+
+    it "returns not_found if work id is invalid" do
+      # fill in
+      
+      # bad_work_id = Work.last.id + 1
+      # work_data = {
+      #   work: {
+      #     title: "booktitle"
+      #   }
+      # }
+      # work.update_attributes(work_data[:work])
+      # work.wont_be :valid?
+      # start_work_count = Work.count
+      #
+      # patch work_path(work), params: bad_work_data
+      # must_respond_with :bad_request
+      # Work.count.must_equal start_work_count
+    end
+
+    it "returns bad_request when change is invalid" do
+      work = Work.first
+      bad_work_data = {
+        work: {
+          title: ""
+        }
+      }
+      work.update_attributes(bad_work_data[:work])
+      work.wont_be :valid?
+      start_work_count = Work.count
+
+      patch work_path(work), params: bad_work_data
+      must_respond_with :bad_request
+      Work.count.must_equal start_work_count
+    end
 
   end
 
   describe "destroy" do
+    it "success when book is deleted" do
+      #fill in
+    end
 
+    it "returns bad_request when ???" do
+      # fill in
+    end
   end
 end
