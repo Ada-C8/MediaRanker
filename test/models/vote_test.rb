844f37403277c3357f04e454e4312b2f49337b41
Canaan West <canaan.west@gmail.com>
2017-10-11 15:45:34 -0700

    vote validation tests written

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -3,8 +3,18 @@ require "test_helper"
 describe Vote do
   let(:vote) {votes(:one)}
 
-  it "must be valid" do
+  it "will be valid when user and work are present" do
     vote.valid?.must_equal true
+  end
+
+  it "will not be valid unless user is present" do
+    vote.user = nil
+    vote.valid?.must_equal false
+  end
+
+  it "will not be valid unless a work is present" do
+    vote.work = nil
+    vote.valid?.must_equal false
   end
 
 end
