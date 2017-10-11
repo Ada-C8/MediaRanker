d6d80ca6897ffd484cd05503c5e461958b8c990d
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-10 19:17:08 -0700

    *Fix order of SQL queries to descending by # of votes *Include description and number of votes in media spotlight *Include number of votes for top 10 lists

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,2 +1,7 @@
 class HomepageController < ApplicationController
+  def index
+    @top_work = Work.get_top_work
+    @top_works = Work.get_top_works(Work.get_categories)
+    @plural = @top_work.votes.count == 1 ? "" : "s"
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -5,12 +5,21 @@ class Work < ApplicationRecord
     return Work.distinct.pluck(:category).sort
   end
 
+  def self.get_top_work
+    top_work = Work.joins(:votes).group(:id).order("count(votes.id) DESC").limit(1)[0]
+    p top_work
+    return top_work
+  end
+
   def self.get_top_works(categories)
     all_top_works = {}
     categories.each do |a_category|
-      this_category_top_works = Work.where(category: a_category).joins(:votes).group(:id).order("count(votes.id)").limit(10)
+      this_category_top_works = Work.where(category: a_category).joins(:votes).group(:id).order("count(votes.id) DESC").limit(10)
       all_top_works[a_category] = this_category_top_works
     end
+    puts "----------------------------------------"
+    p categories
+    puts "----------------------------------------"
     return all_top_works
   end
 end
