e02191c1d3be0cb64927b47ae7aaf47644ebd6ef
kee nam <nam.keeyeon@gmail.com>
2017-10-10 17:04:13 -0700

    Added models/vote_test.rb tests and users.csv and works.csv seed_data files

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -3,7 +3,57 @@ require "test_helper"
 describe Vote do
   let(:vote) { Vote.new }
 
-  it "must be valid" do
-    value(vote).must_be :valid?
-  end
+  describe "validations" do
+    it "must not be valid without a user_id nor a work_id" do
+      vote.valid?.must_equal false
+    end
+
+    it "must not be valid without a user_id, even if it has a work_id" do
+      w = Work.create!(title: "Of Mice and Men", id: 1)
+      v = Vote.new(work_id: w.id)
+      is_valid = v.valid?
+      is_valid.must_equal false
+    end
+
+    it "must not be valid without a work_id, even if it has a user_id" do
+      u = User.create!(name: "Selina Kyle", id: 1)
+      v = Vote.new(user_id: u.id)
+      is_valid = v.valid?
+      is_valid.must_equal false
+    end
+
+    it "must be valid with both a user_id and a work_id" do
+      u = User.create!(name: "Selina Kyle", id: 1)
+      w = Work.create!(title: "Of Mice and Men", id: 1)
+      v = Vote.create!(user_id: u.id, work_id: w.id)
+
+      is_valid = v.valid?
+      is_valid.must_equal true
+    end
+  end # end validations tests
+
+  describe 'relations' do
+    it 'responds to user' do
+      vote.must_respond_to :user
+    end
+
+    it 'has a User' do
+      u = User.create!(name: "Selina Kyle", id: 1)
+      v = Vote.new(user_id: u.id)
+      v.user_id.must_equal 1
+      v.user.must_equal u
+    end
+
+    it 'responds to work' do
+      vote.must_respond_to :work
+    end
+
+    it 'has a Work' do
+      w = Work.create!(title: "Of Mice and Men", id: 1)
+      v = Vote.new(work_id: w.id)
+      v.work_id.must_equal 1
+      v.work.must_equal w
+    end
+
+  end # end relations tests
 end
