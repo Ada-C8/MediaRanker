3e9fd287b962d62e203991175c16453f87c946d5
Andrea Chen <andreachen.ac@gmail.com>
2017-10-11 15:30:54 -0700

    Add #new and #create to WorksController. Add view for #new

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,4 +4,25 @@ class WorksController < ApplicationController
     @works = Work.all
   end
 
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to works_path
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+
 end
