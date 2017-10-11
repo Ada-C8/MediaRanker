ca03f3217557e3bdb73f4dda09197278d7bf77fd
hellomarisamorris <hellomarisamorris@gmail.com>
2017-10-10 22:00:49 -0700

    Added validations to user and works.

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,7 @@
 class Work < ApplicationRecord
+  validates :title, presence: true
+  validates :category, presence: true
+  validates :creator, presence: true
+  validates :publication_year, numericality: true
   has_many :votes
 end
