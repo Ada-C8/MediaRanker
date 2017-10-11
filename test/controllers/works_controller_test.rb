7ff5e1e627c0351d3a36b86caf6122b17246cba8
averikitsch <aekitsch@gmail.com>
2017-10-11 11:01:29 -0700

    Added all basic tests to the work controller

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -6,38 +6,42 @@ describe WorksController do
     get works_path
     must_respond_with :success
   end
-  #
-  # it "should get new" do
-  #   get works_new_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get edit" do
-  #   get works_edit_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get create" do
-  #   get works_create_url
-  #   value(response).must_be :success?
-  # end
-  #
+
+  it "should get new" do
+    get new_work_path
+    must_respond_with :success
+  end
+
+  it "should get edit" do
+    get edit_work_path(book.id)
+    must_respond_with :success
+  end
+
+  it "should create" do
+    proc {
+      post works_path, params: {work: {title: "New Title",creator:"Me",publication_year:2014,category:"movie"}}
+    }.must_change 'Work.count', 1
+    must_respond_with :redirect
+    must_redirect_to works_path
+  end
+
   it "should get show" do
     get work_path(book.id)
     must_respond_with :success
   end
-  #
-  it "should get update" do
-    put work_path(works(:hp).id), params: {work: {title: "Harry and The Stone"} }
-    book = Work.find(works(:hp).id)
-    book.title.must_equal "Harry and The Stone"
+
+  it "should update" do
+    put work_path(book.id), params: {work: {title: "Harry and The Stone"} }
+    work = Work.find(book.id)
+    work.title.must_equal "Harry and The Stone"
     must_respond_with :redirect
     must_redirect_to works_path
   end
-  #
-  # it "should get destroy" do
-  #   get works_destroy_url
-  #   value(response).must_be :success?
-  # end
+
+  it "should destroy" do
+    delete work_path(book.id)
+    must_respond_with :redirect
+    must_redirect_to works_path
+  end
 
 end
