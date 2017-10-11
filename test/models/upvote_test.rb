229443391900db7f67e938b4786f1b1877606322
averikitsch <aekitsch@gmail.com>
2017-10-11 11:05:06 -0700

    Added all basic tests to the upvote controller

--- test/models/upvote_test.rb
+++ test/models/upvote_test.rb
@@ -2,8 +2,19 @@ require "test_helper"
 
 describe Upvote do
   let(:upvote) { Upvote.new }
-
+  let(:vote) { upvotes(:one) }
   it "empty upvote must be invalid" do
     upvote.valid?.must_equal false
+  end
+  it "upvote should be valid with both ids" do
+    vote.valid?.must_equal true
+  end
+  it "upvote is invalid with missing id" do
+    vote.work_id = nil
+    vote.valid?.must_equal false
+  end
+  it "upvote is invalid with missing id" do
+    vote.user_id = nil
+    vote.valid?.must_equal false
   end
 end
