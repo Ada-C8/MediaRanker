6cf11677edd3eff2dd556df7f78b654dcf94e16e
Jan Edrozo <jedrozo@gmail.com>
2017-10-11 16:47:29 -0700

    generated works controller

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'works/index'
+
+  get 'works/show'
+
+  get 'works/edit'
+
+  get 'works/update'
+
+  get 'works/new'
+
+  get 'works/create'
+
+  get 'works/destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
