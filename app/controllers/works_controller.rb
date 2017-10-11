3c42b86e04d5a747bc25730dc6215f6f3dde335a
kee nam <nam.keeyeon@gmail.com>
2017-10-10 20:53:08 -0700

    Added standard methods to works_controller.rb

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,48 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+    @work.update_attributes(work_params)
+    if @work.save
+      redirect_to work_path(@work)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @work = Work.find(params[:id])
+    @work.destroy
+    redirect_to works_path
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:title, :category, :creator, :year, :desc)
+  end
 end
