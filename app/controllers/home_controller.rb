dfc9d3221834a9dd90c7b8ffe29c9f4a5a502b22
Bennett Rahn <rahnibennett@gmail.com>
2017-10-11 09:08:52 -0700

    put top 10 on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-
+    @books = Work.where(category: "book").limit(10)
+    @albums = Work.where(category: "album").limit(10)
+    # @movies = 
   end
 end
