578497e5553d9db2ff4108c41ae150e659f419a1
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-11 14:49:55 -0700

    created session controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources:users
 
   resources:votes
+
+  get '/login', to: 'sessions#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
