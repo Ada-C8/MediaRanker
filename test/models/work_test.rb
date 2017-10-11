aa45e2bb78038f3e38dcc85c9318ba87dbbe9973
Tanisha <tanishahampden@gmail.com>
2017-10-10 22:24:07 -0700

    create test to test work validations

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,59 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  describe "validations" do
+    it "can be created with all fields" do
+      w = Work.new(title: "test title", creator: "test creator", publication_year: 2012, category: "book", description: "test test")
+      w.must_be :valid?
+    end
 
-  it "must be valid" do
-    value(work).must_be :valid?
+    it "requires a title" do
+      w = Work.new
+      is_valid = w.valid?
+      is_valid.must_equal false
+      w.errors.messages.must_include :title
+    end
+
+    it "requires a creator" do
+      w = Work.new
+      is_valid = w.valid?
+      is_valid.must_equal false
+      w.errors.messages.must_include :creator
+    end
+
+    it "requires a category" do
+      w = Work.new
+      is_valid = w.valid?
+      is_valid.must_equal false
+      w.errors.messages.must_include :category
+    end
   end
+
+  describe "description" do
+    it "rejects descriptions > 500 characters" do
+      desc = "t" * 501
+      w = Work.new(description: desc)
+
+      is_valid = w.valid?
+      is_valid.must_equal false
+
+      w.errors.messages.must_include :description
+    end
+
+    it "allow descriptions <= 500 characters" do
+      skip
+      descriptions = [
+        "a" * 500,
+        "a" * 10,
+        "a" * 0
+      ]
+
+      descriptions.each do |desc|
+        w = Work.new(title: "test", creator: "test creator", description: desc)
+
+        w.must_be :valid?
+      end
+    end
+  end
+
 end
