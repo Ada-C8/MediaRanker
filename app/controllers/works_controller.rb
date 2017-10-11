7d0a9bf9b43b7cf69003ea6da3f9fe24970b089b
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-11 15:43:21 -0700

    Added controller logic for index, show, and homepage

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,7 +3,17 @@ class WorksController < ApplicationController
     @works = Work.order(:id)
   end
 
+  def homepage
+    @albums = Work.where("category = 'album'")
+    @books = Work.where("category = 'book'")
+    @movies = Work.where("category = 'movie'")
+  end
+
   def show
+    @work = Work.find_by(id: params[:id])
+    if @work == nil
+      redirect_to root_path
+    end
   end
 
   def new
@@ -20,4 +30,5 @@ class WorksController < ApplicationController
 
   def update
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: 'works#index'
+  root to: 'works#homepage'
 
   resources :works
 
