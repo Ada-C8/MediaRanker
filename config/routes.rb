d416eca5e91a8e05fc6a50f6a37429a6a048ada5
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-10-10 17:16:00 -0700

    organized work/index by category

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'works/index'
+  get '/works', to: 'works#index'
 
   get 'works/edit'
 
@@ -20,6 +13,14 @@ Rails.application.routes.draw do
   get 'works/new'
 
   get 'works/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
