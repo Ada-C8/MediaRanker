86e78afb00a3090d9124882ee84ed967c9f807d8
Mariana <lmarianarp@gmail.com>
2017-10-10 19:15:17 -0700

    index view

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,13 @@
+class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+  end  
+end
