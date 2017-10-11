c7abd6f477728489e85590c04942df242d4a4293
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-10 20:07:04 -0700

    Begin works#show work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,4 +3,11 @@ class WorksController < ApplicationController
     @works = Work.get_works
     print @works
   end
+
+  def show
+    @work = Work.find_by(id: params[:id].to_i)
+    unless @work
+      redirect_to works_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/', to: 'homepage#index', as: 'homepage'
   # ~~~~~~~~~~~~~~~~ Works ~~~~~~~~~~~~~~~~
   get '/works', to: 'works#index', as: 'works'
+  get '/works/:id', to: 'works#show', as: 'work'
 end
