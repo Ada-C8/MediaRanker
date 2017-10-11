c4e9513c19f1a34d9fbd12d1b492ef0f653a50e4
Jessica Owens <vertige@gmail.com>
2017-10-10 20:51:10 -0700

    Created Works controller and wrote out basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'works/index', to: 'works#index', as: 'works'
+
+  get 'works/new', to: 'works#new', as: 'new_work'
+
+  get 'works/:id/edit', to: 'works#edit', as: 'edit_work'
+
+  get 'works/:id', to: 'works#show', as: 'work'
+
+  patch 'works/:id', to: 'works#update', as: 'update_path'
+
+  post 'works', to: 'works#create', as: 'create_work'
+
+  delete 'works/:id', to: 'works#destroy', as: 'delete_work'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
