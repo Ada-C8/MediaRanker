2f09d2d096a05caefa3642ef444159a693c65c0a
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-10 21:30:03 -0700

    Add functionality to delete works and their votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :destroy
   validates :username, uniqueness: true, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get '/works/:id', to: 'works#show', as: 'work'
   get '/works/:id/edit', to: 'works#edit', as: 'edit_work'
   patch '/works/:id', to: 'works#update', as: 'update_work'
-  delete '/works/:id', to: 'works#destroy', as: 'delete_trip'
+  delete '/works/:id', to: 'works#destroy', as: 'delete_work'
 end
