a19c56a5062db9e3fe9cdd375b6fde71c75a8b63
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-10 22:57:14 -0700

    added new and create methods and views (including creating partial form and revising edit view to use it)

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -13,9 +13,16 @@ class WorksController < ApplicationController
   end
 
   def new
+    @work = Work.new
   end
 
   def create
+    @work = Work.new(category: params[:work][:category], title: params[:work][:title], creator: params[:work][:creator], publication_year: params[:work][:publication_year], description: params[:work][:description])
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
   end
 
   def edit
