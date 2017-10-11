487fa1296ec8332a0d38ba4b465a153d762b4539
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-10 20:39:08 -0700

    Add tests for relationships and validations for User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,29 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  let(:u) { User.new(name: "Mira") }
+  let(:v) { Vote.new(user_id: u.id, date: Date.today) }
 
-  it "must be valid" do
+describe "Relationships" do
+  it "has many votes" do
+    u.must_respond_to :votes
+    u.votes.must_be_empty
+
+    u.votes << v
+    u.votes.must_include v
+  end # it "has many votes" do
+end # describe "Relationships" do
+
+describe "Validations" do
+  it "can be created with all fields" do
+    u = User.new(name: "Marc")
+    u.must_be :valid?
+  end # it "can be created with all fields" do
+
+  it "requires a name" do
+    u2 = User.new
+    u2.wont_be :valid?
   end
-end
+end # describe "Validations" do
+
+end # User do
