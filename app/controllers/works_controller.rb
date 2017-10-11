595a09afd1fe07846e0b1ef68dba723174105db6
nkiru onwuneme <generalhrh@gmail.com>
2017-10-10 22:06:02 -0700

    Link to works show page from index page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,20 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+
+
+  private
+  def driver_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+
 end
