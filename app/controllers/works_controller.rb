4b3f555f48f6172b27a49251c2b77c0a502b62b3
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-10 21:11:02 -0700

    Complete functionality to add work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -10,4 +10,22 @@ class WorksController < ApplicationController
       redirect_to works_path
     end
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new work_params
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/', to: 'homepage#index', as: 'homepage'
   # ~~~~~~~~~~~~~~~~ Works ~~~~~~~~~~~~~~~~
   get '/works', to: 'works#index', as: 'works'
+  get '/works/new', to: 'works#new', as: 'new_work'
+  post '/works', to: 'works#create', as: 'create_work'
   get '/works/:id', to: 'works#show', as: 'work'
 end
