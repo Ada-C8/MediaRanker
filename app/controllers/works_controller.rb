29e715fb527747d015a59648f9daaae67ab01701
Maria McGrew <mcgmar1@outlook.com>
2017-10-11 16:07:08 -0700

    test for controller update action passing

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -8,9 +8,22 @@ class WorksController < ApplicationController
   end
 
   def edit
+    @work = Work.find_by(id: params[:id].to_i)
+
+    unless @work
+      redirect_to works_path
+    end
   end
 
   def update
+    @work = Work.find_by(id: params[:id].to_i)
+    redirect_to works_path unless @work
+
+    if @work.update_attributes work_params
+      redirect_to works_path
+    else
+      render :edit
+    end
   end
 
   def new
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -16,6 +16,15 @@ describe "WorksController" do
       post works_path, params: { work: {category: "movie", title: "This is It", creator: "The movie gods", publication_year: 2011, description: "a movie about the King of Pop"}}
     }.must_change 'Work.count', 1
 
-    
+    must_respond_with :redirect
+    must_redirect_to works_path
+  end
+
+  it "should update a work" do
+    put work_path ( works(:almost) ), params: { work: { title: "New Title" }}
+
+    updated_work = Work.find( works(:almost).id )
+    updated_work.title.must_equal "New Title"
+    must_redirect_to works_path
   end
 end
