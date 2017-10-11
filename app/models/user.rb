8f18e0dff26e096a1bba1d10415f11c464c7b98e
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-11 10:54:07 -0700

    corrected user model name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :votes
   validates :name, presence: true
+  # do we want to require unique user names??
+  validates_uniqueness_of :name, message: "user name has already been taken"
 end
