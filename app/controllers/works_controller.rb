ec9e7c9149ff1c8bcee8ba68d85d4cbf0a34fd52
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-10 21:46:43 -0700

    Adds work controller actions

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -6,8 +6,45 @@ class WorksController < ApplicationController
     @albums = Work.where(category: "album")
   end
 
-  # def works_params
-  #   # params are what fields the user is allowed to set
-  #   return params.require(:work).permit(:title, :category, :creator, :publication_year, :description)
-  # end
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+    @work.update_attributes(work_params)
+    if @work.save
+      redirect_to work_path(@work)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @work = Work.find(params[:id])
+    @work.destroy
+    redirect_to works_path
+  end
+  def works_params
+    # params are what fields the user is allowed to set
+    return params.require(:work).permit(:title, :category, :creator, :publication_year, :description)
+  end
 end
