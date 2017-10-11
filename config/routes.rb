636b78a627f6abf9b794e98416c2274bc61ae197
Irene DeVera <irenneeyyy@gmail.com>
2017-10-10 20:42:23 -0700

    Add main indext html.erb apge

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# get "/users", to: "users#index", as: "users"
+# get "/users/new", to: "users#new", as: "new_user"
+# post "/users", to: "users#create"
+# get "/users/:id", to: "users#show", as: "user"
+# get "/users/:id/edit", to: "users#edit", as: "edit_user"
+# patch "/users/:id", to: "users#update"
+# delete "/users/:id", to: "users#destroy"
+
+get '/', to: 'main#index', as: 'root'
+
+resources :users
+resources :votes
+resources :works
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,9 @@
 require "test_helper"
 
-# describe User do
-#
-# end
+describe User do
+  let :user { User.first }
+  describe "validations" do
+    it "can be created with all fields" do
+    end
+  end
+end
