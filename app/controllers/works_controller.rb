420c42d1debae21774094e55925412d6ed42e0d2
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-10 21:39:36 -0700

    works index show a table of all books, movies, albums

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,9 @@
 class WorksController < ApplicationController
   def index
-
+    @works = Work.all
+    @movies = Work.where(category: "movie")
+    @books = Work.where(category: "book")
+    @albums = Work.where(category: "album")
   end
 
   # def works_params
