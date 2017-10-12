7e68264a692dfd062155a9a5d1e52a6774cd64ce
Jessica Owens <vertige@gmail.com>
2017-10-11 22:33:09 -0700

    developing WorksController#update test

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,42 +1,39 @@
 require "test_helper"
 
 describe WorksController do
-  let(:work) { works(:book1) }
+  let(:book1) { works(:book1) }
+  let(:book2) { works(:book2) }
 
   it "should get index" do
     get works_path
     must_respond_with :success
   end
-  #
-  # it "should get new" do
-  #   get works_new_url
-  #   value(response).must_be :success?
-  # end
-  #
+
   it "should get work detail page (#show) or render a 404" do
-    get work_path(work.id)
+    get work_path(book1.id)
     must_respond_with :success
 
     get work_path("ada")
     must_respond_with :not_found
   end
-  #
-  # it "should get create" do
-  #   get works_create_url
-  #   value(response).must_be :success?
-  # end
-  #
-  it "should update the work" do
-    old_title = work.title
-    patch work_path(work.id), params: {work: {title: "#{work.title} Updated"}}
 
-    work_updated = Work.find(work.id)
-    work_updated.title.must_equal "#{old_title} Updated"
-    must_redirect_to work_path(work.id)
+  it "should update the work" do
+    old_title = book1.title
+    patch work_path(book1.id), params: {work: {title: "#{book1.title} Updated"}}
+
+    book1_updated = Work.find(book1.id)
+    book1_updated.title.must_equal "#{old_title} Updated"
+    must_redirect_to work_path(book1.id)
+
+    patch work_path(book2.id), params: {work: {title: book1.title, category: book1.category}}
+    must_respond_with :error
+
+    #wrong id
+    #data is bad and validations failed
   end
 
   it "should display an edit form" do
-    get edit_work_path(work.id)
+    get edit_work_path(book1.id)
     must_respond_with :success
 
     #TODO if no id
