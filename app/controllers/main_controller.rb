b9e7bd43367ff55f66f86d5dbd12ad2591b1bed3
sairagula <abdukhalieva@gmail.com>
2017-10-12 13:36:02 -0700

    Added media spotlight to the main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,8 @@
 class MainController < ApplicationController
   def index
-    @works = Work.all
+    @media_spotlight = Work.first
+    @top_movies = Work.where(category: "movie").limit(10)
+    @top_books = Work.where(category: "book").limit(10)
+    @top_albums = Work.where(category: "album").limit(10)
   end
 end
