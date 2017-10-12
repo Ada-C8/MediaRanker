e8caea82d6ffe15335ff5f9c8b17391b5dca45a2
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-12 15:39:58 -0700

    Any final changes to user-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :works
-  resources :users
+  resources :users, only: [:index, :show]
   root 'main#index'
 end
