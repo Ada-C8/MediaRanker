3e3bdf242efed57bbb13d87d54d6ad9ac7d5b4b1
nkiru onwuneme <generalhrh@gmail.com>
2017-10-11 22:23:37 -0700

    Added testing for user model

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,6 +1,31 @@
 require "test_helper"
 
 describe WorksController do
+  describe "index" do
+    it "returns a success status" do
+      @work = Work.new(category: "book", title: "Purple Hibiscus", creator: "Chimamanda", publication_year: 2006, description: "Riveting")
+      
+      get works_path
+
+      must_respond_with :success
+    end
+
+    it "returns a success status when there are no works" do
+      @work = Work.new(category: "book", title: "Purple Hibiscus", creator: "Chimamanda", publication_year: 2006, description: "Riveting")
+
+      Work.destroy_all
+      get works_path
+
+      must_respond_with :success
+    end
+
+    it "returns success status when given a valid work" do
+      @work = Work.new(category: "book", title: "Purple Hibiscus", creator: "Chimamanda", publication_year: 2006, description: "Riveting")
+
+      get works_path(@work)
+      must_respond_with :success
+    end
+  end
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
