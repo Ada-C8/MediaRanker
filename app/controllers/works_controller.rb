e68a1e27b427d8013df967e7fb639f3a8bbaa405
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-11 20:02:05 -0700

    Add _new_form for new page for work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -7,9 +7,14 @@ class WorksController < ApplicationController
   find_work_by_params
   end # show
 
+  def new
+    @work = Work.new
+  end # new
+
   private
   def find_work_by_params
     @work = Work.find_by(id: params[:id])
+    # TODO: uncomment this once i have clickable links
     # unless @book
     #   head :not_found
     # end
