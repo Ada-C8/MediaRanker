8d0f1011436f71b4ac658b9a02968a95debcc06b
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-12 15:05:35 -0700

    Generated Users controller and added index, login

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # root 'main#index'
 
   resources :works
+  resources :users, :only => [:index, :show]
+
+  get 'login', to: 'users#login_form', as: 'login'
+  post 'login', to: 'users#login'
 
 end
