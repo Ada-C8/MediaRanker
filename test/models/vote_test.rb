d6f2b777c490e8046050ecb0880853a94508a183
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-12 14:21:43 -0700

    set up vote model tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,15 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  let(:vote) { Vote.new(work_id: Work.first.id, user_id: User.first.id) }
 
   # it "must be valid" do
   #   value(vote).must_be :valid?
   # end
+
+  describe 'validations' do
+    it 'can be created with all validations' do
+      vote.must_be :valid?
+    end
+  end
 end
