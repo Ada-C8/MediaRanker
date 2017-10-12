21ead3c441b7eb38a3ccf3869697b24b94ae4287
kee nam <nam.keeyeon@gmail.com>
2017-10-11 18:09:57 -0700

    Adding tests to users_controller_test.rb

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,4 +7,26 @@ describe UsersController do
       must_respond_with :success
     end
   end
+
+  describe "new" do
+    it "returns success for a new work" do
+      # get new_user_path
+      # must_respond_with :success
+    end
+  end
+
+  describe "create" do
+  end
+
+  describe "show" do
+  end
+
+  describe "edit" do
+  end
+
+  describe "update" do
+  end
+
+  describe "destroy" do
+  end
 end
