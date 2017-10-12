b566708ccc0145cf4a8dc656f3a8d5166a01b6a0
Kayla Ecker <The Email Associated with Your Github Account>
2017-10-11 17:06:48 -0700

    passing initial tests for controller

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,39 +1,47 @@
 require "test_helper"
 
 describe WorksController do
-  it "should get create" do
-    get works_create_url
-    value(response).must_be :success?
+
+  it "the books index page is successful" do
+    get works_path
+    must_respond_with :success
   end
 
-  it "should get destroy" do
-    get works_destroy_url
-    value(response).must_be :success?
-  end
+end #for describe
 
-  it "should get edit" do
-    get works_edit_url
-    value(response).must_be :success?
-  end
-
-  it "should get index" do
-    get works_index_url
-    value(response).must_be :success?
-  end
-
-  it "should get new" do
-    get works_new_url
-    value(response).must_be :success?
-  end
-
-  it "should get show" do
-    get works_show_url
-    value(response).must_be :success?
-  end
-
-  it "should get update" do
-    get works_update_url
-    value(response).must_be :success?
-  end
-
-end
+# it "should get create" do
+#   get works_create_url
+#   value(response).must_be :success?
+# end
+#
+# it "should get destroy" do
+#   get works_destroy_url
+#   value(response).must_be :success?
+# end
+#
+# it "should get edit" do
+#   get works_edit_url
+#   value(response).must_be :success?
+# end
+#
+# it "should get index" do
+#   get works_index_url
+#   value(response).must_be :success?
+# end
+#
+# it "should get new" do
+#   get works_new_url
+#   value(response).must_be :success?
+# end
+#
+# it "should get show" do
+#   get works_show_url
+#   value(response).must_be :success?
+# end
+#
+# it "should get update" do
+#   get works_update_url
+#   value(response).must_be :success?
+# end
+#
+# end
