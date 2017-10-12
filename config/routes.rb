004b742c04f395002e49bd437e9040c3fa858962
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-12 14:36:39 -0700

    we did some stuff for css framework in class that did not work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index]
-  resources :works, only: [:index, :show, :edit, :update, :create, :destroy]
+  resources :works, only: [:index, :show, :new, :edit, :update, :create, :destroy]
   # resources :votes
 
   root 'main#index'
