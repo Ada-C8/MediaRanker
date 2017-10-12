136bcb4a45ecbfb3ecca2d0c8180060851836132
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-10-11 22:20:15 -0700

    created valid tests for create and update

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -14,10 +14,27 @@ describe WorksController do
    must_respond_with :redirect
    must_redirect_to works_path
  end
- it "generates a successful create work page" do
-   get create_work_path
+ it "generates a successful new work page" do
+   get new_work_path
    must_respond_with :success
  end
+ it "should be able to create a post" do
+   proc   {
+     post works_path, params: { work: {title: "Emotions", creator: "Carly Rae Jepsen"}  }
+   }.must_change 'Work.count', 1
+
+   must_respond_with :redirect
+   must_redirect_to work_path(Work.find_by(title: "Emotions").id)
+ end
+ it "should be able to successfully update a work title" do
+
+  patch work_path(works(:kesha)), params: { work: {title: "Rainbow2"}}
+
+  updated_work = Work.find(works(:kesha).id)
+  updated_work.title.must_equal "Rainbow2"
+  must_redirect_to work_path(works(:kesha).id)
+end
+
 
   # it "should get edit" do
   #   get works_edit_url
