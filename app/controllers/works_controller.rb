c7f195e5cc307fe982acba05d6d602c5f2651835
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-11 20:44:07 -0700

    Add create controller action for work with tests

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -11,6 +11,16 @@ class WorksController < ApplicationController
     @work = Work.new
   end # new
 
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to works_path
+    else
+      render :new, status: :bad_request
+    end # if/else
+  end # create
+
   private
   def find_work_by_params
     @work = Work.find_by(id: params[:id])
@@ -19,5 +29,9 @@ class WorksController < ApplicationController
       head :not_found
     end
   end
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end # work_params
 
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -15,6 +15,35 @@ describe WorksController do
     end #it "will return a success status" do
   end # describe "new" do
 
+  describe "create" do
+    it "adds the work to the DB and then redirects to the works_path when work data is valid" do
+      # Arrange
+      work_data = {
+        work: {
+          category: "book",
+          title: "HP 3",
+          creator: "J.K Rowling"
+        }
+      }
+      new_work = Work.new(work_data[:work]).must_be :valid?
+
+      start_count_work = Work.count
+
+      # Act
+      post works_path, params: work_data
+
+      # Assert
+      must_respond_with :redirect
+      must_redirect_to works_path
+
+      Work.count.must_equal start_count_work + 1
+    end # "it adds the work..."
+
+    xit "returns bad_request when the work data is invalid" do
+      # QUESTION: what is invalid data? add this test if we add validations to the Work model
+    end # it "returns bad_request when the work data is invalid" do
+  end #describe "create" do
+
   describe "show" do
     it "returns success when given a valid work id" do
       id = Work.first.id
@@ -28,4 +57,5 @@ describe WorksController do
       must_respond_with :not_found
     end # it "returns not_found ... invalid work id" do
   end #describe "show" do
+
 end
