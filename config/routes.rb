5787e59af9f4453ef68d6e17daf29f3d35cc5872
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-12 15:47:46 -0700

    Add login_form and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :works
   resources :users, only: [:index, :show]
   root 'main#index'
+
+  get 'login', to: 'users#login_form', as: 'login'
+  post 'login', to: 'users#login'
 end
