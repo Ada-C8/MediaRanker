3de1cfc1fd983f96d55554cca8b8e749cca3c2cd
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-11 20:54:59 -0700

    added uniqueness validation to work model to ensure that two works cant have same title if they are in same category

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,6 @@ class Work < ApplicationRecord
   has_many :votes, dependent: :destroy
   validates :category, presence: true
   validates :title, presence: true
+  # can't add two works of same title to same category
+  validates_uniqueness_of :title, scope: [:category]
 end
