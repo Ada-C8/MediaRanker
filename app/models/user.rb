79a5fad08838cc43ddf332b048050168f503d118
Andrea Chen <andreachen.ac@gmail.com>
2017-10-12 13:47:11 -0700

    Add tests for User model, create validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+  validates :name, presence: {messages: "Please enter a username"}
+  validates :joined, presence: {messages: "Must have a joined date"}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,12 +3,24 @@ require "test_helper"
 describe User do
 
   describe "validations" do
+    let(:user) { User.new }
 
     it "can be created with all fields" do
       u = User.new(name: "new user", joined: Date.new)
 
       u.must_be :valid?
+    end
 
+    it "is invalid without a name" do
+      result = user.valid?
+      result.must_equal false
+      user.errors.messages.must_include :name
+    end
+
+    it "is invalid without a joined date" do
+      result = user.valid?
+      result.must_equal false
+      user.errors.messages.must_include :joined
     end
 
   end
