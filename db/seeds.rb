42d515d9015e89f1ab4f5b43097aefd65d536bd8
Maria McGrew <mcgmar1@outlook.com>
2017-10-11 22:42:23 -0700

    added media spotlight to welcome page

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,9 @@ CSV.foreach(WORKS_FILE, :headers => true) do |row|
   work.id = row['id']
   work.category = row['category']
   work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row ['description']
   puts "Created work: #{work.inspect}"
   successful = work.save
   if !successful
