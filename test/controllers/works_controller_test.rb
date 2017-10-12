21db81b32a777bbbcafe4d6a34121c64dabf88f5
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-12 10:00:30 -0700

    Created tests for creating, showing index/show, and deleting a work(s)

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,39 +1,61 @@
 require "test_helper"
 
 describe WorksController do
-  it "should get index" do
-    get works_index_url
-    value(response).must_be :success?
+
+  describe "Work Index" do
+    it "should get index" do
+      get works_url
+      assert_response :success
+    end
   end
 
-  it "should get show" do
-    get works_show_url
-    value(response).must_be :success?
+
+  describe "Work Show" do
+    it "should get the show page of a single work" do
+      get work_path(works(:zipangu).id)
+      assert_response :success
+    end
+
+    it "should redirect to 404 page if user goes to a non-existent work's show page" do
+      get work_path(9999)
+
+      assert_response :not_found
+    end
   end
 
-  it "should get new" do
-    get works_new_url
-    value(response).must_be :success?
+  describe "Work New/Create" do
+    it "should create a new work" do
+      proc {
+        post works_path, params: { work: {category: "movie",
+          title: "Pokemon: The Movie",
+          creator: "Game Freak",
+          publication_year: 1997,
+          description: "First Pokemon Movie"}  }
+        }.must_change 'Work.count', 1
+        must_respond_with :redirect, "must redirect to the work's show page after creation"
+      end
   end
 
-  it "should get create" do
-    get works_create_url
-    value(response).must_be :success?
+  describe "Work Destroy" do
+    it "should delete a work" do
+      proc {
+        # run the delete verb on the post_path with a param equal to 1
+        delete work_path(works(:poodr).id)
+      }.must_change 'Work.count', -1
+
+      must_respond_with :redirect
+      must_redirect_to works_path
+    end
   end
 
-  it "should get destroy" do
-    get works_destroy_url
-    value(response).must_be :success?
+  describe "Work Edit/Update" do
+    puts "Not done!"
+    # it "should update information in a work" do
+    #   get works_edit_url
+    #   value(response).must_be :success?
+    # end
   end
 
-  it "should get edit" do
-    get works_edit_url
-    value(response).must_be :success?
-  end
 
-  it "should get update" do
-    get works_update_url
-    value(response).must_be :success?
-  end
 
 end
