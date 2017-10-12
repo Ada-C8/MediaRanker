93fc34dda96b370a198438fc58771fe38572a570
Jan Edrozo <jedrozo@gmail.com>
2017-10-11 17:29:27 -0700

    Added new and create method contents for work

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  root to: 'works#homepage'
 
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'works/index'
-
-  get 'works/show'
-
-  get 'works/edit'
-
-  get 'works/update'
-
-  get 'works/new'
-
-  get 'works/create'
-
-  get 'works/destroy'
+  resources :works
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
