bbb777e63160e1696810601310a47bec8b7b97fb
enigmagnetic <lauren.cardella@gmail.com>
2017-10-12 16:16:39 -0700

    Add vote and user models. Add user and work references to votes table.

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
-class WorkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Work do
+  let(:work) { Work.new }
+
+  describe 'validations' do
+    it "must have a title to be valid" do
+      work.valid?.must_equal false
+      work.title = "The Great Gatsby"
+      work.save
+      work.valid?.must_equal true
+    end
+  end
+
+  describe 'top_work' do
+    it "must return the work object with the highest number of votes" do
+
+    end
+  end
 end
