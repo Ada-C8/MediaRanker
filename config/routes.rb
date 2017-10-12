b7c4b0948817435fef082ade5e6fe4f58b4f1878
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-11 19:05:46 -0700

    add login routes/page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'main#index'
-  
+
   resources :works
-  resources :users, only:[:index]
+  resources :users, only:[:index, :show]
+
+  get '/login', to: 'users#login', as: 'login'
 end
