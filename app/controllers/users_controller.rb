72b5690b3a229ae3624fc0a3f3748991c687de84
Lauren <Laurenelee88@gmail.com>
2017-10-11 20:41:56 -0700

    Playing with routes and creating a vote

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,23 +27,24 @@ class UsersController < ApplicationController
 
   def login
     if session[:user_id] == nil
-      user = find_by(name: params[:name])
+      user = User.find_by(name: params[:name])
       if user
         session[:user_id] = user.id
-        flash[:success] = "Successfully logged in as existing user #{user.name}!"
+        flash[:success] = "Successfully logged in as existing user #{ user.name }"
         redirect_to root_path
       else
         if User.create(name: params[:name])
           user = User.find_by(name: params[:name])
-          flash[:success] = "Successfully created and logged in as #{params[:name]} with ID #{user.id}!"
+          session[:user_id] = user.id
+          flash[:success] = "Successfully created new user #{params[:name]} with ID #{user.id}"
           redirect_to root_path
         else
-          flash.now[:error] = "User #{params[:name]} not created."
+          flash.now[:error] = "Username #{params[:name]} was not created"
           render :login_form
         end
       end
     else
-      flash[:error] = "You are already logged in as #{User.find_by(id: session[:user_id].to_i).username}"
+      flash[:error] = "You are already logged in as #{User.find_by(id: session[:user_id].to_i).name}"
       redirect_to root_path
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  ####### main ######
   root 'main#index'
+  ####### main ######
+
+  ####### works ######
   resources :works
-  resources :users do
-    resources :votes, only: [:index, :new]
-  end
-  # resources :votes
-  post '/votes/:user_id/:work_id', to: 'votes#create', as: 'create_vote'
+  ####### works ######
+
+  ####### users and login ######
+  get 'users', to: 'users#index', as: 'users'
+  get 'users/:id', to: 'users#show', as: 'user'
 
   get 'login', to: 'users#login_form', as: 'login'
   post 'login', to: 'users#login'
   get '/logout', to: 'users#logout', as: 'logout'
-  #think about logout
+  ####### users ######
+
+  ####### votes ######
+  post '/votes/:user_id/:work_id', to: 'votes#create', as: 'create_vote'
+  ####### votes ######
 end
