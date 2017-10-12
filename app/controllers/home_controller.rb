ab6be21664e10e43e20ce56ed5386964f806d2bf
nkiru onwuneme <generalhrh@gmail.com>
2017-10-11 22:24:42 -0700

    Add home page, custom route and home index

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def index
+    @media_spotlight = Work.first
+
+    @top_movies = Work.where(category: "movie").limit(10)
+    @top_albums = Work.where(category: "album").limit(10)
+    @top_books = Work.where(category: "book").limit(10)
+  end
+end
