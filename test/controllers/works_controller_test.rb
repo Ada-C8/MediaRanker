ca0c661fcbac5b6c253b69e5c80c4bc3ad7fdfc8
Shaunna <skwiens@gmail.com>
2017-10-11 22:13:41 -0700

    update feature and tests added

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -36,4 +36,40 @@ describe WorksController do
     must_respond_with :success
   end
 
+  it "should be able to successfully update a title of work" do
+    put work_path( works(:album)), params: { work: { title: "Updated Title"} }
+    updated_work = Work.find( works(:album).id )
+    updated_work.title.must_equal "Updated Title"
+    must_redirect_to works_path
+  end
+
+  it "should be able to successfully update a category of a work" do
+    put work_path( works(:album)), params: { work: { category: "book"} }
+    updated_work = Work.find( works(:album).id )
+    updated_work.category.must_equal "book"
+    must_redirect_to works_path
+
+  end
+
+  it "should be able to successfully update a creator of a work" do
+    put work_path( works(:album)), params: { work: { creator: "Nathan K"} }
+    updated_work = Work.find( works(:album).id )
+    updated_work.creator.must_equal "Nathan K"
+    must_redirect_to works_path
+  end
+
+  it "should be able to successfully update the publication year of a work" do
+    put work_path( works(:album)), params: { work: { publication_year: 2005} }
+    updated_work = Work.find( works(:album).id )
+    updated_work.publication_year.must_equal 2005
+    must_redirect_to works_path
+  end
+
+  it "should be able to update the description of a work" do
+    put work_path( works(:album)), params: { work: {description: "new description"} }
+    updated_work = Work.find( works(:album).id )
+    updated_work.description.must_equal "new description"
+    must_redirect_to works_path
+  end
+
 end
