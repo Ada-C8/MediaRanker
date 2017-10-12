8becc0c549582c5e4e7438b3b1dcf8cc41209bd7
Andrea Chen <andreachen.ac@gmail.com>
2017-10-11 22:01:31 -0700

    Add test for MainController#index. Add link to BooksController#show page

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -4,4 +4,11 @@ describe MainController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+  describe "index" do
+    it "always returns a success status" do
+      get root_path
+      must_respond_with :success
+    end
+  end
+
 end
