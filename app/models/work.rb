28c190bad6cb22851d08cc81fb0ccd6eb9a1f1ed
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-12 11:23:40 -0700

    add fountation css file and followed along with Dans nav bar example

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,11 +5,20 @@ class Work < ApplicationRecord
 
   #relationships
   has_many :votes
+
   def self.popular_works(category)
     top_works = Work.all.where(category: category).sort_by{|work| -work.votes.count}[0..9]
     return top_works
   end
 
+  def sorted_works
+    all_works_sorted = Work.all.sort_by{|work| -work.votes.count}
+    return all_works_sorted
+  end
+
+  def top_work
+
+  end
   def self.top_works(category)
     top_works = Work.where(category: category).joins(:votes).group(:id).order("count(votes.id) DESC").limit(10)
     return top_works
