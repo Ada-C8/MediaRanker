739cce9484bbd965fbb79d6e06a2077c8718bb56
Lauren <Laurenelee88@gmail.com>
2017-10-12 15:00:42 -0700

    moved logic to model

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,14 +1,15 @@
 class MainController < ApplicationController
   def index
-    # MOVE TO MODEL!
-    # @albums = Work.popular_works("albums")
-    # @movies = Work.popular_works("movies")
-    # @books = Work.popular_works("books")
+
+    # why not working?
+    @albums = Work.popular_works("album")
+    @movies = Work.popular_works("movie")
+    @books = Work.popular_works("book")
     @top =  Work.top
     # @votes = Vote.where(work_id: params[:id])
 
-    @movies = Work.where(category: "movie").sort_by{ |work| -work.votes.count}[0..9]
-    @books = Work.where(category: "book").sort_by{ |work| -work.votes.count}[0..9]
-    @albums = Work.where(category: "album").sort_by{ |work| -work.votes.count}[0..9]
+    # @movies = Work.where(category: "movie").sort_by{ |work| -work.votes.count}[0..9]
+    # @books = Work.where(category: "book").sort_by{ |work| -work.votes.count}[0..9]
+    # @albums = Work.where(category: "album").sort_by{ |work| -work.votes.count}[0..9]
   end
 end
