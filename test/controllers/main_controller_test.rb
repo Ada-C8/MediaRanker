ad50847f712d58b4d0057a214c38f0c2ea6dcd51
sairagula <abdukhalieva@gmail.com>
2017-10-12 14:16:12 -0700

    main controller is tested

---
+++ test/controllers/main_controller_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+describe MainController do
+  describe "index" do
+    it "returns a success status for all works" do
+      get root_path
+      must_respond_with :success
+    end
+
+    it "returns a success status when there are no works" do
+      Work.destroy_all
+      get root_path
+      must_respond_with :success
+    end
+
+  end
+end
