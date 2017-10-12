e6f8f2c2b87c32b5157a62d55fe406a9a933ae5b
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-11 18:28:05 -0700

    added defs in works controller. Fixed issue in Works Model def for top ten. Fixed issue in partial and works index html page. Dried up the code

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,8 +3,58 @@ class WorksController < ApplicationController
     @works = Work.all
   end
 
-  # def show
-  #   @work = Work.find(params[:id])
-  # end
+  def show
+    find_work_by_id
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to works_path
+      return
+    else
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+  def edit
+    find_work_by_id
+  end
+
+  def update
+    if find_work_by_id
+      @work.update_attributes(work_params)
+      if @work.save
+        redirect_to work_path(@work)
+        return
+      else
+        render edit, status: :bad_request
+        return
+      end
+    end
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:title, :category, :creator, :publication_year, :description)
+  end
+
+  def find_work_by_id
+    @work = work.find(params[:id])
+    unless @work
+      head :not_found
+    end
+  end
 
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,10 @@
 class Work < ApplicationRecord
   has_many :votes
+
+  def self.top_ten
+    top_ten = all.sort_by do |work|
+      work.votes.count
+    end
+    return top_ten[0...10]
+  end
 end
