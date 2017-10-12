ec5fd3714fe6e5d102acd4944715a7e236b7e255
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-12 15:57:56 -0700

    Add some login stuff to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,19 @@ class UsersController < ApplicationController
     find_user_by_params_id
   end # show
 
+  def login
+    # set the username
+    username = params[:user][:name]
+
+    # check if the user already exists
+    user = User.find_by(name: name)
+    if user
+      session[:logged_in_user] = user.id
+      redirect_to root_path
+    else
+      # TODO: add logic here to create a new user if the user doesn't already exist
+  end
+
   private
   def user_params
     return params.require(:user).permit(:name)
