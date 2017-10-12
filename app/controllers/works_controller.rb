c838a3071fcf548171d5b5a63ce5aee2b5eec37e
sairagula <abdukhalieva@gmail.com>
2017-10-11 19:56:53 -0700

    detail pages added for each work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,29 +3,53 @@ class WorksController < ApplicationController
     @works = Work.all
   end
 
-  # def show
-  #
-  # end
-  #
+  def show
+    @work = Work.find(params[:id])
+  end
+
   # def new
-  #
+  #   @work = Work.new
   # end
   #
   # def create
-  #
+  #   @work = Work.new(work_params)
+  #   if @work.save
+  #     redirect_to works_path
+  #   else
+  #     render :new
+  #   end
   # end
-  #
+
   # def edit
-  #
+  #   @work = Work.find(params[:id])
   # end
-  #
+
   # def update
+  #   @work = Work.find(params[:id])
+  #   result = @work.update_attributes(work_params)
   #
+  #   if result
+  #     redirect_to work_path(params[:id])
+  #   else
+  #     render :edit
+  #   end
   # end
-  #
+
   # def destroy
-  #
+  #   work = Work.find(params[:id])
+  #   result = work.destroy
+  #   if result
+  #     redirect_to works_path
+  #   else
+  #     redirect_to work_path(params[:id])
+  #   end
   # end
-  
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:title, :creator, :publication_year, :category, :description)
+  end
+
 
 end
