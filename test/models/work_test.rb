042364cc8ba4eea92cbd095d981f46b84cb4d032
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-10-12 14:10:26 -0700

    Add initial test for model validations

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,33 @@
 require "test_helper"
 
-describe Work do
-  let(:work) { Work.new }
+#test any lines of code you have added to the code
 
-  it "must be valid" do
-    value(work).must_be :valid?
+
+describe Work do
+  describe "validations" do
+    it "can be created with all fields" do
+      w = Work.new(category: "album", title: "test")
+
+      w.must_be :valid?
+    end
+
+    it "requires a title" do
+      w = Work.new(category: "album")
+      is_valid = w.valid?
+      is_valid.must_equal false
+      w.errors.messages.must_include :title
+    end
+
+    it "requires a category" do
+      w = Work.new(title: "test")
+      is_valid = w.valid?
+      is_valid.must_equal false
+      w.errors.messages.must_include :category
+    end
+
   end
+
+  describe "relations" do
+  end
+
 end
