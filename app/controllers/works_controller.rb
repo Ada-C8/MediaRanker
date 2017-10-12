6695a4fc2f5d956539322f74c5e2809dc75e0f2e
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-11 19:24:02 -0700

    HTML for show page for work, still need to add links to edit, delete, and a upvote button

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,17 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end # index
+
+  def show
+  find_work_by_params
+  end # show
+
+  private
+  def find_work_by_params
+    @work = Work.find_by(id: params[:id])
+    # unless @book
+    #   head :not_found
+    # end
+  end
+
 end
