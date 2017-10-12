695fde59738f903b978ce5f8851e166ae0cfd0e7
VirtualLindsey <tapircode@gmail.com>
2017-10-12 15:54:34 -0700

    adding resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   get 'user/index'
 
+  resources :users, :books, :movies, :albums
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
