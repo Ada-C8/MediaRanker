4a66d3468a2c64a71c6debcb774519cadd3315a4
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-10-13 15:24:48 -0700

    completed homepage. added spotlight with link_to

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,8 @@ class HomeController < ApplicationController
     @books = Work.where(category: "book").limit(10)
     @movies = Work.where(category: "movie").limit(10)
     @albums = Work.where(category: "album").limit(10)
+
+    @spotlight = Vote.media_spotlight
   end
 
 
