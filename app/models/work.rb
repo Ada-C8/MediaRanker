511a3d651619db92d8f722fb9317a9969ccfbaf3
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-13 13:15:19 -0700

    Created validation for publication_year to be a valid number and set default value for creator to be 'Anonymous' if blank. Formatting edits.

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,23 @@
 class Work < ApplicationRecord
-  validates :category, presence: true
-  validates :title, presence: true, uniqueness: { scope: :category,
-    message: "This work already exists" }
+  validates :category,
+            presence: true
+  validates :title,
+            presence: true,
+            uniqueness: {
+                          scope: :category,
+                          message: "This work already exists"
+                        }
+  validates :publication_year,
+            allow_blank: true,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 1000
+            }
+
+  after_initialize :set_default
+  def set_default
+    if self.creator.blank?
+      self.creator = "Anonymous"
+    end
+  end
 end
