321f13daae69b107bc47728dbbf69e585ac7cd09
Canaan West <canaan.west@gmail.com>
2017-10-12 18:36:22 -0700

    users basic tests written and red

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,39 +1,42 @@
-# require "test_helper"
-#
-# describe UsersController do
-#   it "should get index" do
-#     get users_index_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get show" do
-#     get users_show_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get new" do
-#     get users_new_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get create" do
-#     get users_create_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get destroy" do
-#     get users_destroy_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get edit" do
-#     get users_edit_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get update" do
-#     get users_update_url
-#     value(response).must_be :success?
-#   end
-#
-# end
+require "test_helper"
+
+describe UsersController do
+  it "should get index" do
+    get users_path
+    must_respond_with :success
+  end
+
+  it "should get show" do
+    get user_path(users(:west).id)
+    must_respond_with :success
+  end
+
+  it "should get new" do
+    get new_user_path
+    must_respond_with :success
+  end
+
+  it "should get create" do
+    post users_path, params: {works: {name: "new user"}}
+    must_respond_with :redirect
+    must_redirect_to :users_path
+  end
+
+  it "should get destroy" do
+    delete user_path(users(:west).id)
+    must_respond_with :redirect
+    must_redirect_to :users_path
+  end
+
+  it "should get edit" do
+    get edit_user_path(users(:west).id)
+    must_respond_with :success
+  end
+
+  it "should get update" do
+    patch user_path(users(:west).id), params: {works: {name: "Update User"}}
+    must_respond_with :redirect
+    must_redirect_to :users_path
+  end
+
+end
