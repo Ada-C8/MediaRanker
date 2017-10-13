b78adc8b9f9e0fc6be592b1c599c6fcefe684a74
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-13 08:47:20 -0700

    Removed create from users controller, test file, and resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :works
-  resources :users, except: [:edit, :update, :delete]
-  resources :votes,  only: [:new, :create]
+  resources :users, only: [:index, :create, :show,]
+
+  resources :votes, only: [:new, :create]
   root 'home#index'
 end
