43ed75fe4ac804a8e61c1e56dc5fded69b9db0ee
Maria McGrew <mcgmar1@outlook.com>
2017-10-13 15:06:28 -0700

    added method to application control to show which user is logged in, created link to logged in user's show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :logged_user
+
+  private
+
+  def logged_user
+    @user = User.find_by(id: session[:user_id].to_i)
+  end
 end
