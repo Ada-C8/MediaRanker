36acda7154d0093a10138fb2a5a62720acf64c90
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-13 09:47:45 -0700

    Add some logic to vote testing

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe VotesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  let(:vote) { Vote.new }
+  let(:user) { users(:one) }
+  let(:album) { works(:album) }
+  let(:book) { works(:book) }
+
+  it "must create a vote with valid parameters when the user is logged in" do
+    skip
+    # need to test logging in first
+    # proc { post create_vote_path(user.id, album.id) }.must_change 'Vote.count', +1
+    # must_respond_with :redirect
+  end
+
+  it "must not create a vote with invalid parameters" do
+    skip
+  end
 end
