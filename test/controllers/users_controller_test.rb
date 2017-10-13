20b46e8259288819a8588a3b393a99adf6d6b02f
Kayla Ecker <The Email Associated with Your Github Account>
2017-10-13 13:53:08 -0700

    more tests added successfully

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,9 +6,20 @@ describe UsersController do
     value(response).must_be :success?
   end
 
+  it "index page successful" do
+    get works_path
+    must_respond_with :success
+  end
+
   it "should get new" do
     get users_new_url
     value(response).must_be :success?
+  end
+
+
+  it "loads new_work" do
+    get new_work_path
+    must_respond_with :success
   end
 
   it "should get show" do
@@ -19,6 +30,26 @@ describe UsersController do
   it "should get create" do
     get users_create_url
     value(response).must_be :success?
+  end
+
+  it "creates & saves a new work" do
+    proc {
+      post works_path, params: { work: {category: "album", title: "Red", creator: "Lindsey", publication_year: "1952", description: "Check"}}
+    }.must_change 'Work.count', 1
+
+    must_respond_with :redirect
+    must_redirect_to works_path
+  end
+
+  it "creates a new work in the database" do
+    proc {
+      post works_path, params: { work: {category: "movie", title: "A Beautiful Mind", creator: "Someone", publication_year: 1999}}
+    }.must_change 'Work.count', 1
+
+    work = Work.find_by(title: "A Beautiful Mind")
+    work.creator.must_equal "Someone"
+    work.publication_year.must_equal 1999
+
   end
 
   it "should get edit" do
@@ -36,4 +67,4 @@ describe UsersController do
     value(response).must_be :success?
   end
 
-end
+end #describe do
