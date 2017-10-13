0516fa404ce0272ff49883f7c5525108181b7209
Tanja Stroble <plusdeltah@gmail.com>
2017-10-13 12:53:12 -0700

    setting spotlight aside to work on tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @top_movies = Work.where(category: 'movie').order(total_votes: :desc).limit(10)
-    @top_books = Work.where(category: 'book').limit(10).order(total_votes: :desc).limit(10)
-    @top_albums = Work.where(category: 'album').limit(10).order(total_votes: :desc).limit(10)
-    @spotlight = Work.spotlight
+    @top_movies = Work.where(category: 'movie').limit(10)
+    @top_books = Work.where(category: 'book').limit(10)
+    @top_albums = Work.where(category: 'album').limit(10)
+    # @spotlight = Work.all.max_by { |work| work.votes.length }
   end
 
 end
