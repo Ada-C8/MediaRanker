0d57a121d3996307f46cfb71cd86b1bbb09a6e95
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-13 13:06:58 -0700

    Add new model vote and set relationships between models

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,5 @@
 class Work < ApplicationRecord
+  has_many :votes
 
   validates :title, presence: { message: "Please enter a title of your entry" }
   validates :category, presence: { message: "Please select a category" }
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010224616) do
+ActiveRecord::Schema.define(version: 20171013200008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "work_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
