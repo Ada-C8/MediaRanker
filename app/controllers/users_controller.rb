c01a8f48000f7c7480fd6972558c696fa23717ed
Julia <julia.meier@gmail.com>
2017-10-13 15:26:27 -0700

    started setup of show page. did not manage to complete show method or testing, nor create table. will need to go back to this

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,10 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id].to_i)
+    @user = User.find_by(id: params[:id])
+    @user_votes = Vote.where(user_id: params[:id])
+    @works = Work.all
+
   end
 
   def edit
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def convert_date(date)
+    
+  end
+
+
 end
