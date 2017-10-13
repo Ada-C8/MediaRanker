a23577aef771169981a04471e6b49c26b639404f
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-13 12:41:43 -0700

    renames user new view to login_form and adds a form tag for user login

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :works
-  resources :users, only: [:index, :create, :show, :new]
+  resources :users, only: [:index, :create, :show]
 
   resources :votes, only: [:new, :create]
   root 'home#index'
