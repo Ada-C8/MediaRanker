dbfb99092dc7558795a4e6018242ec9ba929d076
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-12 19:31:39 -0700

    Adds users controller, index, and show view

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+describe UsersController do
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    
+  end
+end
