77609f19a1e81bddc951cb9872489d054f977251
Jessica Owens <vertige@gmail.com>
2017-10-13 10:06:46 -0700

    wrote and passes user validation tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,28 @@ describe User do
       user1.votes.length.must_equal 1
     end
   end #Relationships
+
+  describe "validations" do
+    it "must have a username to be valid" do
+      #false
+      user.valid?.must_equal false
+      user.errors[:username].must_include "can't be blank"
+
+      #true
+      user.username = "A Name"
+      user.valid?.must_equal true
+    end
+
+    it "must have a unique username to be valid" do
+      # false
+      user.username = user1.username
+      user.valid?.must_equal false
+
+      #true
+      user.username = "#{user1.username} Changed"
+      user.valid?.must_equal true
+    end
+
+  end # validations set
+
 end
