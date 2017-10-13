f1a6538604df900749360286e157b162fb5d3115
Kayla Ecker <The Email Associated with Your Github Account>
2017-10-13 13:40:57 -0700

    working beginning tests for work

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,8 +2,21 @@ require "test_helper"
 
 describe Work do
   let(:work) { Work.new }
+  let(:book) { works(:book) }
 
-  it "must be valid" do
-    value(work).must_be :valid?
-  end
+  # it "must be valid" do
+  #   value(work).must_be :valid?
+  # end
+
+  it "must have a title" do
+  book.valid?.must_equal true
+
+  book.category = nil
+  book.title = nil
+  book.save
+
+  book.errors.keys.must_include :title
+  book.errors.keys.must_include :category
+
+end
 end
