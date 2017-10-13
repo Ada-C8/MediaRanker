5fa81b8752138a2efb7e5f3499a06252e51c8d51
Tanja Stroble <plusdeltah@gmail.com>
2017-10-13 12:33:02 -0700

    added votes count migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011181008) do
+ActiveRecord::Schema.define(version: 20171013185000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20171011181008) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "vote_count", default: 0
   end
 
   add_foreign_key "votes", "users"
