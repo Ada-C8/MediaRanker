b50513a1cb06f547c722c15b98be24e1bd3aee36
IsabelDePapel <isuchanek@gmail.com>
2017-10-13 15:15:22 -0700

    update flash messages in works controller to work partial

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,19 +1,11 @@
 class WorksController < ApplicationController
   def home
-    # @media = media_hash(top_ten: true)
-    # @media = Work.works_by_type_hash
-    # @media = { "albums": Work.works_of_type("album", top_ten: true),
-    #             "books": Work.works_of_type("book", top_ten: true),
-    #             "movies": Work.works_of_type("movie", top_ten: true) }
     media_hash = Work.works_by_type_hash
     @media = Work.top_ten(media_hash)
-
-    # works = Work.left_outer_joins(:votes).distinct.select('works.*, COUNT(votes.*) AS num_votes').group('works.id').order('num_votes DESC')
     @spotlight = Work.spotlight
   end
 
   def index
-    #@works = Work.left_outer_joins(:votes).distinct.select('works.*, COUNT(votes.*) AS num_votes').group('works.id').order('num_votes DESC')
     @media = Work.works_by_type_hash
   end
 
@@ -33,13 +25,16 @@ class WorksController < ApplicationController
     @work = Work.new(work_params)
 
     if @work.save
-      flash[:success] = "Successfully created #{@work.category.name} #{@work.id}"
+      flash[:status] = :success
+      flash[:message] = "Successfully created #{@work.category.name} #{@work.id}"
       redirect_to work_path(@work.id)
     else
       # puts "error message"  DEBUGGING
       # puts @work.errors.inspect
-      flash.now[:error] = "A problem occurred: Could not create #{@work.category.name}"
-      render :new
+      flash.now[:status] = :failure
+      flash.now[:message] = "A problem occurred: Could not create #{@work.category.name}"
+      flash.now[:details] = @work.errors.messages
+      render :new, status: :bad_request
     end
   end
 
@@ -57,11 +52,14 @@ class WorksController < ApplicationController
     end
 
     if @work.update_attributes work_params
-      flash[:success] = "Successfully updated #{@work.category.name}"
+      flash[:status] = :success
+      flash[:message] = "Successfully updated #{@work.category.name}"
       redirect_to root_path
     else
-      flash.now[:error] = "A problem occurred: Could not update #{@work.category.name}"
-      render :edit
+      flash.now[:status] = :failure
+      flash.now[:message] = "A problem occurred: Could not update #{@work.category.name}"
+      flash.now[:details] = @work.errors.messages
+      render :edit, status: :bad_request
     end
 
   end
@@ -71,10 +69,15 @@ class WorksController < ApplicationController
     @work.destroy
 
     if @work.destroyed?
-      flash[:success] = "Successfully destroyed #{@work.category.name} #{@work.id}"
+      flash[:status] = :success
+      flash[:message] = "Successfully destroyed #{@work.category.name} #{@work.id}"
       redirect_to root_path
     else
-      flash.now[:error] = "Unable to delete #{@work.category.name} #{@work.id}"
+      flash[:status] = :failure
+      flash[:message] = "Unable to delete #{@work.category.name} #{@work.id}"
+      flash[:details] = @work.errors.messages
+
+      redirect_back fallback_location: root_path
     end
   end
 
@@ -82,20 +85,6 @@ class WorksController < ApplicationController
 
   def work_params
     return params.require(:work).permit(:title, :creator, :publication_year, :description, :category_id)
-  end
-
-  def media_hash(top_ten = true)
-    albums = Work.left_outer_joins(:votes).where(category_id: Category.find_by(name: "album")).distinct.select('works.*, COUNT(votes.*) AS num_votes').group('works.id').order('num_votes DESC')
-
-    movies = Work.left_outer_joins(:votes).where(category_id: Category.find_by(name: "movie")).distinct.select('works.*, COUNT(votes.*) AS num_votes').group('works.id').order('num_votes DESC')
-
-    books = Work.left_outer_joins(:votes).where(category_id: Category.find_by(name: "book")).distinct.select('works.*, COUNT(votes.*) AS num_votes').group('works.id').order('num_votes DESC')
-
-    if top_ten
-      return {"albums": albums[0...10], "movies": movies[0...10], "books": books[0...10]}
-    else
-      return {"albums": albums, "movies": movies, "books": books}
-    end
   end
 
 end
