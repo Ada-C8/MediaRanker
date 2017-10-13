738a50d795686344a25893e9ee2aa9c48ee8fa81
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-10-12 20:16:13 -0700

    controller test done

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 describe WorksController do
+  it "generates a successful home page" do
+    get home_path
+    must_respond_with :success
+  end
  it "generates a successful works index page" do
    get works_path
    must_respond_with :success
@@ -18,7 +22,7 @@ describe WorksController do
    get new_work_path
    must_respond_with :success
  end
- it "should be able to create a post" do
+ it "should be able to create a work" do
    proc   {
      post works_path, params: { work: {title: "Emotions", creator: "Carly Rae Jepsen"}  }
    }.must_change 'Work.count', 1
@@ -27,12 +31,18 @@ describe WorksController do
    must_redirect_to work_path(Work.find_by(title: "Emotions").id)
  end
  it "should be able to successfully update a work title" do
-
   patch work_path(works(:kesha)), params: { work: {title: "Rainbow2"}}
-
   updated_work = Work.find(works(:kesha).id)
   updated_work.title.must_equal "Rainbow2"
   must_redirect_to work_path(works(:kesha).id)
+end
+it "should be able to delete a work" do
+  proc   {
+    delete work_path(works(:kesha).id)
+  }.must_change 'Work.count', -1
+
+  must_respond_with :redirect
+  must_redirect_to root_path
 end
 
 
