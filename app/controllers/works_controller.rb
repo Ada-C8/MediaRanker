6a96b71f98d24ceacd2bfc0d2b495a4797c06ec8
Diane Lenertz <lenertzd@grinnell.edu>
2017-10-12 22:41:57 -0700

    add necessary view pages and html

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -19,8 +19,8 @@ class WorksController < ApplicationController
   end
 
   def create
-    strong_params = work_params
-    @work = Work.new(strong_params)
+    # strong_params = work_params
+    @work = Work.new(params)
     @work.save
 
     if @work.save
@@ -31,9 +31,9 @@ class WorksController < ApplicationController
   end
 
   def update
-    strong_params = work_params
+    # strong_params = work_params
     @work = Work.find(params[:id])
-    @work.update_attributes(strong_params)
+    @work.update_attributes(params)
     @work.save
     redirect_to works_path
   end
@@ -44,10 +44,10 @@ class WorksController < ApplicationController
     redirect_to works_path
   end
 
-private
-def work_params
-  return params.require(:work).permit(:title, :category)
-end
+# private
+# def work_params
+#   return params.require(:work).permit(:title, :category, :creator, :pub_year)
+# end
 
 
 end
