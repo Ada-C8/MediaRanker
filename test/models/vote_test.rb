dbb4e2f5b4a0c2ba6bbf36bdd18bcd510c962df1
nkiru onwuneme <generalhrh@gmail.com>
2017-10-13 10:10:07 -0700

    Tested belongs_to relationship

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -2,37 +2,31 @@ require "test_helper"
 
 describe Vote do
   describe "validations" do
-    let :user { User.create!(name: "bob") }
-    let :work { Work.create!(category: "album", title: "purple rain", creator: "prince", publication_year: 1984, description: "one of the best albums of the year") }
-    # it "requires a user id" do
-    #   user_id = 1
-    #   work_id = 1
-    #   v = Vote.new(user_id, work_id)
-    #   result = v.valid?
-    #   result.must_equal true
-    # end
+    # let :user { User.create!(name: "bob") }
+    # let :work { Work.create!(category: "album", title: "purple rain", creator: "prince", publication_year: 1984) }
 
     # it "can be created with all fields" do
-    #   u = User.create!
-    #   w = Work.create!
     #
-    #   v = Vote.new(user_id: u, work_id: w)
+    #
+    #   v = Vote.new(user: user, work: work)
     #   result = v.valid?
     #   result.must_equal true
     # end
 
     it "can be created with all fields" do
-      # u = User.new({name: "cheetara"})
-      # w = Work.new({category: "album", title: "purple rain", creator: "prince", publication_year: 1984, description: "one of the best albums of the year"})
+    u = users(:cheetara)
+    w = works(:awesome_movie)
 
-      v = Vote.new(user: user, work: work)
-      result = v.valid?
-      result.must_equal true
+    v = Vote.new(user_id: u.id, work_id: w.id)
+
+    result = v.valid?
+    result.must_equal true
     end
 
+
     it "requires a work id" do
-      u = User.new(name: "cheetara")
-      v = Vote.new(user_id: u)
+      u = users(:cheetara)
+      v = Vote.new(user_id: u.id)
       result = v.valid?
       result.must_equal false
 
@@ -40,8 +34,8 @@ describe Vote do
     end
 
     it "requires a user id" do
-      w = Work.new(category: "album", title: "purple rain", creator: "prince", publication_year: 1984, description: "one of the best albums of the year")
-      v = Vote.new(work_id: w)
+      w = works(:awesome_movie)
+      v = Vote.new(work_id: w.id)
       result = v.valid?
       result.must_equal false
 
@@ -57,21 +51,18 @@ describe Vote do
 
   describe "relationships" do
     it "has a user" do
-      v = Vote.new(user_id: user.id, work_id: work.id)
-      u = User.new(name: "cheetara")
+      u = users(:cheetara)
+      w = works(:awesome_movie)
 
-      v.must_respond_to :user
-      v.user.must_equal u
+      v = Vote.new(user_id: u.id, work_id: w.id)
+
+      v.must_respond_to :user_id
       v.user_id.must_equal u.id
+      # v.user_id.must_be_kind_of String
+      # v.user.id.must_equal user.id
+
+      v.must_respond_to :work_id
+      v.work_id.must_equal w.id
     end
-
-    it "has a work" do
-      v = Vote.new
-
-      v.must_respond_to :work
-      v.work.must_equal work
-      v.work_id.must_equal work.id
-    end
-  end
-
+   end
 end
