5b03d3030d146fd2d1b40c937556f81df347e554
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-10-13 09:16:29 -0700

    re-added votes model method, votes controller test, and votes yml file.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,17 @@
 class Vote < ApplicationRecord
   belongs_to :User
   belongs_to :work
+
+  def media_spotlight
+    # if there is a tie, the first key is used(the oldest record or lowest id)
+
+    # returns should return a hash that has work_id as a key and associated count as the value.
+    hash = Vote.group(:work_id).count
+
+   # using max_by to find the key value pair to find the max value
+    media_max = hash.max_by{ |k,v| v}
+
+    # media max is an array. index 0 is the key(id) index 1 is the value(amount of votes)
+    return Work.find(media_max[0])
+  end
 end
--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,39 +1,12 @@
 require "test_helper"
 
 describe VotesController do
-  # it "should get index" do
-  #   get votes_index_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get show" do
-  #   get votes_show_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get create" do
-  #   get votes_create_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get update" do
-  #   get votes_update_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get destroy" do
-  #   get votes_destroy_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get edit" do
-  #   get votes_edit_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get new" do
-  #   get votes_new_url
-  #   value(response).must_be :success?
-  # end
+  it "Should be able to CREATE a new vote" do
+    proc  {
+      post votes_path, params: {votes: {user: :one, work: :one}} }.must_change 'votes.count', 1
+
+      must_respond_with :redirect
+      must_redirect_to work_path(@work.id)
+  end
 
 end
