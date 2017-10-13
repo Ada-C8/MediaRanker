1435937f117845a1a66b62b3be67775f4ba86837
nkiru onwuneme <generalhrh@gmail.com>
2017-10-12 18:25:38 -0700

    Added tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 describe User do
-  describe 'validations' do
+  describe "validations" do
     it "is valid with a name" do
       u = User.new(name: "bob")
       result = u.valid?
@@ -15,11 +15,15 @@ describe User do
     end
   end
 
-  describe 'relationships' do
+  describe "relations" do
     it "must have votes" do
       u = User.new(name: "bob")
       u.votes.count.must_equal 0
     end
 
   end
+
+  # describe "custom method" do
+  #
+  # end
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,48 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  describe "validations" do
+    # it "requires a user id" do
+    #   user_id = 1
+    #   work_id = 1
+    #   v = Vote.new(user_id, work_id)
+    #   result = v.valid?
+    #   result.must_equal true
+    # end
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+    it "can be created with all fields" do
+      u = User.create!
+      w = Work.create!
+
+      v = Vote.new(user_id: u, work_id: w)
+      result = v.valid?
+      result.must_equal true
+    end
+
+    # it "requires a work_id" do
+    #   v = Vote.new(work_id: 1)
+    #   result = v.valid?
+    #   result.must_equal true
+    # end
+    #
+    # it "requires a user id" do
+    #   v = Vote.new
+    #   result = v.valid?
+    #   result.must_equal false
+    # end
+    #
+    # it "requires a work_id" do
+    #   v = Vote.new
+    #   result = v.valid?
+    #   result.must_equal false
+    # end
   end
+
+  # describe "relations" do
+  #   it "must have user_id and work_id" do
+  #     u = User.new(name: "bob")
+  #     u.votes.count.must_equal 0
+  #   end
+  # end
+
 end
