c6e8a777981d23e4cd7dcaccb25772dd3c478e9e
Canaan West <canaan.west@gmail.com>
2017-10-12 18:13:08 -0700

    updated controller tests for work

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,39 +1,47 @@
 require "test_helper"
 
 describe WorksController do
-  it "should get index" do
-    get works_index_url
-    value(response).must_be :success?
+  it "should display a list of all works" do
+    get works_path
+    must_respond_with :success
   end
 
-  it "should get show" do
-    get works_show_url
-    value(response).must_be :success?
+  it "should be able to show an individual work's page" do
+    get work_path(works(:greys).id)
+    must_respond_with :success
   end
 
-  it "should get create" do
-    get works_create_url
-    value(response).must_be :success?
+  it "should be produce a form to make a new page" do
+    get new_work_path
+    must_respond_with :success
   end
 
-  it "should get new" do
-    get works_new_url
-    value(response).must_be :success?
+  it "should be able to create a new work" do
+    post works_path, params: {work: {title: "New Title", category: "movie"} }
+    must_respond_with :redirect
+    must_redirect_to works_path
   end
 
-  it "should get destroy" do
-    get works_destroy_url
-    value(response).must_be :success?
+  it "should produce an edit form" do
+    get edit_work_path(works(:greys).id)
+    must_respond_with :success
   end
 
-  it "should get edit" do
-    get works_edit_url
-    value(response).must_be :success?
+  it "should update a work through the update path" do
+    patch work_path(works(:greys).id), params: {work: {title: "Update this work", category: "movie"}}
+    update_work = Work.find(works(:greys).id)
+    update_work.title.must_equal "Update this work"
+    must_redirect_to works_path
   end
 
-  it "should get update" do
-    get works_update_url
-    value(response).must_be :success?
+  it "should be able to destroy a work" do
+    delete work_path(works(:greys).id)
+    must_respond_with :redirect
+    must_redirect_to works_path
   end
 
+  it "root_path shoudl direct to the home page" do
+    get top_works_path
+    must_respond_with :success
+  end
 end
