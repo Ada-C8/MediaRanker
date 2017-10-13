1dff1e46dc1d1c5b63c5fdb7e51e2fb20bc2044a
Jessica Owens <vertige@gmail.com>
2017-10-12 22:15:51 -0700

    Wrote basic relations tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -2,8 +2,33 @@ require "test_helper"
 
 describe Vote do
   let(:vote) { Vote.new }
+  let(:vote1) { votes(:vote1) }
+  let(:user3) { users(:user3) }
+  let(:movie) { works(:movie1) }
 
-  it "must be valid" do
-    value(vote).must_be :valid?
-  end
+  describe "relationships" do
+    it 'can set the user through "user"' do
+      vote1.user = user3
+
+      vote1.user_id.must_equal user3.id
+    end
+
+    it 'can set the user through "user_id"' do
+      vote1.user_id = user3.id
+
+      vote1.user.must_equal user3
+    end
+
+    it 'can set the work through "work"' do
+      vote1.work = movie
+
+      vote1.work_id.must_equal movie.id
+    end
+
+    it 'can set the work through "work_id"' do
+      vote1.work_id = movie.id
+
+      vote1.work.must_equal movie
+    end
+  end #Relationships
 end
