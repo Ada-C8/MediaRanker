3bd7de8d0d005465259a17d9912eb74f1cff45f9
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-13 11:33:09 -0700

    changed work model to require title be unique inside a category

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
   validates :category, presence: true
-  validates :title, presence: true, uniqueness: { scope: :creator,
+  validates :title, presence: true, uniqueness: { scope: :category,
     message: "This work already exists" }
 end
