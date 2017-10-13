5e48c61b9ade618de79cebcaf2e2c46a6842c5d8
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-13 09:02:29 -0700

    Reoved date column from the votes table and changed votes show page to show the date of the vote

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010235657) do
+ActiveRecord::Schema.define(version: 20171013155941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20171010235657) do
 
   create_table "votes", force: :cascade do |t|
     t.integer "user_id"
-    t.date "date"
     t.integer "work_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
