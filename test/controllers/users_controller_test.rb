fc89a7a4691ceebbeff95e63f94fd1628e7b3466
Jessica Owens <vertige@gmail.com>
2017-10-13 10:38:35 -0700

    wrote #new test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,12 +6,12 @@ describe UsersController do
       get users_path
       must_respond_with :success
     end
-    #
-    # it "should get new" do
-    #   get users_new_url
-    #   value(response).must_be :success?
-    # end
-    #
+
+    it "should display a form for a new User" do
+      get new_user_path
+      must_respond_with :success
+    end
+
     # it "should get edit" do
     #   get users_edit_url
     #   value(response).must_be :success?
