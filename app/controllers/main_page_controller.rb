be425a2cd2cedfb07befc92b07c30ce366928e66
averikitsch <aekitsch@gmail.com>
2017-10-12 20:40:52 -0700

    fixed ranking on main page

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -3,14 +3,18 @@ class MainPageController < ApplicationController
     works = Upvote.group(:work_id).count
     top = works.max_by { |id,count| count }
     @top = Work.find(top[0])
+
     books = Work.where(category: "book")
     book_results = books.sort_by { |work| -1*work.upvotes.count }
-    @top_books = book_results[1..[10, book_results.length].min]
+    @top_books = book_results[0..[9, book_results.length].min]
+
     movies = Work.where(category: "movie")
     movie_results = movies.sort_by { |work| -1*work.upvotes.count }
-    @top_movies = movie_results[1..[10, movie_results.length].min]
+    @top_movies = movie_results[0..[9,
+    movie_results.length].min]
+
     albums = Work.where(category: "album")
     album_results = albums.sort_by { |work| -1*work.upvotes.count }
-    @top_albums = album_results[1..[10,album_results.length].min]
+    @top_albums = album_results[0..[9,album_results.length].min]
   end
 end
