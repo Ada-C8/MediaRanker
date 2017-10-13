e51caccbf1d25c2e4638401cf2be0cea4f5dcdad
Irene DeVera <irenneeyyy@gmail.com>
2017-10-12 21:31:53 -0700

    Add routes, sessions, flash messages, log in and log out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,45 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.new(name: params[:name])
+    @user.save
+    return @user
+  end
+
+  def login
+    name = params[:name]
+    user = User.find_by(name: name)
+
+    if user
+      session[:logged_in_session] = user.id
+      flash[:success] = "Successfully logged in as existing user #{user.name}"
+      redirect_to root_path
+    end
+    # if !user
+    #   new_user = create
+    #   session[:logged_in_session] = new_user.id
+    #   flash[:new_success] = "Successfully created new user #{new_user.name} with ID #{new_user.id}"
+    #   redirect_to root_path
+    # end
+    #Successfully created new user 1231233123213123123 with ID 357
+  end
+
+  def logout
+    session[:logged_in_session] = nil
+    flash[:logged_out] = "Successfully Logged Out"
+    redirect_to root_path
+  end
+end
+
+# def login
+#  +    name = params[:author][:name]
+#  +
+#  +    # Double check that this author exists
+#  +    author = Author.find_by(name: name)
+#  +    if author
+#  +      session[:logged_in_author] = author.id
+#  +      redirect_to books_path
+#  +    else
+#  +      flash[:status] = :failure
+#  +      flash[:message] = "No author found with name #{name}"
+#  +      render :login_form
+#  +    end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,28 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-# get "/users", to: "users#index", as: "users"
-# get "/users/new", to: "users#new", as: "new_user"
-# post "/users", to: "users#create"
-# get "/users/:id", to: "users#show", as: "user"
-# get "/users/:id/edit", to: "users#edit", as: "edit_user"
-# patch "/users/:id", to: "users#update"
-# delete "/users/:id", to: "users#destroy"
+  # get "/users", to: "users#index", as: "users"
+  # get "/users/new", to: "users#new", as: "new_user"
+  # post "/users", to: "users#create"
+  # get "/users/:id", to: "users#show", as: "user"
+  # get "/users/:id/edit", to: "users#edit", as: "edit_user"
+  # patch "/users/:id", to: "users#update"
+  # delete "/users/:id", to: "users#destroy"
 
-get '/', to: 'main#index', as: 'root'
+  get '/', to: 'main#index', as: 'root'
 
-resources :users
+  resources :users
 
-resources :votes
+  resources :votes
 
-resources :works
+  resources :works
 
-post 'works/:id/votes', to: 'votes#create'
+  resources :sessions
 
-# get "/passengers/:id/trips/new", to: "trips#new", as: "new_passenger_trip"
+  get '/login', to: 'sessions#login', as: 'login'
+  post '/login', to: 'sessions#login'
 
-# get 'works/:id/votes/new', to: 'votes#new', as: new_work_vote_path
+  get '/logout', to: 'sessions#logout', as: 'logout'
+  post '/logout', to: 'sessions#logout'
 
 end
