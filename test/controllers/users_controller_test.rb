3fadd624ce4a815c9264c9e8f8bdb56e50d991d1
Jan Edrozo <jedrozo@gmail.com>
2017-10-13 13:27:37 -0700

    Added work_show page skeletal content

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,24 +1,6 @@
 require "test_helper"
 
 describe UsersController do
-  # it "should get index" do
-  #   get users_index_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get show" do
-  #   get users_show_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get new" do
-  #   get users_new_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get create" do
-  #   get users_create_url
-  #   value(response).must_be :success?
-  # end
+
 
 end
