e6cdde6cba4f6595ff68266687745d1907f31b9b
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-13 11:59:44 -0700

    formatting for lists

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,2 +1,8 @@
 class MainController < ApplicationController
+  def index
+    @best = Work.best
+    @best_movies = Work.best_10('movies')
+    @best_books = Work.best_10('books')
+    @best_albums = Work.best_10('albums')
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -10,4 +10,12 @@ class Work < ApplicationRecord
   def self.all_categories
     LEGAL_CATEGORIES
   end
+
+  def self.best
+    Work.all.sort_by{|w| -w.votes.count}.first
+  end
+
+  def self.best_10(category)
+    Work.where(category: category).sort_by{|w| -w.votes.count}[0..10]
+  end
 end
