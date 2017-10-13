61c60bf6b85d60a50c27a2db5032d65f02975dec
Irene DeVera <irenneeyyy@gmail.com>
2017-10-13 13:32:11 -0700

    Add tests to vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,8 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
 
-  validates :user_id, numericality: { only_integer: true }, uniqueness: true
-  validates :work_id, numericality: { only_integer: true }, uniqueness: true
+  validates :user_id, numericality: { only_integer: true }
+  validates :work_id, numericality: { only_integer: true }
+  validates_presence_of :user
+  validates_presence_of :work
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -6,25 +6,24 @@ describe Vote do
   let(:vote) { Vote.new }
 
   describe "validations" do
-    it "can be created with all fields" do
+    it "can be created with all valid fields" do
       vote = Vote.new(
         user_id: user.id,
-        work_id: work.id,
+        work_id: works(:movie).id
       )
+
+      vote.must_respond_to :user
+      vote.must_respond_to :work
       vote.must_be :valid?
+      vote.errors.messages.wont_include :user_id, :work_id
     end
 
-    it "cannot be created if the user_id and work_id already exists in the database" do
-      first = votes(:vote)
-      vote = Vote.new(
-        user_id: first.user,
-        work_id: first.work
-      )
-
-      vote.wont_be :valid?
-      vote.errors.messages.must_include :user_id
-      vote.errors.messages.must_include :work_id
-    end
+    # it "cannot be created if the user_id and work_id already exists as a vote" do
+    #   # vote = Vote.new(
+    #   #   user_id: user.id,
+    #   #   work_id: work.id
+    #   # )
+    # end
 
     it "has a user_id as an integer" do
       vote = Vote.new(user_id: user.id)
@@ -51,9 +50,17 @@ describe Vote do
     end
 
     it "is invalid if the user_id is blank or is not a number" do
+      vote = Vote.new(user_id: "This is not an integer", work_id: work.id)
+
+      vote.wont_be :valid?
+      vote.errors.messages.must_include :user_id
     end
 
     it "is invalid if the work_id is blank or is not a number" do
+      vote = Vote.new(user_id: user.id, work_id: "This is not an integer")
+
+      vote.wont_be :valid?
+      vote.errors.messages.must_include :work_id
     end
   end
 
@@ -76,32 +83,8 @@ describe Vote do
       vote.work.must_equal work
       vote.work_id.must_equal work.id
     end
-
-    # it 'can set the author through "author"' do
-    #   # Create two models
-    #   author = Author.create!(name: "test author")
-    #   book = Book.new(title: "test book")
-    #
-    #   # Make the models relate to one another
-    #   book.author = author
-    #
-    #   # author_id should have changed accordingly
-    #   book.author_id.must_equal author.id
-    # end
-
-    # it 'can set the author through "author_id"' do
-    #   # Create two models
-    #   author = Author.create!(name: "test author")
-    #   book = Book.new(title: "test book")
-    #
-    #   # Make the models relate to one another
-    #   book.author_id = author.id
-    #
-    #   # author should have changed accordingly
-    #   book.author.must_equal author
-    # end
-  end
-end
+  end # Des
+end # Des
 
 
   #must_be(expression, fail_message)
