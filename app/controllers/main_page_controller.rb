b590c5904a7d576ae36cc62b8985566098ecf114
murog <guillermina.muro@gmail.com>
2017-10-12 22:21:15 -0700

    loops through all works and displays them on main page

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,4 +1,5 @@
 class MainPageController < ApplicationController
   def index
+    @works = Work.order(:title).group_by { |work| work.category }
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,8 @@
 class Work < ApplicationRecord
   has_many :votes
+
+  def votes_count
+    return "n0 v0tes" if work.votes == nil
+    return work.votes.length
+  end
 end
