cec795e676b7b58ace0262465974bfcf94a1280c
averikitsch <aekitsch@gmail.com>
2017-10-13 13:17:50 -0700

    Added upvotes controller tests

--- test/controllers/upvotes_controller_test.rb
+++ test/controllers/upvotes_controller_test.rb
@@ -1,15 +1,15 @@
 require "test_helper"
 
 describe UpvotesController do
-  # it "should get create" do
-  #   get upvotes_create_url
-  #   value(response).must_be :success?
-  # end
 
   it "should create upvote" do
-    proc {
-      post upvote_path(works(:hp).id), params: {session: {user_id: users(:dex).id}}
-    }.must_change 'Upvote.count', 1
+    post login_path, params: {name: users(:dex).name}
+    proc { post upvote_path(works(:hp).id)}.must_change 'Upvote.count', 1
+    must_respond_with :redirect
+  end
+
+  it "should can't upvote if not logged in" do
+    proc { post upvote_path(works(:hp).id)}.must_change 'Upvote.count', 0
     must_respond_with :redirect
   end
 
