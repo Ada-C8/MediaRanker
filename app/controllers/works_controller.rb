ba99ded34ac03504430741c178006196164591b0
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-13 10:34:46 -0700

    Added destroy and edit logic in work controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -12,7 +12,7 @@ class WorksController < ApplicationController
   def show
     @work = Work.find_by(id: params[:id])
     if @work == nil
-      redirect_to root_path
+      redirect_to "/404.html"
     end
   end
 
@@ -23,27 +23,41 @@ class WorksController < ApplicationController
   def create
     @work = Work.new(work_parameters)
     if @work.save
+      flash[:success] = "Work was successfully saved."
       redirect_to @work
     else
+      flash[:error] = "Work was unsuccessfully saved."
       render :new
     end
   end
 
   def destroy
-    work = Work.find_by(id: params[:id])
+    work = Work.find_by(id: params[:id].to_i)
     if work
       work.destroy
       flash[:success] = "Work ##{work.id} was successfully deleted."
     else
-      flash[:fail] = "Work cannot be deleted."
+      flash[:error] = "Work cannot be deleted."
     end
     redirect_to root_path
   end
 
   def edit
+    @work = Work.find_by(id: params[:id].to_i)
+    unless @work
+      flash[:error] = "Work does not exist."
+      redirect_to root_path
+    end
+
   end
 
   def update
+    @work = Work.find_by(id: params[:id].to_i)
+    if @work.update_attributes work_parameters
+      redirect_to @work
+    else
+      render :new
+    end
   end
 
   private
