65f29fe403881a907b1e5241b8f4a3e1d0599145
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-10-13 16:47:59 -0700

    Sessions login fixed and table added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,16 @@ class SessionsController < ApplicationController
   end
 
   def login
-    @user = User.find_by(name: params[:username])
+    @user = User.find_by(username: params[:username])
 
-    if user
+
+    if @user
       flash[:success] = "successfully logged in as #{user.name}"
       session[:user_id] = user.id
       redirect_to root_path
     else
-      flash[:error] = "No user found by that name #{ params[:username]}"
+      @user = User.create(username: params[:username])
+      flash[:error] = "successfully created User: #{ params[:username]}"
       redirect_to root_path
     end
   end
