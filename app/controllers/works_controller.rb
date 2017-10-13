11ee2bef87820ba09511baa22d0bc527754c644e
IsabelDePapel <isuchanek@gmail.com>
2017-10-12 17:58:34 -0700

    move flash error to partial view, add edit work view

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -27,12 +27,12 @@ class WorksController < ApplicationController
     @work = Work.new(work_params)
 
     if @work.save
-      #flash[:success] = "Successfully created #{@work.category.name}"
+      flash[:success] = "Successfully created #{@work.category.name}"
       redirect_to root_path
     else
-      puts "error message"
-      puts @work.errors.inspect
-      flash[:error] = "ERROR"
+      # puts "error message"  DEBUGGING
+      # puts @work.errors.inspect
+      flash.now[:error] = "A problem occurred: Could not create #{@work.category.name}"
       render :new
     end
   end
@@ -54,6 +54,7 @@ class WorksController < ApplicationController
       flash[:success] = "Successfully updated #{@work.category.name}"
       redirect_to root_path
     else
+      flash.now[:error] = "A problem occurred: Could not update #{@work.category.name}"
       render :edit
     end
 
@@ -67,7 +68,7 @@ class WorksController < ApplicationController
       flash[:success] = "Successfully deleted #{@work.category.name}"
       redirect_to root_path
     else
-      flash[:error] = "Unable to delete #{@work.category.name}"
+      flash.now[:error] = "Unable to delete #{@work.category.name}"
     end
   end
 
@@ -81,7 +82,7 @@ class WorksController < ApplicationController
     albums = Work.left_outer_joins(:votes).where(category_id: Category.find_by(name: "album")).distinct.select('works.*, COUNT(votes.*) AS num_votes').group('works.id').order('num_votes DESC')
 
     movies = Work.left_outer_joins(:votes).where(category_id: Category.find_by(name: "movie")).distinct.select('works.*, COUNT(votes.*) AS num_votes').group('works.id').order('num_votes DESC')
-    
+
     books = Work.left_outer_joins(:votes).where(category_id: Category.find_by(name: "book")).distinct.select('works.*, COUNT(votes.*) AS num_votes').group('works.id').order('num_votes DESC')
 
     if top_ten
