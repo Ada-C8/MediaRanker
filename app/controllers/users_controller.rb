519930b57f44a11a0ddf6a1f2af67cd2aa7e88bd
sairagula <abdukhalieva@gmail.com>
2017-10-13 14:23:58 -0700

    Login successful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,25 +4,36 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def new
-  end
-
-  def create
+  def show
   end
 
 
   def login_form
-
+    @user = User.new
+    if params[:user_id]
+      @user.user_id = params[:user_id]
+    end
   end
 
   def login
-    username = params[:user][:username]
-    user = User.find(username: username)
+    @user = User.new(user_params)
 
-    if user
-      session[:logged_in_user] = user.id
-      flash[:success] = "#{ user.username } is successfully logged in"
+    if save_and_flash(@user)
       redirect_to root_path
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+  def user_params
+    return params.require(:user).permit(:username)
+  end
+
+  def find_user_by_params_id
+    @user = User.find_by(id: params[:id])
+    unless @user
+      head :not_found
     end
   end
 end
