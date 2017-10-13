fd232c41345d12937f6eb672ddf16c74638dcd48
averikitsch <aekitsch@gmail.com>
2017-10-13 14:55:40 -0700

    added missing upvote controller test

--- test/controllers/upvotes_controller_test.rb
+++ test/controllers/upvotes_controller_test.rb
@@ -8,6 +8,13 @@ describe UpvotesController do
     must_respond_with :redirect
   end
 
+  it "can't upvote more than once" do
+    post login_path, params: {name: users(:dex).name}
+    post upvote_path(works(:hp).id)
+    proc { post upvote_path(works(:hp).id)}.must_change 'Upvote.count', 0
+    must_respond_with :redirect
+  end
+
   it "should can't upvote if not logged in" do
     proc { post upvote_path(works(:hp).id)}.must_change 'Upvote.count', 0
     must_respond_with :redirect
