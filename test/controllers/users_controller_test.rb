f9b9273f80b94b8db5c942b4a1cf8981acd2dd6e
Jan Edrozo <jedrozo@gmail.com>
2017-10-13 14:38:35 -0700

    Added text for user_index page; still need to populate users table

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,14 @@
 require "test_helper"
 
 describe UsersController do
-  
+
   it "successfully loads works index page to list all works" do
     get users_path
+    must_respond_with :success
+  end
+
+  it "successfully loads an individual users' voting summary (#show)page" do
+    get user_path( users(:jane).id )
     must_respond_with :success
   end
 
