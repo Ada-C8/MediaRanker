244fbf719cd0a699292d7a0642660cc506e401e6
Jan Edrozo <jedrozo@gmail.com>
2017-10-13 13:43:35 -0700

    Added table layout for List of Users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 describe UsersController do
-
+  
+  it "successfully loads works index page to list all works" do
+    get users_path
+    must_respond_with :success
+  end
 
 end
