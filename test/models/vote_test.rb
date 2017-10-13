0084ad12dbba2eacf6d4e80c66b4c09afe95812f
Julia <julia.meier@gmail.com>
2017-10-12 21:48:02 -0700

    set up yml files for testing

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -3,7 +3,26 @@ require "test_helper"
 describe Vote do
   let(:vote) { Vote.new }
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+  describe "validations" do
+    it "responds to and accesses user_id and work_id" do
+      vote.must_respond_to :user_id
+      vote.user_id.must_equal :user_id
+      vote.must_respond_to :work_id
+      vote.work_id.must_equal :work_id
+    end
+
+    it "requires a user_id and work_id to be valid" do
+    end
+
+    it "requires the same user_id and work_id cannot be associated in more than 1 vote instance" do
+    end
   end
+
+  describe "relations" do
+
+  end
+
+  describe "custom methods" do
+  end
+
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -48,15 +48,15 @@ describe Work do
 
   end
 
-  # describe "custom methods" do
-    # it "calculates total votes" do
-    #   test_user = User.create!(name: "Shel Silverstein")
-    #   test_work = Work.create!(title: "Amelia Badelia", category: "book")
-    #   test  _vote = Vote.create!(user_id: test_user.user_id, work_id: test_work.work_id)
-    #   puts "vote1: #{vote1}, #{vote1.user_id}"
-    #   puts "----------"
-    #   test_vote.valid?.must_equal true
+  describe "custom methods" do
+    it "calculates total votes" do
+      test_user = User.create!(name: "Shel Silverstein")
+      test_work = Work.create!(title: "Amelia Badelia", category: "book")
+      test  _vote = Vote.create!(user_id: test_user.user_id, work_id: test_work.work_id)
+      puts "vote1: #{vote1}, #{vote1.user_id}"
+      puts "----------"
+      test_vote.valid?.must_equal true
 
-    # end
+    end
 
 end
