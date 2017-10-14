487d8b15242198d139eedec4b84c6207dc560b6d
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-14 13:36:04 -0700

    User model passed all test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  describe "validations" do
+
+    it "must have a username to be valid" do
+      user = users(:user1)
+      user.username = nil
+      user.valid?.must_equal false
+
+      user.username = "Marcel"
+      user.valid?.must_equal true
+    end
+
+    it "must have an username that doesn't already exist to be valid" do
+      user = users(:user1)
+      name = user.username
+      new_user = User.new
+      new_user.username = name
+      new_user.valid?.must_equal false
+    end
+
   end
 end
