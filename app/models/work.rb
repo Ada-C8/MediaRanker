6c5e3c7c1813eeb674579a3e344ec7670010b563
Amy Lee <amy.yj.lee@gmail.com>
2017-10-14 12:19:38 -0700

    modified Works show page with placeholder html for showing a votes table, pseudocode for upvote method added to Work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,11 @@
 class Work < ApplicationRecord
   has_many :votes
-  
+
   validates :title, presence: true, uniqueness: {scope: :category, message: "There is already a Work of this category with this title " }
+
+  def upvote
+    #if user is logged in
+    #then you can create a vote on a work
+    #else, flash error that says you must be logged in to upvote
+  end
 end
