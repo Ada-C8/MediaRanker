a8ad7180f6de4d167b885c3c7e3f866e380c07b7
Amy Lee <amy.yj.lee@gmail.com>
2017-10-14 11:48:42 -0700

    Modified User index page and tested that User index and show pages pull up correctly

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,23 +2,14 @@ require "test_helper"
 
 describe UsersController do
   it "should get index" do
-    get users_index_url
-    value(response).must_be :success?
-  end
-
-  it "should get create" do
-    get users_create_url
-    value(response).must_be :success?
-  end
-
-  it "should get new" do
-    get users_new_url
-    value(response).must_be :success?
+    get users_path
+    must_respond_with :success
   end
 
   it "should get show" do
-    get users_show_url
-    value(response).must_be :success?
+    user = users(:user_one)
+    get user_path(user.id)
+    must_respond_with :success
   end
 
 end
