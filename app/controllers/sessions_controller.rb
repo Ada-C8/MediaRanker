8733bf2aaa0723de68b75fbc0e1185dfb61e6f02
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-13 22:49:21 -0700

    added flash to root page. Added basic code in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+
+  def login
+    user = User.find_by(name: params[:name])
+
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as #{ user.name }!"
+      redirect_to root_path
+    end
+    
+  end
 end
