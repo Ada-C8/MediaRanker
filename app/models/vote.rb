7e3e33455f4664e0a6ebfec6ef907dac7c20346b
Canaan West <canaan.west@gmail.com>
2017-10-14 13:11:00 -0700

    added uniquness validation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,6 @@ class Vote < ApplicationRecord
   belongs_to :user
 
   validates :user_id, presence: :true
+  validates :user_id, uniqueness: {scope: :work_id}
   validates :work_id, presence: :true
 end
