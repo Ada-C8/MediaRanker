30471f4bd5ed2a354bf7b51e5f04e7335a06e9e9
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-10-14 16:27:11 -0700

    trying to make sessions work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,31 +4,47 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find_by(id: params[:id])
   end
 
   def login_form
+
   end
 
   def login
-    user_id =  params[:user][:user_id]
-    @user = User.find_by(id: user_id)
+    input = params[:name]
+    @user  = User.find_by(name: input)
     if @user
-      session[:logged_in] = user_id
+    # login_input = params[:name]
+    #
+    # @user = User.find_by(name: login_input)
+    # user_id = params[:id]
+    #
+    # #if user is true mean user != nil, returns a user id
+    # if @user
+      session[:logged_in] = @user
       flash[:status] = :success
-      flash[:message] = "Successfully logged in as existing user #{@user.name}"
+      flash[:message] = "Successfully logged in as existing user #{@user.name},#{@user.id}"
       redirect_to root_path
-
-    elsif @user.save
-      # if User.where(name: params[:name])
-      flash[:status] = :success
-      flash[:message] = "Successfully created new user #{@user.name} with ID #{@user.id}"
-      redirect_to root_path
+    else
+      create_user_path
     end
 
+    def create
+      # @user = User.create! name: params[:name]
+      @user = User.new(user_params)
+      # if @user != nil
+
+        session[:logged_in] = @user.id
+        flash[:status] = :success
+        flash[:message] = "Successfully created new user #{@user.name} with ID #{@user.id} "
+        redirect_to root_path
+      # end
+    end
   end
 
   private
   def user_params
-    return params.require(:user).permit(:name, :created_at)
+    return params.require(:user).permit(:id, :name, :created_at)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,7 @@ Rails.application.routes.draw do
 
   post '/login', to: 'users#login'
 
+  post '/users', to: 'users#create', as: 'create_user'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
