dd756a07f8c551c52cc588f7b0f0b70ed62e7c2a
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-10-14 12:27:52 -0700

    Add vote logic in vote controller -- need to be logged in and cannot double vote

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,17 @@ Rails.application.routes.draw do
 get '/', to: 'main#index', as: 'root'
 
 resources :works
+post '/works/:id/upvote', to: 'votes#upvote', as: 'work_upvote'
+
 
 resources :users
 
-resources :votes
+# resources :votes
 
 get '/login', to: 'sessions#login_form', as: 'login'
 post '/login', to: 'sessions#login'
 
 delete '/login', to: 'sessions#logout', as: 'logout'
+
 
 end
