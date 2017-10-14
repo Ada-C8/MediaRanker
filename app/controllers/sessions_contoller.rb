1c049c7427488a433549d7584866a2d6073753fe
VirtualLindsey <tapircode@gmail.com>
2017-10-13 22:08:59 -0700

    session work

--- app/controllers/sessions_contoller.rb
+++ app/controllers/sessions_contoller.rb
@@ -1,5 +1,23 @@
-class SessionsController < ApplicationController
-  def login
+require 'date'
 
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:name])
+
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "log in successful!"
+    else
+      flash.now.alert = 'Invalid email or password'
+      render "new       "
+    end
+  end
+
+  def destory
+    session[:user_id] = nil
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   #root 'home#index'
   get '/works', to: 'works#index', as: 'works'
-  get '/login', to: 'login#index', as: 'login'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destory', as: 'logout'
   get '/works/new', to: 'works#new', as: 'new_work'
   post '/works', to: 'works#create'
   post '/works/:id/upvote', to: 'works#upvote', as: 'upvote_work'
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
   delete '/works/:id', to: 'works#destroy'
   root 'home#index'
 #  root 'works#index'
-  resources :users, :books, :movies, :albums
+  resources :users, :books, :movies, :albums, :sessions
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
