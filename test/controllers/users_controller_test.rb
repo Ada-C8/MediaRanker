281f3fd8d4e08b30d2fbf555dae85a708983f637
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-14 16:57:40 -0700

    Clean up user controller test file, deletes old work

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,20 +1,6 @@
 require "test_helper"
 
 describe UsersController do
-  let :user_data {
-    user_data = {
-      user: {
-        username: "Sarah"
-      }
-    }
-  }
-  let :bad_user_data {
-    bad_user_data = {
-      user: {
-        username: ""
-      }
-    }
-  }
   let :user_id { User.last.id }
   let(:user) { User.new(username: 'test username') }
 
@@ -30,32 +16,6 @@ describe UsersController do
       must_respond_with :success
     end
   end
-
-  # describe "create" do
-  #   it "redirects to the users_path if the user data is valud and adds a user to the db" do
-  #     # # Arrange
-  #     # test if work is valid
-  #     User.new(user_data[:user]).must_be :valid?
-  #     user_count = User.count
-  #     # # Act
-  #     post users_path, params: user_data
-  #     # # Assert
-  #     must_respond_with :redirect
-  #     must_redirect_to users_path
-  #     User.count.must_equal user_count + 1
-  #   end
-  #
-  #   it "returns bad_request status when the user data is invalid" do
-  #     # # Arrange
-  #     User.new(bad_user_data[:user]).wont_be :valid?
-  #     user_count = User.count
-  #     # # Act
-  #     post users_path, params: bad_user_data
-  #     # # Assert
-  #     must_respond_with :bad_request
-  #     User.count.must_equal user_count
-  #   end
-  # end
 
   describe "show" do
     it "returns success when given a valid user ID" do
