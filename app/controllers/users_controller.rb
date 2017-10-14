fd4e65997bd31fdbdcb10f1a969854629c6a9237
Sara C <srachandler@gmail.com>
2017-10-14 15:25:44 -0700

    on users branch, filling out controller and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
+
   def index
+    @users = Users.all
   end
 
   def show
+    @user = User.find(param [:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,22 @@
 require "test_helper"
 
 describe UsersController do
-  it "should get index" do
-    get users_index_url
-    value(response).must_be :success?
+
+  describe 'index' do
+
+    it "should display index page with users" do
+      User.count_must_be :>, 0
+      get users_path
+      must_respond_with :success
+    end
+
+
+    it "should get index" do
+      get users_index_url
+      must_respond_with :success
+    end
   end
+
 
   it "should get show" do
     get users_show_url
