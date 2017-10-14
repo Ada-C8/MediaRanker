3629e4c30881e7d689ed225c55e76349b007fd85
Angela Wilson <wilsonange@gmail.com>
2017-10-14 11:14:09 -0700

    inadvertantly worked on work controller. CRUD function skeletons created without code guts.

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,8 +2,20 @@ require "test_helper"
 
 describe Work do
   let(:work) { Work.new }
+  describe "validations" do
+    it "create work with all fields" do
 
-  it "must be valid" do
-    value(work).must_be :valid?
+    end
+      # work = Work.new(
+      #   category: "album",
+      #   title: "test work",
+      #   publication_year: 2017,
+      #   desciption: "",
+      #   creator: "me",
+      # )
+
+      # work.must_be :valid?
+
   end
+
 end
