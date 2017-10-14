3fc19459010f749dce05bf03932f56f74dd4d41f
Canaan West <canaan.west@gmail.com>
2017-10-14 13:25:36 -0700

    upvote create paths fixed

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,27 +6,24 @@ class VotesController < ApplicationController
   def show
   end
 
-  def new
-    @vote = Vote.new
-    @user_id = session[:logged_in_user]['id'].to_i
-    @work_id = vote_params
-  end
+  # def new
+  #   @vote = Vote.new
+  #   @user_id = session[:logged_in_user]['id'].to_i
+  #   @work_id = params[:id]
+  # end
 
   def create
     @vote = Vote.new
-    @vote.user_id = session[:logged_in_user]['id'].to_i
-    @vote.work_id = vote_params
+    @vote.user_id = session[:logged_in_user][:id]
+    @vote.work_id = params[:id].to_i
     @vote.save
     if @vote.save
       flash[:success] = "Successfully upvoted!"
+      redirect_to work_path(@vote.work_id)
     else
       flash[:failure] = "You cannot upvote twice!"
+      redirect_to work_path(params[:id])
     end
   end
 
-
-private
-  def vote_params
-    params.require(:works).permit(:id)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   resources :works
   resources :users
   resources :votes
+  post 'votes/:id', to: 'votes#create', as: 'votes_path'
 
   get 'login', to: 'users#login_form', as: 'login'
   post 'login', to: 'users#login'
-  
+
   post 'logout', to: 'users#logout', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
