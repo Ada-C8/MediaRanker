73119ed380d2ab0380d4f341ca2195a56bfe16a2
Amy Cash <amymcash@gmail.com>
2017-10-14 14:28:25 -0700

    Fix Logins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,25 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def login_form 
+  def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to login_path
+    # if @user.save
+    #   flash[:success] = "Welcome New User!"
+    #   redirect_to works_index_path
+    # else
+    #   render :new
+    # end
+  end
+
+  def login_form
+  end
+
 
   def login
     username = params[:user][:username]
@@ -13,15 +30,22 @@ class UsersController < ApplicationController
 
     if user
       session[:logged_in_user] = user.id
-      redirect_to works_path
+      redirect_to works_index_path
     else
       flash.now[:status] = :failure
       flash.now[:message] = "No such user exists."
-      render :login_form
+      redirect_to new_user_path
     end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+  def user_params
+    return params.require(:user).permit(:username)
+  end
+
 end
