3a9eb0f4cd9300f36c0ac03de15cbf72c1bb62a4
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-13 22:42:42 -0700

    small update before trying sessions stuff

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,8 +5,6 @@ class Vote < ApplicationRecord
   validates :user_id, presence: true
   validates :work_id, presence: true
 
-  # @user = user.id
-
   def self.votes_by_user(user)
     return Vote.where(user_id: user.id)
   end
