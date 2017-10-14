d6a8c17272a6e0ca3f195d42c53d9838eaa5b85e
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-14 13:08:15 -0700

    completed Users controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,49 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    redirect_to users_path unless @user
   end
 
   def new
+    @user = user.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    redirect_to user_path unless @user
+    if @user.update_attributes user_params
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    return params.require(:user).permit(:username)
+  end
+
+  def find_user
+    @user = User.find_by_id(params[:id])
   end
 end
