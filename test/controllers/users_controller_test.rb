c1dd4192e9b12640e96dbdb43a1f7dc3ccb0bcfe
sairagula <abdukhalieva@gmail.com>
2017-10-14 11:57:36 -0700

    Vote model added

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,22 @@
 require "test_helper"
 
 describe UsersController do
-  it " should return a success for all users" do
-    get users_path
-    must_respond_with :success
+  describe "index" do
+    it " should return success status for all users" do
+      get users_path
+      must_respond_with :success
+    end
+    it "should return success when theere are no users" do
+      User.destroy_all
+      get users_path
+      must_respond_with :success
+    end
   end
-  it "should return success when theere are no users" do
-    User.destroy_all
-    get users_path
-    must_respond_with :success
+
+  describe "login_form" do
+    it "should return a success" do
+      get login_path
+      must_respond_with :success
+    end
   end
 end
