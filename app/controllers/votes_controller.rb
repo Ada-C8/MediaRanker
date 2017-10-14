16faf1377428bbce8c07b8e83ab340e74b320798
Canaan West <canaan.west@gmail.com>
2017-10-14 13:39:46 -0700

    Successfully saving upvotes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,26 +3,19 @@ class VotesController < ApplicationController
     @votes = Vote.all
   end
 
-  def show
-  end
-
-  # def new
-  #   @vote = Vote.new
-  #   @user_id = session[:logged_in_user]['id'].to_i
-  #   @work_id = params[:id]
-  # end
-
   def create
-    @vote = Vote.new
-    @vote.user_id = session[:logged_in_user][:id]
-    @vote.work_id = params[:id].to_i
+    work = params[:id].to_i
+    user = session[:logged_in_user]['id']
+    @vote = Vote.new(user_id: user, work_id: work )
+
     @vote.save
     if @vote.save
       flash[:success] = "Successfully upvoted!"
       redirect_to work_path(@vote.work_id)
     else
-      flash[:failure] = "You cannot upvote twice!"
       redirect_to work_path(params[:id])
+      flash[:failure] = "You cannot upvote twice!"
+    #   redirect_to work_path(params[:id])
     end
   end
 
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,5 @@ class Vote < ApplicationRecord
   belongs_to :work
   belongs_to :user
 
-  validates :user_id, presence: :true
-  validates :user_id, uniqueness: {scope: :work_id}
-  validates :work_id, presence: :true
+  validates :user_id, uniqueness: {scope: [:work_id]}
 end
