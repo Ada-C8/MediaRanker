ec0efea70cef73d9739a18783a8fe8bca34541ad
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-14 00:54:17 -0700

    wrote more code in controller for votes, need to test.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,17 +5,51 @@ class VotesController < ApplicationController
   end
 
   def create
-    user_id = User.find_by(id: params[:user][:id]).id
-    work_id = Work.find_by(id: params[:work][:id].id
-    @vote = Vote.new(user_id: user_id, work_id: work_id)
-      if @vote.save
-        redirect_to work_path
-        return
-      else
+    user = User.find_by(id: params[:user][:id])
+
+    if user == nil
+      flash[:status] = :failure
+      flash[:message] = "Sorry, you must be logged in to vote."
+      redirect_to work_path, status: :bad_request
+      return
+    end
+
+    work = Work.find_by(id: params[:work][:id])
+
+    if work == nil
+      flash[:status] = :failure
+      flash[:message] = "Sorry, that media no longer exists in the database."
+      redirect_to works_path, status: :bad_request
+      return
+    end
+
+    @vote = Vote.new(user_id: user.id, work_id: work.id)
+
+    Vote.votes_by_user(user).each do |vote|
+      if vote.work_id == work.id
+        flash[:status] = :failure
+        flash[:message] = "You have already voted on this work - you can only vote once."
         redirect_to work_path, status: :bad_request
         return
       end
+    end
 
+    if @vote.save
+      flash[:status] = :success
+      flash[:message] = "Thank you for your vote!"
+      redirect_to work_path
+      return
+    else
+      flash[:status] = :failure
+      flash[:message] = "Sorry, your vote could not be processed."
+      redirect_to work_path, status: :bad_request
+      return
+    end
 
   end
+
+  # private
+  # def vote_params
+  #   params.require(:user, :work)
+  # end
 end
