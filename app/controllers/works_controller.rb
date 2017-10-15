117679570d9db043c3ea2b0e713e4bbc65175ef0
murog <guillermina.muro@gmail.com>
2017-10-14 17:36:25 -0700

    update works controller to include private mthods, index includes table

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,6 @@
 class WorksController < ApplicationController
   def index
+    @works = all_works_by_category
   end
 
   def show
@@ -19,4 +20,18 @@ class WorksController < ApplicationController
 
   def destroy
   end
+end
+
+private
+def all_works_by_category
+  works = Work.order(:title).group_by { |work| work.category }
+  return works
+end
+
+def all_works_by_votes
+  works = all_works_by_category
+  works = works.each do |key, values|
+    works[key] = (values.sort_by {|value| value.votes.length}).reverse
+  end
+  return works
 end
