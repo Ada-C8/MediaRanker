fbd2a4a8e50b87b582b2f76f0e1843248ea18cb7
Jessica Owens <vertige@gmail.com>
2017-10-15 14:49:36 -0700

    temp fixed a buggy path in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def login
-    # puts params
     user = User.find_by(username: params[:name])
 
     if user
-      # puts user.id
       flash[:success] = "Successfully logged in as existing user #{user.username}"
       session[:logged_in_as_user] = user.id
-      # puts "Successfully logged in as existing user #{User.find(session[:logged_in_as_user]).username}"
       redirect_to root_path
     else
       user = User.new(username: params[:name])
@@ -20,8 +17,10 @@ class SessionsController < ApplicationController
         flash[:success] = "Successfully created new user #{user.username} with ID #{user.id}"
         redirect_to root_path
       else
-        flash.now[:error] = "A problem occurred: Could not log in"
-        render login_path, status: bad_request
+        flash[:error] = "A problem occurred: Could not log in"
+        redirect_back(fallback_location: root_path)
+        #BUG redirect_to login_path
+        #BUG render :login_form, status: :no_content
       end
     end
   end
