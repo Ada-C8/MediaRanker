f36c524f2c3f2a94b681e0f76971f754faf557bc
Canaan West <canaan.west@gmail.com>
2017-10-15 12:28:24 -0700

    added flash error if user is not logged in when trying to vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,17 +4,25 @@ class VotesController < ApplicationController
   end
 
   def create
-    work = params[:id].to_i
-    user = session[:logged_in_user]['id']
-    @vote = Vote.new(user_id: user, work_id: work)
-    @vote.save
-    if @vote.save
-      flash[:success] = "Successfully upvoted!"
+    if session[:logged_in_user] == nil
       redirect_back(fallback_location: work_path(params[:id]))
+      flash[:error] = "You must be logged in to vote!"
     else
-      flash[:failure] = "You cannot upvote twice!"
-      redirect_back(fallback_location: work_path(params[:id]))
+
+      work = params[:id].to_i
+      user = session[:logged_in_user]['id']
+      @vote = Vote.new(user_id: user, work_id: work)
+      @vote.save
+
+      if @vote.save
+        flash[:success] = "Successfully upvoted!"
+        redirect_back(fallback_location: work_path(params[:id]))
+      else
+        flash[:failure] = "You cannot upvote twice!"
+        redirect_back(fallback_location: work_path(params[:id]))
+      end
     end
+
   end
 
 end
