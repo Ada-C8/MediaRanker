2b8283dba4cafe7c16772d702d93a4859bb33ffc
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-14 17:10:04 -0700

    Adds username uniqueness validation to user model and reciprocal model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   #validations
-  validates :username, presence: { message: "can't be blank" }
+  validates :username, presence: { message: "can't be blank" }, uniqueness: true
 
   #relationships
   has_many :votes
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,6 +12,16 @@ describe User do
       u.wont_be :valid?
       u.errors.messages.must_include :username
     end
+
+    it "must have a unique name"do
+      # create a user
+      u = User.new(username: "test")
+      u.save
+      # make a new user with same username
+      v = User.new(username: "test")
+      v.wont_be :valid?
+      v.errors.messages.must_include :username
+    end
   end
 
   describe "relations" do
