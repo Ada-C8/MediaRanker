bfa1e87ee01226cb9add65b5b8658fba661aedf8
Gale Harrington <gale.harrington19@gmail.com>
2017-10-15 09:42:14 -0700

    Only allow upvote for user that is logged in, don't allow more than one upvote per user per work

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,7 +4,14 @@ class VotesController < ApplicationController
     user_id = session[:user_id]
     work_id = params[:id].to_i
     @vote = Vote.new(user_id: user_id, work_id: work_id)
-    if @vote.save
+
+    if session[:user_id].nil?
+      flash[:failure] = "You must be logged in to upvote a work!"
+      redirect_to login_path
+    elsif Vote.where(user_id: user_id, work_id: work_id) != []
+      flash[:failure] = "You can only upvote the same work once!"
+      redirect_to root_path
+    elsif @vote.save
       flash[:success] = "Successfully upvoted #{Work.find(work_id).title}!"
       redirect_to root_path
     else
