488575cfdcc83f840d05dab7709f32269b08a251
Jan Edrozo <jedrozo@gmail.com>
2017-10-14 22:47:14 -0700

    Added controller and model for votes

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,6 +4,10 @@ class WorksController < ApplicationController
 
   def index
     @works = Work.order(:title)
+
+    @movies = Work.sort_by_category("movie")
+    @books = Work.sort_by_category("book")
+    @albums = Work.sort_by_category("album")
   end
 
   def new
