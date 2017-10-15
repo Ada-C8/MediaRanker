53fd7f33778b4f0cef0994ec7918e8f3d2fd2636
Bennett Rahn <rahnibennett@gmail.com>
2017-10-15 16:05:28 -0700

    add data to seeds

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,7 +3,8 @@ class Vote < ApplicationRecord
   belongs_to :work
 
   validates :user_id, presence: true
-  validates :user, uniqueness: {message: "has already voted for this work"}
+  validates :user, uniqueness: { scope: :work, message: "has already voted for this work"}
+
   validates :work_id, presence: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 require 'csv'
 require 'rails'
 
-# puts Rails.root
+puts Rails.root
 
-MEDIA_FILE = Rails.root.join('db', 'media_seeds.csv')
+MEDIA_FILE = Rails.root.join('db', 'seed_data', 'media_seeds.csv')
 # what is this doing and why does it need to happen?
 puts "Loading raw driver data from #{MEDIA_FILE}"
 
@@ -36,18 +36,37 @@ end
 puts "Added #{Work.count} work records"
 puts "#{work_failures.length} works failed to save"
 
-# driver_failures = []
-# CSV.foreach(MEDIA_FILE, :headers => true) do |row|
-#   driver = Driver.new
-#   driver.id = row['id']
-#   driver.name = row['name']
-#   driver.vin = row['vin']
-#   puts "Created driver: #{driver.inspect}"
-#   successful = driver.save
-#   if !successful
-#     driver_failures << driver
-#   end
+# ____________________________________________________
+
+
+USER_FILE = Rails.root.join('db', 'seed_data', 'user_seeds.csv')
+puts "Loading raw user data from #{USER_FILE}"
+
+user_failures = []
+
+CSV.foreach(USER_FILE, :headers => true) do |row|
+  user = User.new
+  user.name = row['name']
+  puts "Created user: #{user.inspect}"
+  successful = user.save
+  if !successful
+    user_failures << user
+  end
+end
+
+puts "Added #{User.count} user records"
+puts "#{user_failures.length} users failed to save"
+
+
+
+
+# Since we set the primary key (the ID) manually on each of the
+# tables, we've got to tell postgres to reload the latest ID
+# values. Otherwise when we create a new record it will try
+# to start at ID 1, which will be a conflict.
+# puts "Manually resetting PK sequence on each table"
+# ActiveRecord::Base.connection.tables.each do |t|
+#   ActiveRecord::Base.connection.reset_pk_sequence!(t)
 # end
 #
-# puts "Added #{Driver.count} driver records"
-# puts "#{driver_failures.length} drivers failed to save"
+# puts "done"
