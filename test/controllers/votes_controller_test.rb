8498fb485bead30c926aa0796fe656323715e00f
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-15 18:40:51 -0500

    added edge case to votes controller tests

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,25 +1,32 @@
 require "test_helper"
 
 describe "VotesController" do
-# 
-#   let(:vote) { Vote.new }
-#   let(:fanGirl) { users(:fanGirl) }
-#   let(:fanBoy) { users(:fanBoy) }
-#   let(:twilight) { works(:twilight) }
-#   let(:harry_potter) { works(:harry_potter) }
-#
-# it "can't vote if not logged in" do
-#   vote = Vote.new(user_id: nil, work_id: (:twilight.id))
-# vote.must_equal "Must be logged in to vote"
-#   end
-# end
-#
-# it "logged in and vote, vote count increases by one" do
-# post login_path(user: (:fanGirl.name)
-# proc {post create_vote_path(one.id, album.id)}.must_change 'Vote.count', +1
-# must_respond_with :redirect
-# flash[:success].must_equal "Successfully upvoted!"
-#
-# end
+
+  let(:vote) { Vote.new }
+  let(:fanGirl) { users(:fanGirl) }
+  let(:fanBoy) { users(:fanBoy) }
+  let(:harry_potter) { works(:harry_potter) }
+  let(:one) { users(:one) }
+  let(:two) { users(:two) }
+
+  it "can't vote if not logged in" do
+    proc { post work_votes_path(works(:twilight).id)}.must_change 'Vote.count', 0
+    flash[:error].must_equal "Must be logged in to vote"
+  end
+
+  it "logged in and vote, vote count increases by one" do
+    post login_path, params: {name: users(:fanGirl).name}
+    proc {post work_votes_path(works(:harry_potter).id)}.must_change 'Vote.count', +1
+    must_respond_with :redirect
+    flash[:success].must_equal "Successfully upvoted!"
+  end
+
+  it "same user can't vote for same work twice" do
+    post login_path, params: {name: users(:fanGirl).name}
+    post work_votes_path(works(:harry_potter).id)
+    proc { post work_votes_path(works(:twilight).id)}.must_change 'Vote.count', 0
+    flash[:error].must_equal "Vote not recorded"
+  end
+
 
 end
