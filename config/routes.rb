83a9ae15401b6b6b74a9334c13b4967702762926
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-15 15:13:39 -0700

    Added login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
 
-  get 'users/index'
-
-  get 'users/show'
-
   root to: 'works#homepage'
 
+  get '/users', to: 'users#index', as: 'users'
+
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  get '/login', to: 'sessions#login_form', as: 'login'
+
+  post '/login', to: 'sessions#login'
+
+  post '/logout', to: 'sessions#logout'
+
   resources :works
+
+  # directs non-valid pages to 404.html
+  get '*path' => redirect('/404.html')
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
