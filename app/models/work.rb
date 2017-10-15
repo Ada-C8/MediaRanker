479382480fdc9ae37687afcc7511c346b232de09
Mariana <lmarianarp@gmail.com>
2017-10-15 16:56:50 -0700

    Shows top works

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,7 +5,7 @@ class Work < ApplicationRecord
   validates :category, presence: true, inclusion: { in: %w(book album movie)}
 
 
-  def self.sort_all
-    Work.all.sort_by{ |work| -work.votes.count }
+  def self.sort_all(category)
+    category.all.sort_by{ |work| -work.votes.count }
   end
 end
