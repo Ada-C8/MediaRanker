8a0e4f75606a6d3e1812da6053cd0340cf83d3d8
Tanja Stroble <plusdeltah@gmail.com>
2017-10-15 00:42:53 -0700

    tests complete but some fail

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  it "should get index" do
+    get users_path
+    must_respond_with :success
+  end
+
+  it "should get show" do
+    get user_path(users(:tanja))
+    must_respond_with :success
+  end
+
+  it "should show a 404 when user is not found" do
+    get user_path(123)
+    must_respond_with :missing
+  end
+
+  it "should affect the model when creating a user" do
+    proc {
+      post users_path, params: { user:
+        { name: "gogol bordello"
+        }
+      }
+    }.must_change 'User.count', 1
+  end
 end
--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 describe VotesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "able to vote when logged in" do
+    proc   {
+      post login_path(name: "gogol bordello")
+      post vote_path(works(:relaxer).id)
+    }.must_change 'Vote.all.count', 1
+  end
+
+  it "should not be able to vote when logged out " do
+    proc   {
+      post login_path(name: "")
+      post vote_path(works(:baby).id)
+    }.must_change 'Vote.all.count', 0
+  end
+
+  it "can't vote on the same work multiple times" do
+    proc   {
+      post login_path(name: "gogol bordello")
+      post vote_path(works(:baby).id)
+      post vote_path(works(:baby).id)
+      post vote_path(works(:baby).id)
+    }.must_change 'Vote.all.count', 1
+  end
+
+  it "should be able to vote for a movie" do
+    proc   {
+      post login_path(name: "gogol bordello")
+      post vote_path(works(:conroy).id)
+    }.must_change 'Vote.all.count', 1
+  end
+
+  it "should be able to vote for a album" do
+    proc   {
+      post login_path(name: "gogol bordello")
+      post vote_path(works(:relaxer).id)
+    }.must_change 'Vote.all.count', 1
+  end
+
+  it "should be able to vote for a book" do
+    proc   {
+      post login_path(name: "gogol bordello")
+      post vote_path(works(:baby).id)
+    }.must_change 'Vote.all.count', 1
+  end
 end
