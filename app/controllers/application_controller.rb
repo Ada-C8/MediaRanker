4a63ef82135b2e702fd41ea2c49468f2d3d38520
Bennett Rahn <rahnibennett@gmail.com>
2017-10-15 15:16:43 -0700

    flash messages for user login stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,12 @@ protected
     result = save
     if result
       flash[:status] = :success
-      flash[:message] = "Successfully #{edit} #{model.category} #{model.id}"
+      specific = model.class == User ? "user #{model.name} with ID" : "#{model.category}"
+      flash[:message] = "Successfully #{edit} #{specific} #{model.id}"
     else
       flash.now[:status] = :failure
-      flash.now[:message] = "A problem occurred: Could not create #{model.category}"
+      specific = model.class == User ? "log in" : "create #{model.category}"
+      flash.now[:message] = "A problem occurred: Could not #{specific}"
       flash.now[:details] = model.errors.messages
       return false
     end
