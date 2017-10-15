7297a8a94c6846592031d21a3f697569ff4384f2
Canaan West <canaan.west@gmail.com>
2017-10-15 11:58:05 -0700

    updating testing for users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,38 +2,34 @@ require "test_helper"
 
 describe UsersController do
   let(:west) {users(:west)}
-  it "should get index" do
+  it "should show all users on the index page" do
     get users_path
     must_respond_with :success
   end
 
-  it "should get show" do
+  it "should show the details for a specific user on the show page" do
     get user_path(users(:west).id)
     must_respond_with :success
   end
 
-  it "should get new" do
+  it "should get a form to create a new user" do
     get new_user_path
     must_respond_with :success
   end
 
-  it "should get create" do
-    # count = users.count
-    post users_path, params: {user: {name: "new user"}}
+  it "should be able to create a new user when provided with a name" do
+    proc {post users_path, params: {user: {name: "new user"}}}.must_change 'User.count', 1
     must_respond_with :redirect
     must_redirect_to users_path
-    # users.count.must_equal count+1
   end
 
-  it "should get edit" do
+  it "should not be able to create a new user if no name is provided" do
+    proc {post users_path, params: {user: {name: nil}}}.must_change 'User.count', 0
+  end
+
+  it "should get and edit form" do
     get edit_user_path(users(:west).id)
     must_respond_with :success
-  end
-
-  it "should get update" do
-    patch user_path(users(:west).id), params: {user: {name: "Update User"}}
-    must_respond_with :redirect
-    must_redirect_to user_path
   end
 
   it "should get a login form" do
@@ -43,13 +39,14 @@ describe UsersController do
 
   it "should log a user in when that user already exists" do
     proc { post login_path params: {user: {name: users(:west).name}}}.must_change 'User.count', 0
-
+    session[:logged_in_user].must_equal users(:west)
     must_respond_with :redirect
     must_redirect_to users_path
   end
 
   it "should create a new user when a non-existent username is entered" do
-    proc {post login_path params: {user: {name: "New User!"}}}.must_change 'User.count', 1
+    proc { post login_path params: {user: {name: "New User!"}}}.must_change 'User.count', 1
+    session[:logged_in_user][:name].must_equal "New User!"
     must_respond_with :redirect
     must_redirect_to users_path
 
