1c965588ab2b394d407ca10904a0fa93c3f1dc6d
Eva Sawyer <esawyer25@gmail.com>
2017-10-15 15:24:56 -0700

    testing deployment

--- app/models/work.rb
+++ app/models/work.rb
@@ -6,18 +6,14 @@ class Work < ApplicationRecord
 
   def self.top_ten(media_type)
     works = Work.all.where(category: media_type)
-    if works
       vote_hash = {}
 
       top_ten = works.all.sort_by { |work| -work.vote.count}[0..9]
       return top_ten
-    end
   end
 
   def self.top_work
-    if Work.all
       Work.all.sort_by { |work| -work.vote.count}.first
-    end
   end
 
 end
