3bede695080d6929503fc23dbfa94366c8491bf1
Angela Wilson <wilsonange@gmail.com>
2017-10-14 19:47:24 -0700

    create helper method find_work_by_params_id for show method in work controller. apply this helper method as before_actio to show, edit, update, and destroy.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,6 @@
 class WorksController < ApplicationController
+  before_action :find_work_by_params_id, only: [:show, :edit, :update, :destroy]
+
   def index
     @works = Work.all
   end
@@ -20,25 +22,26 @@ class WorksController < ApplicationController
     end
   end
 
-  def show
+  def show ; end
 
-  end
+  def edit ; end
 
-  def edit
+  def update ; end
 
-  end
-
-  def update
-
-  end
-
-  def destroy
-
-  end
+  def destroy ; end
 
   private
 
   def work_params
     return params.require(:work).permit(:catergory, :title, :creator, :publication_year, :description)
+  end
+
+  def find_work_by_params_id
+    @work = Work.find_by(id: params[:id])
+
+    unless @work
+      head :not_found
+    end
+
   end
 end
