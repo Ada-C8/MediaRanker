8a01796b2093bcb851a4edd0aad71e55882bd150
Maria McGrew <mcgmar1@outlook.com>
2017-10-14 21:05:54 -0700

    added works model testing - passing

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,6 +2,5 @@ class Work < ApplicationRecord
   validates :category, presence: true
   validates :title, presence: true
   validates :creator, presence: true
-  validates :publication_year, presence: true, numericality: true
-  validates :creator, presence: true
+  validates :publication_year, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,8 +2,47 @@ require "test_helper"
 
 describe Work do
   let(:work) { Work.new }
+  let(:almost) { works(:almost) }
+  let(:gatsby) { works(:gatsby) }
+  let(:thriller) { works(:thriller) }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  it "must have category to be valid" do
+    almost.valid?.must_equal true
+    almost.category = nil
+    almost.valid?.must_equal false
   end
+
+  it "must have title to be valid" do
+    almost.valid?.must_equal true
+    almost.title = nil
+    almost.valid?.must_equal false
+  end
+
+  it "must have creator to be valid" do
+    almost.valid?.must_equal true
+    almost.creator = nil
+    almost.valid?.must_equal false
+  end
+
+  it "must have publication year to be valid" do
+    almost.valid?.must_equal true
+    almost.publication_year = nil
+    almost.valid?.must_equal false
+  end
+
+  it "must have a publication year that is a number" do
+    almost.valid?.must_equal true
+    almost.publication_year = "yellow"
+    almost.valid?.must_equal false
+  end
+  
+  it "must have publication year must be greater than 0" do
+    almost.valid?.must_equal true
+    almost.publication_year = -1
+    almost.valid?.must_equal false
+    almost.publication_year = 0
+    almost.valid?.must_equal false
+  end
+
+
 end
