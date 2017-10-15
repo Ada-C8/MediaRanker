97be49c6e6fc2de55970db2b3b46bc9755c70a48
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-15 15:45:50 -0700

    Expanded vote model tests.

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -11,9 +11,27 @@ describe Vote do
     vote.user_id = users(:test_user).id
     vote.save
     vote.valid?.must_equal false
+
     vote.work_id = works(:test_book).id
     vote.save
     vote.valid?.must_equal true
   end
 
+  it "is not valid if it is missing a user_id" do
+    vote = Vote.new
+    vote.work_id = works(:test_book).id
+    vote.save
+    vote.valid?.must_equal false
+
+    vote.user_id = users(:test_user).id
+    vote.save
+    vote.valid?.must_equal true
+  end
+
+  it "assigns a vote to a work" do
+    work = Work.create!(category: "book", title: "This is a book", creator: "An author")
+    vote = Vote.new(user_id: users(:test_user).id, work_id: work.id)
+    vote.save
+    work.votes.count.must_equal 1
+  end
 end
