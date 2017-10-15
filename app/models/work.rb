7ef3fa57e9dc916fdd64089c626465a7838fa705
Gale Harrington <gale.harrington19@gmail.com>
2017-10-15 12:50:57 -0700

    Seperate out scopes for top 10 movies, books, and albums in works.rb

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,7 +4,13 @@ class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: true
 
   has_many :votes
-  scope :top10, -> {
-    select("works.id, count(votes.id) AS votes_count").left_joins(:votes).group("works.id").order("votes_count DESC").limit(10)
+  scope :top10movies, -> {
+    where(category: "movie").select("works.id, count(votes.id) AS votes_count").left_joins(:votes).group("works.id").order("votes_count DESC").limit(10)
+  }
+  scope :top10books, -> {
+    where(category: "book").select("works.id, count(votes.id) AS votes_count").left_joins(:votes).group("works.id").order("votes_count DESC").limit(10)
+  }
+  scope :top10albums, -> {
+    where(category: "album").select("works.id, count(votes.id) AS votes_count").left_joins(:votes).group("works.id").order("votes_count DESC").limit(10)
   }
 end
