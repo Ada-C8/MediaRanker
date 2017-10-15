a77fc4b4f695e4f8b1c8191a6d8c8513e609da69
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-15 10:07:51 -0700

    Complete user controller testing for login

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 describe UsersController do
   let(:one) { users(:one) }
+  let(:two) { users(:two) }
 
   it "should get index" do
     get users_path
@@ -24,13 +25,33 @@ describe UsersController do
     end
 
     it "should simply log in a user if their username exists and there is no user data in session" do
-      skip
-      #TODO: come back to this with form params
-      # post login_path(one.username)
-      # must_respond_with :redirect
-      # must_redirect_to root_path
-      # session[:user_id].must_equal one.id
-      # flash[:success].must_equal "Successfully logged in as existing user userone"
+      post login_path, params: {username: one.username}
+      must_respond_with :redirect
+      must_redirect_to root_path
+      session[:user_id].must_equal one.id
+      flash[:success].must_equal "Successfully logged in as existing user userone"
+    end
+
+    it "should not allow a user to log in when they are already logged in" do
+      post login_path, params: {username: one.username}
+      session[:user_id].must_equal one.id
+      flash[:success].must_equal "Successfully logged in as existing user userone"
+      post login_path, params: {username: two.username}
+      must_respond_with :redirect
+      must_redirect_to root_path
+      flash[:error].must_equal "You are already logged in as #{one.username}"
+      session[:user_id].must_equal one.id
+    end
+
+    it "should create a new user when the username does not exist" do
+      proc{
+        post login_path, params: {username: "newuser"}
+      }.must_change 'User.count', +1
+      must_respond_with :redirect
+      must_redirect_to root_path
+      newuser = User.find_by(username: "newuser")
+      flash[:success].must_equal "Successfully created new user newuser with ID #{newuser.id}"
+      session[:user_id].must_equal newuser.id
     end
   end
 end
