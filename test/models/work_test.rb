f31502af521e5827fb021ab78cc33b7e05efed8a
Angela Wilson <wilsonange@gmail.com>
2017-10-15 13:14:54 -0700

    write tests for work model's validations and description limit

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 describe Work do
   let(:work) { Work.new }
   describe "validations" do
+
+    #for the following tests I understand the are 'wet' and could be DRYed. At the moment, I am focuesed on completing the assignment. If there is time I will go back and figure out how to utilize the DRYing method taught in class.
     it "create work with all fields" do
       # work = Work.new(
       #   category: "album",
@@ -17,14 +19,50 @@ describe Work do
 
 
 
-    # it "requires category" do
-    #   work = Work.new
-    #
-    #   result = work.valid?
-    #   result.must_be false
-    #   work.errors.messages.must_include :category
-    #
-    #
-    # end
+    it "requires category" do
+      work = Work.new
+
+      result = work.valid?
+      result.must_be false
+      work.errors.messages.must_include :category
+    end
+
+    it "requires title" do
+      work = Work.new
+
+      result = work.valid?
+      result.must_be false
+      work.errors.messages.must_include :title
+    end
+  end # end describe
+
+  describe "description" do
+    it "rejects descriptions >500 char" do
+      desc = "a" * 501
+
+      work = Work.new(description: desc)
+
+      result = work.valid?
+      result.must_be false
+      work.errors.messages.must_include :description
+    end # end it
+
+    it "allows description <= 500 char" do
+      descriptions = [
+        "a" * 500,
+        "a" * 10,
+        "a" * 0
+      ]
+
+      descriptions.each do |desc|
+        result = Work.new(category: "album", title: "test", description: desc)
+
+        result.must_be :valid?
+      end
+    end
+  end # end description
+
+  describe "" do
+
   end
 end
