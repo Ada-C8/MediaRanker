b28d3c3be5b62fc17cccc22a7ec17c9b885c2fae
Amy Cash <amymcash@gmail.com>
2017-10-15 13:06:44 -0700

    Update controller methods

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,5 @@
 class WorksController < ApplicationController
-  before_action :get_work , only: [:show, :edit, :update, :destroy]
+  # before_action :get_work , only: [:update, :destroy]
 
   def get_work
     @work = Work.find(params[:id])
@@ -11,6 +11,7 @@ class WorksController < ApplicationController
 
   def show
     # @work = Work.find(params[:id])
+    find_work_by_params_id
   end
 
   def new
@@ -19,70 +20,90 @@ class WorksController < ApplicationController
 
   def create
     @work = Work.new(work_params)
+
     if @work.save
       flash[:status] = :success
       flash[:message] = "Successfully created work #{@work.id}"
       redirect_to works_path
+      return
     else
       flash.now[:status] = :failure
       flash.now[:message] = "Failed to create new work"
+      flash.now[:details] = @work.errors.messages
       render :new, status: :bad_request
     end
   end
 
-  # @book = Book.new(book_params)
-
-  # if @book.save
-  #   flash[:status] = :success
-  #   flash[:message] = "Successfully created book #{@book.id}"
-  #   redirect_to books_path
-  # else
-    # Tell the user what went wrong
-    # flash.now[:status] = :failure
-    # flash.now[:message] = "Failed to create book"
-    # flash.now[:details] = @work.errors.messages
-    # render :new, status: :bad_request
-  # end
-# end
-
-
-
-
   def edit
     # @work = Work.find(params[:id])
+    find_work_by_params_id
   end
 
   def update
     # @work = Work.find(params[:id])
-    @work.update_attributes(work_params)
-    @work.save
-    redirect_to works_path
+    if find_work_by_params_id
+      @work.update_attributes(work_params)
+      if @work.save
+        redirect_to work_path(@work)
+        return
+      else
+        render :edit, status: :bad_request
+        return
+      end
+    end
   end
 
   def destroy
-    # @work = Work.find(params[:id])
-    @work.destroy
-    redirect_to works_path
+    if find_work_by_params_id
+      @work.destroy
+      redirect_to works_path
+      return
+    else
+      head :not_found
+      return
+    end
   end
+  # @work = Work.find(params[:id])
+  #  unless @work
+  #    head :not_found
+  #  enda
+  # @work.destroy
+  # redirect_to works_path
 
-  def topten
+
+def topten
+end
+
+
+private
+
+def work_params
+  return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+end
+
+def find_work_by_params_id
+  @work = Work.find_by(id: params[:id])
+  unless @work
+    head :not_found
   end
+  return @work
+end
 
+end
 
-
-  # def upvote
-  #   @work = Work.find(params[:id])
-  #   @work.votes.create
-  #   redirect_to new_vote_path
-  #
-  #   # render :topten
-  #   #want to create a vote, which includes this id as the work id and the logged in user id as the user-id
-  #   # @work.votes.create
-  #   # redirect_to works_index_path
-  #   # get 'works/:id/upvote', to: 'works#upvote', as: 'upvote_work'
-  #   #
-  #
-  # end
+# def upvote
+#   @work = Work.find(params[:id])
+#   @work.votes.create
+#   redirect_to new_vote_path
+#
+#   # render :topten
+#   #want to create a vote, which includes this id as the work id and the logged in user id as the user-id
+#   # @work.votes.create
+#   # redirect_to works_index_path
+#   # get 'works/:id/upvote', to: 'works#upvote', as: 'upvote_work'
+#   #
+#
+# end
 
 #   def upvote
 #   @startup = Startup.find(params[:id])
@@ -90,11 +111,3 @@ class WorksController < ApplicationController
 #   upvote.save
 #   redirect_to startups_path
 # end
-
-  private
-
-  def work_params
-    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
-  end
-
-end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -15,206 +15,183 @@ describe WorksController do
 
   end #index tests
 
-    describe "new" do
-      it "gets a new form" do
-        get new_work_path
-        must_respond_with :success
+  describe "new" do
+    it "gets a new form" do
+      get new_work_path
+      must_respond_with :success
+    end
+  end #new tests
+
+
+
+  describe "create" do
+    it "adds the work to the DB and redirects when the work data is valid" do
+      # Arrange
+      work_data = {
+        work: {
+          title: "Test work"
+        }
+      }
+      # Test data should result in a valid work, otherwise
+      # the test is broken
+      Work.new(work_data[:work]).must_be :valid?
+
+      start_work_count = Work.count
+
+      # Act
+      post works_path, params: work_data
+
+      # Assert
+      must_respond_with :redirect
+      must_redirect_to works_path
+
+      Work.count.must_equal start_work_count + 1
+    end #adds work test
+
+    it "sends bad_request when the work data is bogus" do
+      # Arrange
+      invalid_work_data = {
+        work: {
+          # NO TITLE!!!
+          creator: "Fake Creator"
+        }
+      }
+      # Double check the data is truly invalid
+      Work.new(invalid_work_data[:work]).wont_be :valid?
+
+      start_work_count = Work.count
+
+      # Act
+      post works_path, params: invalid_work_data
+
+      # Assert
+      must_respond_with :bad_request
+      # Vanilla rails doesn't provide any way to do this
+      # assert_template :new
+      Work.count.must_equal start_work_count
+    end #bad request
+  end #create
+
+
+
+  describe "show" do
+    it "returns success when given a valid work ID" do
+      # Arrange
+      work_id = Work.first.id
+
+      # Act
+      get work_path(work_id)
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "returns not_found when given an invalid book ID" do
+      invalid_work_id = Work.last.id + 1
+      get work_path(invalid_work_id)
+      must_respond_with :not_found
+    end
+  end
+
+  describe "edit" do
+    it "returns success when given a valid work ID" do
+      # Arrange
+      work_id = Work.first.id
+
+      # Act
+      get edit_work_path(work_id)
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "returns not_found when given an invalid work ID" do
+      invalid_work_id = Work.last.id + 1
+      get edit_work_path(invalid_work_id)
+      must_respond_with :not_found
+    end
+  end
+
+
+  describe "update" do
+    it "returns success if the work ID is valid and the change is valid" do
+      work = Work.first
+      work_data = {
+        work: {
+          title: "New title",
+          creator: "New Creator"
+        }
+      }
+      work.update_attributes(work_data[:work])
+      work.must_be :valid?
+
+      patch work_path(work), params: work_data
+
+      must_respond_with :redirect
+      must_redirect_to work_path
+
+      # Check that the change went through
+      work.reload
+      work.title.must_equal work_data[:work][:title]
+    end
+
+    it "returns not_found if the book ID is invalid" do
+      invalid_work_id = Work.last.id + 1
+      work_data = {
+        work: {
+          title: "New title",
+          creator: "New Creator"
+        }
+      }
+
+      patch work_path(invalid_work_id), params: work_data
+
+      must_respond_with :not_found
+    end
+
+    it "returns bad_request if the change is invalid" do
+      work = Work.first
+      invalid_work_data = {
+        work: {
+          title: ""
+        }
+      }
+      # Check that the update is actually invalid
+      work.update_attributes(invalid_work_data[:work])
+      work.wont_be :valid?
+
+      patch work_path(work), params: invalid_work_data
+
+      must_respond_with :bad_request
+
+      work.reload
+      work.title.wont_equal invalid_work_data[:work][:title]
+    end
+  end
+
+    describe "destroy" do
+      it "returns success and destroys the book when given a valid book ID" do
+        # Arrange
+        work_id = Work.first.id
+
+        # Act
+        delete work_path(work_id)
+
+        # Assert
+        must_respond_with :redirect
+        must_redirect_to works_path
+        Work.find_by(id: work_id).must_be_nil
       end
 
-      # it "works without an author id" do
-      #   get new_book_path
-      #   must_respond_with :success
-      # end
-    end #new tests
+      it "returns not_found when given an invalid book ID" do
+        invalid_work_id = Work.last.id + 1
 
+        start_work_count = Work.count
 
+        delete work_path(invalid_work_id)
 
-      describe "create" do
-        it "adds the work to the DB and redirects when the work data is valid" do
-          # Arrange
-          work_data = {
-            work: {
-              title: "Test work"
-            }
-          }
-          # Test data should result in a valid work, otherwise
-          # the test is broken
-          Work.new(work_data[:work]).must_be :valid?
-
-          start_work_count = Work.count
-
-          # Act
-          post work_path, params: work_data
-
-          # Assert
-          must_respond_with :redirect
-          must_redirect_to works_path
-
-          Work.count.must_equal start_work_count + 1
-        end #adds work test
-
-            it "sends bad_request when the work data is bogus" do
-              # Arrange
-              invalid_work_data = {
-                work: {
-                  # NO TITLE!!!
-                  creator: "Fake Creator"
-                }
-              }
-              # Double check the data is truly invalid
-              Work.new(invalid_work_data[:work]).wont_be :valid?
-
-              start_work_count = Work.count
-
-              # Act
-              post works_path, params: invalid_work_data
-
-              # Assert
-              must_respond_with :bad_request
-              # Vanilla rails doesn't provide any way to do this
-              # assert_template :new
-              Work.count.must_equal start_work_count
-            end #bad request
-          end #create
-
-end#all tests
-
-
-#   describe "index" do
-
-#
-
-#
-#     it "returns a success status when given a valid author_id" do
-#       get author_books_path(Author.first)
-#       must_respond_with :success
-#     end
-#
-#     it "returns something when given a bogus author_id" do
-#       bad_author_id = Author.last.id + 1
-#       get author_books_path(bad_author_id)
-#       must_respond_with :not_found
-#     end
-#   end
-#
-
-#   describe "show" do
-#     it "returns success when given a valid work ID" do
-#       # Arrange
-#       book_id = Book.first.id
-#
-#       # Act
-#       get book_path(book_id)
-#
-#       # Assert
-#       must_respond_with :success
-#     end
-#
-#     it "returns not_found when given an invalid book ID" do
-#       invalid_book_id = Book.last.id + 1
-#       get book_path(invalid_book_id)
-#       must_respond_with :not_found
-#     end
-#   end
-#
-#   describe "edit" do
-#     it "returns success when given a valid book ID" do
-#       # Arrange
-#       book_id = Book.first.id
-#
-#       # Act
-#       get edit_book_path(book_id)
-#
-#       # Assert
-#       must_respond_with :success
-#     end
-#
-#     it "returns not_found when given an invalid book ID" do
-#       invalid_book_id = Book.last.id + 1
-#       get edit_book_path(invalid_book_id)
-#       must_respond_with :not_found
-#     end
-#   end
-#
-#   describe "update" do
-#     it "returns success if the book ID is valid and the change is valid" do
-#       book = Book.first
-#       book_data = {
-#         book: {
-#           title: "Changed title",
-#           author_id: book.author_id
-#         }
-#       }
-#       book.update_attributes(book_data[:book])
-#       book.must_be :valid?
-#
-#       patch book_path(book), params: book_data
-#
-#       must_respond_with :redirect
-#       must_redirect_to book_path(book)
-#
-#       # Check that the change went through
-#       book.reload
-#       book.title.must_equal book_data[:book][:title]
-#     end
-#
-#     it "returns not_found if the book ID is invalid" do
-#       invalid_book_id = Book.last.id + 1
-#       book_data = {
-#         book: {
-#           title: "Changed title",
-#           author_id: Author.first.id
-#         }
-#       }
-#
-#       patch book_path(invalid_book_id), params: book_data
-#
-#       must_respond_with :not_found
-#     end
-#
-#     it "returns bad_request if the change is invalid" do
-#       book = Book.first
-#       invalid_book_data = {
-#         book: {
-#           title: ""
-#         }
-#       }
-#       # Check that the update is actually invalid
-#       book.update_attributes(invalid_book_data[:book])
-#       book.wont_be :valid?
-#
-#       patch book_path(book), params: invalid_book_data
-#
-#       must_respond_with :bad_request
-#
-#       book.reload
-#       book.title.wont_equal invalid_book_data[:book][:title]
-#     end
-#   end
-#
-#   describe "destroy" do
-#     it "returns success and destroys the book when given a valid book ID" do
-#       # Arrange
-#       book_id = Book.first.id
-#
-#       # Act
-#       delete book_path(book_id)
-#
-#       # Assert
-#       must_respond_with :redirect
-#       must_redirect_to books_path
-#       Book.find_by(id: book_id).must_be_nil
-#     end
-#
-#     it "returns not_found when given an invalid book ID" do
-#       invalid_book_id = Book.last.id + 1
-#
-#       start_book_count = Book.count
-#
-#       delete book_path(invalid_book_id)
-#
-#       must_respond_with :not_found
-#       Book.count.must_equal start_book_count
-#     end
-#   end
-# end
+        must_respond_with :not_found
+        Work.count.must_equal start_work_count
+      end
+    end
+  end
