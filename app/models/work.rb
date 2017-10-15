860e46bb68c5af2d4f120fbd105f72866808c1c4
Jan Edrozo <jedrozo@gmail.com>
2017-10-15 00:07:03 -0700

    Updated Work Model with category method (drafting)

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,10 @@ class Work < ApplicationRecord
   has_many :votes
 
   validates :title, presence: true
+
+  def self.sort_by_category(category)
+    works_of_category = Work.where(category: category)
+
+    return works_of_category
+  end
 end
