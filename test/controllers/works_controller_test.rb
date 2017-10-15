e2b8c8a5e702a1f68d2131e0bafbd49128d6e4ae
murog <guillermina.muro@gmail.com>
2017-10-14 23:47:54 -0700

    updated work controller tests

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,39 +1,37 @@
 require "test_helper"
 
+
 describe WorksController do
-  it "should get index" do
-    get works_index_url
-    value(response).must_be :success?
+  it "going to the works index is successful" do
+    get works_path
+    must_respond_with :success
   end
 
-  it "should get show" do
-    get works_show_url
-    value(response).must_be :success?
+  it "should be able to visit an individual work show page" do
+    get work_path(works(:bonito).id)
+    must_respond_with :success
   end
 
-  it "should get edit" do
-    get works_edit_url
-    value(response).must_be :success?
+  it "should be able to create a work" do
+    proc {
+      post works_path, params: {work: {title: "bury me at makeout creek", creator: "mitski", category: "album", publication_year: 2014, description: "i dont smoke"}}
+    }
+    must_respond_with :redirect
+    must_redirect_to works_path
   end
 
   it "should get update" do
-    get works_update_url
-    value(response).must_be :success?
+    get edit_work_path(works(:bonito).id)
+    must_respond_with :success
   end
 
   it "should get new" do
-    get works_new_url
-    value(response).must_be :success?
+    get new_work_path
+    must_respond_with :success
   end
 
-  it "should get create" do
-    get works_create_url
-    value(response).must_be :success?
-  end
 
-  it "should get destroy" do
-    get works_destroy_url
-    value(response).must_be :success?
-  end
+
+
 
 end
