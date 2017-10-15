2863fe7cecc839286cb8a68f4e7f90cf1c1f6f3c
Irene DeVera <irenneeyyy@gmail.com>
2017-10-15 15:23:37 -0700

    Add tests to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,17 +4,24 @@ describe User do
   let :user { User.new }
 
   describe "validations" do
-    it "can be created with all fields - name" do
+    it "can be created with all valid fields" do
+      before_count = User.count
       user.wont_be :valid?
       user[:name] = "Test First User Name"
 
+      user.save!
       user.must_be :valid?
       user.name.must_equal "Test First User Name"
+      User.count.must_equal before_count + 1
     end
 
-    it "should be invalid if username is blank or has no entry" do
+    it "should be invalid if username is has no entry" do
+      before_count = User.count
+      user.save
+
       user.wont_be :valid?
       user.errors.messages.must_include :name
+      User.count.must_equal before_count
     end
 
     it "has a username" do
@@ -31,16 +38,33 @@ describe User do
       user.save!
 
       User.count.must_equal 1
+      user.errors.messages.wont_include :name
     end
   end # Describe
 
   describe "uniqueness" do
     it "must raise on error and cannot be saved if the username already exists" do
-      # Create a new User instance other than the one already in the db
+      before_count = User.count
       user[:name] = "Owly"
       user.save
 
       user.wont_be :valid?
+      user.errors.messages.must_include :name
+      User.count.must_equal before_count
+    end
+  end
+
+  describe "relations" do
+    it "must respond to votes" do
+      user = users(:one)
+      work = works(:book)
+      before_vote_count = user.votes.count
+
+      new_vote = Vote.create!(user_id: user.id, work_id: work.id)
+
+      user.must_respond_to :votes
+      user.votes.must_include new_vote
+      user.votes.count.must_equal before_vote_count + 1
     end
   end
 
