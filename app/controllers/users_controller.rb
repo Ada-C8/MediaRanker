31d6f4416c2d086a8efaba001426bed752c2db1a
Jessica Owens <vertige@gmail.com>
2017-10-15 14:33:22 -0700

    deleted extraneous methods and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,6 @@ class UsersController < ApplicationController
     @users = User.order(:username)
   end
 
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      flash[:success] = "Successfully created User #{@user.username}!"
-      redirect_to user_path(@user.id)
-    else
-      flash.now[:error] = "A problem occurred: Could not create User #{@user.username}"
-      render :new, status: :bad_request
-    end
-  end
-
   def show
     @user = User.find_by(id: params[:id])
     render_404 unless @user
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,23 +9,6 @@ describe UsersController do
       must_respond_with :success
     end
 
-    it "should display a form for a new User" do
-      get new_user_path
-      must_respond_with :success
-    end
-
-    describe "create" do
-      it "should create a new User if input is valid" do
-        proc { post users_path, params: { user: { username: "A Name" } } }.must_change 'User.count', 1
-        must_respond_with :redirect
-      end
-
-      it "re-renders the new form if input is invalid" do
-        proc { post users_path, params: { user: { username: nil } } }.must_change 'User.count', 0
-        must_respond_with :bad_request
-      end
-    end #create
-
     describe "show" do
       it "should get user detail page (#show) or render a 404" do
         get user_path(user1.id)
