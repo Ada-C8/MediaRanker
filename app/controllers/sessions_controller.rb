04aa13a64593b42657aa27c6296aec2c6c8a5aa5
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-15 15:13:15 -0700

    Added logout logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,4 +19,10 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def logout
+    session[:user] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
+  end
+
 end
