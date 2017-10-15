d3e92f8518b92926e8b31da945e3bdb3fb3f9035
Gale Harrington <gale.harrington19@gmail.com>
2017-10-15 12:41:16 -0700

    Add scope to work.rb to list top10 works

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,4 +4,7 @@ class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: true
 
   has_many :votes
+  scope :top10, -> {
+    select("works.id, count(votes.id) AS votes_count").left_joins(:votes).group("works.id").order("votes_count DESC").limit(10)
+  }
 end
