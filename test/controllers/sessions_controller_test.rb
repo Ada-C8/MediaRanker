c5f37cdada232cf557e0d366e3d76b19ec1eb982
Tanja Stroble <plusdeltah@gmail.com>
2017-10-15 00:03:48 -0700

    added sessions test and crashed other tests because of vote_count db that I dropped

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 describe SessionsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "should get login page" do
+    get login_path
+    must_respond_with :success
+  end
+
+  it "recognizes existing user" do
+    user = users(:tanja)
+    post login_path(name:user.name)
+    must_respond_with :redirect
+    must_redirect_to root_path
+  end
+
+  it "recognizes new user" do
+    post login_path(name:"gogol bordello")
+    must_respond_with :redirect
+    must_redirect_to root_path
+  end
+
+  it "should render login form for invalid name" do
+    post login_path(name:"")
+    must_respond_with :success
+  end
+
+  it "invalid user login doesn't change user count" do
+    proc {
+      post login_path("")
+    }.must_change 'User.count', 0
+  end
+
+  it "should allow user to logout" do
+    delete logout_path
+    must_respond_with :found
+    must_respond_with :redirect
+    must_redirect_to root_path
+  end
 end
