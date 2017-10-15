fd19994b09b0f5caa2e5fa46a946e1ac343057d5
Angela Wilson <wilsonange@gmail.com>
2017-10-15 08:53:38 -0700

    complete Show view for Work

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,6 +4,8 @@ class Work < ApplicationRecord
   validates :category, presence: { message: "Must have category" }
   validates :title, presence: { message: "Must have a title" }
 
+
+# feedback from rideshare noted using an instance variable instead of .self. I worked on using @blah and was unsuccessful.
   def self.spotlight
     works = Work.all
     spotlight = works.max_by {|work| work.votes.count}
@@ -16,7 +18,7 @@ class Work < ApplicationRecord
   end
 
   def self.top_ten(category)
-    top_ten = self.sort(category)
+    top_ten = self.sort(category)[0..9]
     return top_ten
   end
 end
