d71c8f6bcd6fb046279b3e1a85545c4ff528c2e3
Jan Edrozo <jedrozo@gmail.com>
2017-10-15 02:33:17 -0700

    Added Login Form and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,19 @@ class UsersController < ApplicationController
 
   def create
   end
+
+  def login_form
+  end
+
+  def login
+
+      user = User.find_by(name: params[:name])
+      if user
+        session[:user_id] = user.id
+        flash[:message] = "Successfully logged in as existing user #{ user.name }"
+        redirect_to root_path
+      else
+        render :login_form
+      end
+  end
 end
