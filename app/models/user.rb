6dc92bbcdc4525b73847e3c5486348fc720e2d85
Amy Lee <amy.yj.lee@gmail.com>
2017-10-16 00:03:46 -0700

    Fixed issue in model from using delete instead of destroy when establishing dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :votes, dependent: :delete
+  has_many :votes, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
   #will automatically login if the username is not unique
