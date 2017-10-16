173e159a2577dbc4612dbb173253cd64d1cd514a
Irene DeVera <irenneeyyy@gmail.com>
2017-10-16 03:22:41 -0700

    Change date on schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011045500) do
+ActiveRecord::Schema.define(version: 20171016100646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20171011045500) do
   create_table "votes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "work_id"
-    t.datetime "created_at", null: false
+    t.date "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
