543d7e14cd428881fce5766b63da863bbcc07a99
Tanisha <tanishahampden@gmail.com>
2017-10-16 00:22:08 -0700

    add 2 columns to votes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016041233) do
+ActiveRecord::Schema.define(version: 20171016071913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20171016041233) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "work_id"
+    t.integer "user_id"
   end
 
   create_table "works", force: :cascade do |t|
