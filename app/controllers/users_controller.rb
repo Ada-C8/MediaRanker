3c3427951847d2c1a1916cc706f5e941de312c2c
Angela Wilson <wilsonange@gmail.com>
2017-10-15 18:54:21 -0700

    user controller complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,55 @@
 class UsersController < ApplicationController
+  before_action :find_user_by_params_id, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
 
   def new
+    @user = User.new
 
+  end
+
+  def create
+    @user = User.new(user_parauser  )
+    if save_and_flash(@user)
+      redirect_to users_path
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  def show ; end
+
+  def edit ; end
+
+  def update
+    @user.update_attributes(user_params)
+    if save_and_flash(@user)
+      redirect_to user_path(@user)
+      return
+    else
+      render :edit, status: :bad_request
+      return
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    return params.require(:user).permit(:name)
+  end
+
+  def find_user_by_params_id
+    @user = User.find_by(id: params[:id])
+
+    unless @user
+      head :not_found
+    end
   end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -5,13 +5,16 @@ class WorksController < ApplicationController
     @works = Work.all
   end
 
+# I don't understand the new method's if statement code below. the structure is from rails library and I substituted the keywords to fit media ranker. however, I do not know what to insert into the square braces and why we are setting @work.category to params[:category]. Please do in-line comment with answer.
+# also i
   def new
     @work = Work.new
-    if params[:creator]
-      @work.creator = params[:creator]
-    end
+    # if params[:category]
+    #   @work.category = params[:category]
+    # end
   end
 
+  # the new media is not saving to DB. Not sure why. The code structure below is from rails library and theoretically should work. I am still unfamiliar with binding.pry for debugging and pry in the terminal is only so useful. 
   def create
     @work = Work.new(work_params
     )
@@ -47,7 +50,7 @@ class WorksController < ApplicationController
   private
 
   def work_params
-    return params.require(:work).permit(:catergory, :title, :creator, :publication_year, :description)
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 
   def find_work_by_params_id
