a00addcb28ae3e34f3942b4038bf6b581c7e2395
Jessica Owens <vertige@gmail.com>
2017-10-16 11:33:18 -0700

    added biew for oauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -35,7 +35,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    auth_hash = request.env['omniauth.auth']
-    puts auth_hash
+    @auth_hash = request.env['omniauth.auth']
+
+    @user = User.find_by(uid: @auth_hash['uid'], provider: @auth_hash['provider'])
+
+    if @user
+      session[:logged_in_as_user] = @user.id
+      flash[:success] = "Successfully logged in as existing user #{@user.username}"
+    else
+      @user = User.new(uid: @auth_hash['uid'], provider: @auth_hash['provider'], name: @auth_hash['info']['nickname'], email: @auth_hash['info']['email'])
+
+      if @user.save
+        session[:logged_in_as_user] = @user.id
+        flash[:success] = "Welcome #{@user.name}"
+      else
+        flash[:error] = "Unable to save user!"
+      end
+    end
+
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
 
   get 'logout', to: 'sessions#logout'
 
-  get "/auth/:provider/callback", to: "sessions#create"
+  get 'auth/:provider/callback', to: 'sessions#create'
 
   post 'votes', to: 'votes#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
