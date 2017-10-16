92f81ec3600c09b0b6d9cc4c41ebf6d0d7620d09
Jan Edrozo <jedrozo@gmail.com>
2017-10-16 01:44:10 -0700

    voting buttons and error message drafting

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,16 +1,23 @@
 class VotesController < ApplicationController
+
+  def new
+    @vote = Vote.new
+  end
+
   def create
 
-  @vote = Vote.new(user_id:session[:user_id], work_id:params[:id])
+    @vote = Vote.new(user_id:session[:user_id], work_id:params[:id])
+    # vote.user_id = session[:user_id]
+
     if @vote.save
       flash[:success] = "Successfully upvoted!"
       redirect_to work_path(params[:id])
     else
       error = @vote.errors.messages
       if !error[:user].empty?
-        flash[:error] = "You must log in to do that"
+        flash.now[:error] = "You must log in to do that"
       else
-        flash[:error] = "You can't upvote more than once!"
+        flash.now[:error] = "You can't upvote more than once!"
       end
       redirect_to work_path(params[:id])
     end
