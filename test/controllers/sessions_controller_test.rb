e1f13ec1b2e4c17570c25c0dc7526c83ba09819b
murog <guillermina.muro@gmail.com>
2017-10-15 21:52:55 -0700

    failing create user on login test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 describe SessionsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+it "going to the login_form page is successful" do
+  get login_path
+  must_respond_with :success
+end
+
+it "should be able to create a user" do
+  proc {
+    post login_path, params: {user: {name: "alyssa"}}
+  }.must_change 'User.count', 1
+  must_respond_with :redirect
+  must_redirect_to root_path
+end
+
+# it "should be able to login as an existing user" do
+#
+# end
+
 end
