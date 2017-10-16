c7c6caee36beb29477ebebbec40008ec42134d03
Tanisha <tanishahampden@gmail.com>
2017-10-16 15:06:39 -0700

    fix whitespace

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,8 +1,9 @@
 class Work < ApplicationRecord
   has_many :votes
-  
+
   validates :title, presence: true
   validates :creator, presence: true
   validates :category, presence: true
   validates :description, length: { maximum: 500, message: "The description is too long" }
+
 end
