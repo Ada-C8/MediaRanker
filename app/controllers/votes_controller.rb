a793a4dce8b6a4861554da282e1e0de9c7d718a5
enigmagnetic <lauren.cardella@gmail.com>
2017-10-16 07:21:44 -0700

    Somehow lost vote functionality after merging branches. Voting not working.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -10,7 +10,7 @@ class VotesController < ApplicationController
         flash[:error] = "Vote failed to save"
       end
     elsif session[:user_id] && already_voted
-      flash[:error] = "You have already up-voted this work"
+      flash[:error] = "You have already voted for this work"
     else
       flash[:error] = "You must be logged in to vote"
     end
@@ -24,12 +24,3 @@ class VotesController < ApplicationController
     user.votes.any? { |vote| vote.work_id == params[:id] }
   end
 end
-
-# 2.4.0 :002 > Vote.create(work_id: 22, user_id: 1)
-#    (0.2ms)  BEGIN
-#   Work Load (0.3ms)  SELECT  "works".* FROM "works" WHERE "works"."id" = $1 LIMIT $2  [["id", 22], ["LIMIT", 1]]
-#   User Load (0.3ms)  SELECT  "users".* FROM "users" WHERE "users"."id" = $1 LIMIT $2  [["id", 1], ["LIMIT", 1]]
-#   SQL (0.9ms)  INSERT INTO "votes" ("created_at", "updated_at", "user_id", "work_id") VALUES ($1, $2, $3, $4) RETURNING "id"  [["created_at", "2017-10-16 12:11:34.028369"], ["updated_at", "2017-10-16 12:11:34.028369"], ["user_id", 1], ["work_id", 22]]
-#   SQL (1.0ms)  UPDATE "works" SET "votes_count" = COALESCE("votes_count", 0) + 1 WHERE "works"."id" = $1  [["id", 22]]
-#    (0.8ms)  COMMIT
-#  => #<Vote id: 2, created_at: "2017-10-16 12:11:34", updated_at: "2017-10-16 12:11:34", user_id: 1, work_id: 22>
