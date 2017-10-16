973d9d581fc7cecaf909ae7045e4d8400a27e362
Mariana <lmarianarp@gmail.com>
2017-10-16 00:01:05 -0700

    validate uniqueness votes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,4 +14,15 @@ describe User do
       u.errors.messages.must_include :name
     end
   end
+
+  describe "ralations" do
+    it 'can set the vote' do
+      w = works(:work1)
+      u = users(:user1)
+      u.must_respond_to :votes
+      u.votes.count.must_equal 0
+      v = Vote.create!(work_id: w.id, user_id: u.id)
+      u.votes.count.must_equal 1
+    end
+  end
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -23,5 +23,25 @@ describe Vote do
       v.errors.messages.must_include :work
       #como funciona esto?
     end
+
+    it 'unique couple work and user' do
+      w = works(:work1)
+      u = users(:user1)
+      v = Vote.create!(work_id: w.id, user_id: u.id)
+      v1 = Vote.new(work_id: w.id, user_id: u.id)
+      is_valid = v1.valid?
+      is_valid.must_equal false
+    end
   end
+
+  describe 'relations' do
+    it 'respond to user and work' do
+      #u = user(:user1)
+      v = votes(:one)
+      v.must_respond_to :user
+      v.must_respond_to :work
+    end
+  end
+
+
 end
