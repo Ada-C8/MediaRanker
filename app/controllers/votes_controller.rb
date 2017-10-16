e8dfa791c9cadef38059db75e14906e1b99f6bd9
Tanisha <tanishahampden@gmail.com>
2017-10-16 11:41:15 -0700

    add upvote controller action

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,10 @@
 class VotesController < ApplicationController
+  def upvote
+    vote = Vote.new
+    vote.user_id = session[:user_id]
+    vote.work_id = params[:id]
+    vote.save
+    flash[:success] = "You have successfully voted for this work"
+    redirect_to work_path
+  end
 end
