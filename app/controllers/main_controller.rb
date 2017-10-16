5357bae4345da2190c76f9776130eb68f8637373
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-15 21:11:05 -0700

    Fix case when there are no top works

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
   def index
     @work_top = Work.top_work
 
-    @top = Work::CATEGORIES.map { |category| Work.top_ten(category.downcase.to_sym) }
+    @top = Work::CATEGORIES.map { |category| [category, Work.top_ten(category.downcase.to_sym)] }.to_h
 
   end
 end
