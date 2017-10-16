9f2b0aade74afd3596d7ba92d0824b8e12cd46ab
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-15 17:38:09 -0700

    implemented all tables of users and votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   has_many :votes
   has_many :works, through: :votes
   validates :username, presence: true, uniqueness: true
+
+    def number_of_vote
+      vote_num = 0
+      votes.each do |vote|
+        vote_num += 1
+      end
+      return vote_num
+    end
+
+  
 end
