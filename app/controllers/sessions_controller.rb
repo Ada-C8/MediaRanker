9829f158ad4a55c013405a8e5fc8297ac288ded7
murog <guillermina.muro@gmail.com>
2017-10-15 21:45:24 -0700

    included strong params in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,41 @@
 class SessionsController < ApplicationController
+
+  def login_form
+  end
+
+  def login
+    if find_user(params[:username])
+      flash[:success] = "Successfully loggin in as #{@user.name}"
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      @user = User.new user_params
+      if @user.save
+        flash[:success] = "Successfully loggin in as #{@user.name}"
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        flash.now[:error] = "User not logged in successfully"
+        render :login_form
+      end
+    end
+  end
+end
+
+private
+def find_user(input_name)
+  @user = nil
+  all_users = User.all
+  all_users.each do |user|
+    @user = user if user.name == input_name
+  end
+  if @user
+    return @user
+  else
+    return nil
+  end
+end
+
+def user_params
+  return params.require(:user).permit(:name)
 end
