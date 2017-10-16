b3ae9fc8fd4458f2eef7689533735d2e041ae7ab
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-16 11:32:23 -0700

    Added vote_count for sorting (does not work)

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,4 @@
 class Vote < ApplicationRecord
-  belongs_to :work
+  belongs_to :work, counter_cache: :vote_count #https://stackoverflow.com/questions/16996618/rails-order-by-results-count-of-has-many-association
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016020338) do
+ActiveRecord::Schema.define(version: 20171016181734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20171016020338) do
     t.string "creator"
     t.integer "publication_year"
     t.string "description"
+    t.integer "vote_count"
   end
 
 end
