9e331fc43fa1fd950fa883e4531e7c53faa16c1f
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-16 08:04:57 -0700

    Added limit to work list in homepage

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -15,9 +15,9 @@ class WorksController < ApplicationController
       end
     end
 
-    @albums = Work.where("category = 'album'")
-    @books = Work.where("category = 'book'")
-    @movies = Work.where("category = 'movie'")
+    @albums = Work.where("category = 'album'").limit(10)
+    @books = Work.where("category = 'book'").limit(10)
+    @movies = Work.where("category = 'movie'").limit(10)
   end
 
   def show
