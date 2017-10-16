e112efb50ee4f12bd4a54626eba56a288b838dfd
Angela Wilson <wilsonange@gmail.com>
2017-10-16 11:58:05 -0700

    effot to push to heroku

--- db/migrate/20171013203749_add_creator_column_to_db.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatorColumnToDb < ActiveRecord::Migration[5.1]
-  def change
-    add_column :works, :creator, :string
-  end
-end
--- test/models/main_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-# describe Main do
-#   let(:main) { Main.new }
-#
-#   it "must be valid" do
-#     value(main).must_be :valid?
-#   end
-# end
