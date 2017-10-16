b56d94efba6cea2edf6ebb8efedeb64e11632dd5
Tanisha <tanishahampden@gmail.com>
2017-10-16 13:57:04 -0700

    update upvote method with proper session variable name

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,7 @@
 class VotesController < ApplicationController
   def upvote
     vote = Vote.new
-    vote.user_id = session[:user_id]
+    vote.user_id = session[:logged_in_user]
     vote.work_id = params[:id]
     vote.save
     flash[:success] = "You have successfully voted for this work"
