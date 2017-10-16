09cbe7e42300725d06dcd28bbf7d8f9ce49eb63a
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-16 12:17:01 -0700

    Added user-controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,25 @@
 require "test_helper"
 
 describe UsersController do
-  it "should get index" do
-    get users_index_url
-    value(response).must_be :success?
+  describe "User Index" do
+    it "should get index" do
+      get users_url
+      assert_response :success
+    end
   end
 
-  it "should get show" do
-    get users_show_url
-    value(response).must_be :success?
+
+  describe "User Show" do
+    it "should get the show page of a single user" do
+      get user_path(users(:potato).id)
+      assert_response :success
+    end
+
+    it "should redirect to 404 page if user goes to a non-existent user's show page" do
+      get user_path(9999)
+      assert_response :redirect
+      must_redirect_to "/404.html"
+    end
   end
 
 end
