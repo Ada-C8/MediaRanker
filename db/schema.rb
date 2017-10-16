c69c0334474de6eab838e7a53f0fe5314291814b
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-16 06:32:02 -0700

    Created work and user belongs_to relationship for votes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016011243) do
+ActiveRecord::Schema.define(version: 20171016020338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20171016011243) do
   create_table "votes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "work_id"
+    t.integer "user_id"
   end
 
   create_table "works", force: :cascade do |t|
