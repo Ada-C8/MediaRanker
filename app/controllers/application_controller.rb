22230cf68b15ca0c826d53c8d61006e94aa3c996
murog <guillermina.muro@gmail.com>
2017-10-15 22:42:30 -0700

    added controller filter to check if a user is currently logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :find_user_session
+end
+private
+def find_user_session
+  @session_user = nil
+  User.all.each do |user|
+    @session_user = user if user.id == session[:user_id]
+  end
+  return @session_user
 end
