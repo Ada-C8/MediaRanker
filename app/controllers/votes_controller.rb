09bc12ef804eab81eb905d44416ad7ea4b0ca28a
Maria McGrew <mcgmar1@outlook.com>
2017-10-15 17:59:40 -0700

    must be logged in added to votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,9 +4,14 @@ class VotesController < ApplicationController
       @vote = Vote.new
       @vote.user_id = session[:user_id]
       @vote.work_id = params[:work_id]
-        if @vote.save
-          flash[:success] = "Successfully upvoted!"
+        if @vote.user_id == nil
+          flash[:error] = "Must be logged in to vote"
           redirect_to works_path
+        else
+          if @vote.save
+            flash[:success] = "Successfully upvoted!"
+            redirect_to works_path
+          end
         end
   end
 end
