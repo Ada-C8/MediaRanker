48c0fc641abe210aa8cd529ec2aa2e0b896abe46
Tanisha <tanishahampden@gmail.com>
2017-10-16 11:39:21 -0700

    add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
 
   resources :works
   resources :users, only: [:index, :show]
+  resources :votes, only: [:new]
   root 'main#index'
 
   get 'login', to: 'users#login_form', as: 'login'
   post 'login', to: 'users#login'
   get 'logout', to: 'users#logout', as: 'logout'
+
+
 end
