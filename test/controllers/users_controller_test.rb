30f2e812e9e3d3c022561f5c55e4897ad36a2d1a
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-16 14:03:46 -0700

    got foundation fixed

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,23 +6,23 @@ describe UsersController do
       get users_path
       must_respond_with :success
     end
-  end
-
-  describe "show" do
-    it "returns a success status when an existing user is selected" do
-      get users_path(User.first.id)
-      must_respond_with :success
-    end
-
-    it "returns an error message when given a bogus User id" do
-      get users_path(User.last.id + 1)
-      must_respond_with :not_found
-    end
-
+    
     it "returns a success status when there are no users" do
       User.destroy_all
       get users_path
       must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "returns a success status when an existing user is selected" do
+      get user_path(User.first.id)
+      must_respond_with :success
+    end
+
+    it "returns an error message when given a bogus User id" do
+      get user_path(User.last.id + 1)
+      must_respond_with :not_found
     end
   end
 
