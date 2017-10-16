312927925ecf234e5dccb030c8e8c4a51921faf5
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-15 21:17:14 -0700

    Fix name of migration

--- db/migrate/20171010221225_change_names_of_created_by_and_published_columns.rb
+++ db/migrate/20171010221225_change_names_of_created_by_and_published_columns.rb
@@ -1,4 +1,4 @@
-class Change < ActiveRecord::Migration[5.1]
+class ChangeNamesOfCreatedByAndPublishedColumns < ActiveRecord::Migration[5.1]
   def change
     rename_column :works, :created_by, :creator
     rename_column :works, :published, :publication_year
