ec56f5cf4f9f41132735470eb14e0ad9ea1aa84b
murog <guillermina.muro@gmail.com>
2017-10-15 20:55:22 -0700

    failing user validation tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,18 @@ require "test_helper"
 
 describe User do
   let(:user) { User.new }
+  let(:crisco) { users(:crisco) }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  it "must be valid if it has a name" do
+    crisco.valid?.must_equal true
+    crisco.name = nil
+    crisco.valid?.must_equal false
+  end
+
+  it "should not be valid if user with that name already exists" do
+    user.name = crisco.name
+    work.save.must_equal false
+    work.errors.keys.must_include :name
+    work.errors[:name].must_equal ["has already been taken"]
   end
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -2,8 +2,9 @@ require "test_helper"
 
 describe Vote do
   let(:vote) { Vote.new }
+  let(:work) { Work.new }
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+  it "must have a valid work and user to be valid" do
+    
   end
 end
