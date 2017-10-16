6f9795bd5b8629d3b6eafb6d95fa94ea3a1538df
Jan Edrozo <jedrozo@gmail.com>
2017-10-15 23:10:26 -0700

    Reviewed work model test validation

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,7 +3,13 @@ require "test_helper"
 describe Work do
   let(:work) { Work.new }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  let(:book1) { works(:book1) }
+
+  it "must have a title to be valid" do
+    book1.valid?.must_equal true
+    book1.title = nil
+    book1.valid?.must_equal false
   end
+
+
 end
