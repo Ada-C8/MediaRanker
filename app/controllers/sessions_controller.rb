9ad5ddfb06d9d1c93304faf79e10bc458eb9bacb
murog <guillermina.muro@gmail.com>
2017-10-16 17:06:32 -0700

    oauth uid convert to integer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -37,12 +37,12 @@ class SessionsController < ApplicationController
 
   def create
     @auth_hash = request.env['omniauth.auth']
-    @user = User.find_by(uid: @auth_hash['uid'], provider: @auth_hash['provider'])
+    @user = User.find_by(uid: @auth_hash['uid'].to_i, provider: @auth_hash['provider'])
     if @user
       session[:user_id] = @user.id
       flash[:success] = "Welcome #{@user.name}"
     else
-      @user = User.new uid: @auth_hash['uid'], provider: @auth_hash['provider'], name: @auth_hash['info']['nickname'], email: @auth_hash['info']['email']
+      @user = User.new uid: @auth_hash['uid'].to_i, provider: @auth_hash['provider'], name: @auth_hash['info']['nickname'], email: @auth_hash['info']['email']
       if @user.save
         session[:user_id] = @user.id
         flash[:success] = "Welcome #{@user.name}"
