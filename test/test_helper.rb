efda55aca18eaa407b9ad96704134c009a796d14
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-22 15:30:32 -0700

    Turned Mocking and loggin in on

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,4 +20,18 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
+
+  def setup
+    OmniAuth.config.test-mode = true
+  end
+  def mock_auth_hash(user)
+    return {
+      provider: user.oauth_provider,
+      uid: user.oauth_uid,
+      info: {
+        email: user.email,
+        nickname: user.username
+      }
+    }
+  end
 end
