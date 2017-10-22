01ca00cfe29ca4c96a61ed10af79e0cb3eb89551
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-22 16:25:45 -0700

    added validations on the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :votes
   has_many :works, through: :votes
   validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :uid, presence: true, uniqueness: true, numericality: {only_integer: true}
+  validates :provider, presence: true, uniqueness: true
 
   def number_of_vote
     vote_num = 0
