d43292047c5b4913975461d719b20e66b10ee428
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-22 13:53:06 -0700

    Added Could not create new user failure message in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,20 +5,22 @@ class UsersController < ApplicationController
     if auth_hash['uid']
       user = User.find_by(provider: params[:provider], uid: params[:uid])
       if user.nil?
-        user = User.from_auth_hash(:provider, auth_hash)
+        user = User.from_auth_hash(auth_hash['provider'], auth_hash)
         # if user.persisted?
         if user.save
-          flash[:success] = :success
+          flash[:status] = :success
           flash[:message] = "Successfully created new user #{user.name}"
         else
           flash[:status] = :failure
           flash[:message] ="Could not create new user"
         end
       else
-        flash[:success] = :success
+        session[:user_id] = user.id
+        flash[:status] = :success
         flash[:message] = "Successfully logged in as returning user #{user.name}"
       end
-      session[:user_id] = user.id
+      flash[:status] = :failure
+      flash[:message] = "Could not create new user"
     end
   end
 
