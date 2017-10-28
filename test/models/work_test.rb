3e5d6fe39222bb1ef4e66f0ab5181296d226bf32
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-27 21:33:48 -0700

    updated model tests in light of refactoring

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -22,13 +22,13 @@ describe Work do
   end
 
   it "can't have two works with same title and same category" do
-    duplicate_book = Work.new(category: "book", title: "Harry Potter")
+    duplicate_book = Work.new(category: "book", title: "Harry Potter", publication_year: 1998)
     duplicate_book.save
     duplicate_book.valid?.must_equal false
   end
 
   it "same title different category is ok" do
-    book_to_movie = Work.new(category: "movie", title: "Harry Potter")
+    book_to_movie = Work.new(category: "movie", title: "Harry Potter", publication_year: 1998)
     book_to_movie.save
     book_to_movie.valid?.must_equal true
   end
@@ -49,7 +49,7 @@ describe Work do
 
   it "can find the top ten works by category" do
     10.times do |i|
-      Work.create(category: "book", title: "#{i} book")
+      Work.create(category: "book", title: "#{i} book", publication_year: 2000)
     end
     top_books = Work.top_ten("book")
     top_books.size.must_equal 10
@@ -58,7 +58,7 @@ describe Work do
 
   it "top_ten generates works even when no works in category have any votes" do
     10.times do |i|
-      Work.create(category: "movie", title: "#{i} book")
+      Work.create(category: "movie", title: "#{i} book", publication_year: 1998)
     end
     top_books = Work.top_ten("movie")
     top_books.size.must_equal 10
