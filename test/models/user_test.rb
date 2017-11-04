ed37c92f686772fb7f78005b8fb3138b19ac9c22
Angela Wilson <wilsonange@gmail.com>
2017-11-04 12:22:02 -0700

    write user_test.rb tests for relations and validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,22 @@ describe User do
   # it "must be valid" do
   #   value(user).must_be :valid?
   # end
+
+  describe "relations" do
+    it "has a list of votes" do
+      user = users(:user1)
+      user.must_respond_to :votes
+      user.votes.each do |vote|
+        vote.must_be_kind_of Vote
+      end
+    end
+  end
+
+  describe "validations" do
+    it "requires a name" do
+      user = User.new
+      user.valid?.must_equal false
+      user.errors.messages.must_include :name
+    end
+  end
 end
