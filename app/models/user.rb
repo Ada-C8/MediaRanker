935852e12ebaec605ed1b632d079b564f21f1989
Angela Wilson <wilsonange@gmail.com>
2017-11-05 16:57:06 -0800

    add custom user method to make vote, tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
+  def vote(work)
+    if work.id
+      vote = Vote.new(work_id: work.id, user_id: self.id)
+      vote.save
+    else
+      return "must have work id"
+    end
+  end
+
+
+  def valid
+
+    if user_id > 0 && work_id > 0
+      return true
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -24,4 +24,22 @@ describe User do
       user.errors.messages.must_include :name
     end
   end
+
+  describe "custom method" do
+    it "#vote creates a Vote" do
+      work = works(:work1)
+      user = users(:user1)
+
+      user.vote(work).must_be_instance_of Vote
+    end
+
+    it "#vote fails if work id is not present" do
+      work = Work.new
+      user = User.create(name: "name")
+      # puts user
+
+      vote = user.vote(work)
+      vote.must_equal "must have work id"
+    end
+  end
 end
